d55c820f3d2b71197735e64ff0bf71adf0475daf
DVLKinoMan <dvali.daviti@gmail.com>
2019-03-16 15:45:21 +0400

    Ref

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+using static System.Console;
+
+namespace DVL_LeetCode_Problems_Solutions.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/3Sum.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        //My Version. Timeout
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            var dic = new Dictionary<int, List<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    dic[nums[i]].Add(i);
+                else dic.Add(nums[i], new List<int> {i});
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int minus = (nums[i] + nums[j]) * -1;
+                if (dic.ContainsKey(minus) && dic[minus].Any(n => n != i && n != j))
+                {
+                    var l1 = new List<int> {nums[i], nums[j], minus};
+                    if (!list.Any(l => l.Contains(minus) && l.Except(l1).Count() == 0))
+                        list.Add(l1);
+                }
+            }
+
+            return list;
+        }
+
+        public static IList<IList<int>> ThreeSumBetter(int[] nums)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int begin = i + 1, end = nums.Length - 1;
+                while (begin < end)
+                {
+                    int sum = nums[i] + nums[begin] + nums[end];
+                    if (sum == 0)
+                    {
+                        list.Add(new List<int> {nums[i], nums[begin], nums[end]});
+                        begin++;
+                        end--;
+                        while (begin < end && nums[begin] == nums[begin - 1])
+                            begin++;
+                        while (begin < end && nums[end] == nums[end + 1])
+                            end--;
+                    }
+                    else if (sum < 0)
+                    {
+                        begin++;
+                        while (begin < end && nums[begin] == nums[begin - 1])
+                            begin++;
+                    }
+                    else
+                    {
+                        end--;
+                        while (begin < end && nums[end] == nums[end + 1])
+                            end--;
+                    }
+                }
+
+                while (i + 1 < nums.Length && nums[i] == nums[i + 1])
+                    i++;
+            }
+
+            return list;
+        }
+    }
+}

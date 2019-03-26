132a0ab509665ea7118fcdf2e050a0704a8d0a4f
DVLKinoMan <dvali.daviti@gmail.com>
2019-03-26 23:07:17 +0400

    Rotate Image (Not Done) and Merge Sorted Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Merge Sorted Array.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Merge Sorted Array
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="m"></param>
+        /// <param name="nums2"></param>
+        /// <param name="n"></param>
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var res = new List<int>();
+            int i = 0, j = 0;
+            while (i != m || j != n)
+            {
+                if (i == m)
+                    res.Add(nums2[j++]);
+                else if (j == n)
+                    res.Add(nums1[i++]);
+                else
+                {
+                    if (nums1[i] < nums2[j])
+                        res.Add(nums1[i++]);
+                    else res.Add(nums2[j++]);
+                }
+            }
+
+            for (int i2 = 0; i2 < res.Count; i2++)
+                nums1[i2] = res[i2];
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Rotate Image.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        public static void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
+            int i = 0, j = 0;
+            int c = 0;
+            int temp = 0;
+            while (true)
+            {
+                int temp2 = matrix[i][j];
+                matrix[i][j] = temp;
+                temp = matrix[i][j + n - 1];
+                matrix[i][j + n - 1] = temp2;
+                c++;
+            }
+        }
+
+        //private static Tuple<int, int> CalcNextCoordinate(int i, int j, int c, int n)
+        //{
+
+        //    n-c*2
+        //    int c = j >= n / 2 ? n - j - 1 : j;
+
+
+        //}
+    }
+}

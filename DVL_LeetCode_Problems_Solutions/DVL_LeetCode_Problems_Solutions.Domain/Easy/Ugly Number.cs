6a2cbf809cad3b2f3174e9270e73897c800b0496
DVLKinoMan <dvali.daviti@gmail.com>
2019-03-27 22:22:36 +0400

    Ugly Number

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Ugly Number.cs
@@ -0,0 +1,27 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Ugly Number
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsUgly(int num)
+        {
+            if (num == 0)
+                return false;
+            if (num == 1)
+                return true;
+
+            if (num % 2 == 0)
+                return IsUgly(num / 2);
+            if (num % 3 == 0)
+                return IsUgly(num / 3);
+            if (num % 5 == 0)
+                return IsUgly(num / 5);
+
+            return false;
+        }
+    }
+}

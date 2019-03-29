1407b0ad4ae24eae0ee51e1cb848bc5b894aab8a
DVLKinoMan <dvali.daviti@gmail.com>
2019-03-30 00:00:52 +0400

    Group Anagrams

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Group Anagrams.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dic=new Dictionary<string, List<string>>();
+            foreach (var str in strs)
+            {
+                var strSorted = string.Concat(str.OrderBy(s => s));
+                if (dic.ContainsKey(strSorted))
+                    dic[strSorted].Add(str);
+                else dic.Add(strSorted, new List<string> { str });
+            }
+
+            var result = new List<IList<string>>();
+            foreach (var d in dic)
+                result.Add(d.Value);
+
+            return result;
+        }
+
+        //Deprecated
+        //private static int GetUniqueCodeForString(string str)
+        //{
+        //    int unique = 0;
+        //    foreach (var ch in str)
+        //        unique += (int) Math.Pow(2, ch - 'a');
+        //    return unique;
+        //}
+    }
+}

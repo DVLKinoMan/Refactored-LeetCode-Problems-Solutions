16a2c609fa1c18c9833e78dd8126e5ecec71adf6
DVLKinoMan <dvali.daviti@gmail.com>
2019-04-04 21:50:32 +0400

    Merge Intervals

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Classes/Interval.cs
@@ -0,0 +1,20 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain.Classes
+{
+    public class Interval
+    {
+        public int start;
+        public int end;
+
+        public Interval()
+        {
+            start = 0;
+            end = 0;
+        }
+
+        public Interval(int s, int e)
+        {
+            start = s;
+            end = e;
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Merge Intervals.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Merge Intervals
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <returns></returns>
+        public static IList<Interval> Merge(IList<Interval> intervals)
+        {
+            var result = new List<Interval>();
+            Interval lastInterval = null;
+
+            foreach (var interval in intervals.OrderBy(i => i.start))
+            {
+                if (lastInterval == null)
+                    lastInterval = interval;
+                else if (lastInterval.end >= interval.start)
+                    lastInterval.end = Math.Max(lastInterval.end, interval.end);
+                else
+                {
+                    result.Add(lastInterval);
+                    lastInterval = interval;
+                }
+            }
+
+            if (lastInterval != null && (result.Count == 0 || result.Last() != lastInterval))
+                result.Add(lastInterval);
+
+            return result;
+        }
+    }
+}

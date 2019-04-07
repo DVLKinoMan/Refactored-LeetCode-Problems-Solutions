4ea6cdf873405da5a62b47203a4d53062e3d0e2d
DVL_KinoMan <dvali.daviti@gmail.com>
2019-04-07 12:41:21 +0400

    Rotate List

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Rotate List.cs
@@ -0,0 +1,42 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Rotate List
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            var curr = head;
+            int count = 1;
+            while (curr.next != null)
+            {
+                curr = curr.next;
+                count++;
+            }
+
+            curr.next = head;
+
+            int c = 1, index = count - (k % count);
+            curr = head;
+            while (c != index)
+            {
+                curr = curr.next;
+                c++;
+            }
+
+            var result = curr.next;
+            curr.next = null;
+
+            return result;
+        }
+    }
+}

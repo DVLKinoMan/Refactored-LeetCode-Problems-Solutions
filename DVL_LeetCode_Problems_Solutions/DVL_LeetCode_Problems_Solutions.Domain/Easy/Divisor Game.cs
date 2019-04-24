10398499d3552fac5d303baf92e0f6009e8bcdcc
DVL_KinoMan <dvali.daviti@gmail.com>
2019-04-24 23:15:33 +0400

    Divisor Game

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Divisor Game.cs
@@ -0,0 +1,15 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Divisor Game (Not Mine Solution)
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public static bool DivisorGame(int N)
+        {
+            return N % 2 == 0;
+        }
+    }
+}

4dcd1ea3ac0b738289252316acc738881916d1dd
DVL_KinoMan <dvali.daviti@gmail.com>
2019-04-27 22:44:53 +0400

    Construct Binary Search Tree from Preorder Treversal

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Construct Binary Search Tree from Preorder Traversal.cs
@@ -0,0 +1,29 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Construct Binary Search Tree from Preorder Treversal (Not mine Solution)
+        /// </summary>
+        /// <param name="preorder"></param>
+        /// <returns></returns>
+        public static TreeNode BstFromPreorder(int[] preorder)
+        {
+            int i = 0;
+            return BstFromPreOrderHelper(preorder, int.MaxValue, ref i);
+        }
+
+        private static TreeNode BstFromPreOrderHelper(int[] arr, int bound, ref int i)
+        {
+            if (arr.Length == i || arr[i] > bound)
+                return null;
+
+            TreeNode root = new TreeNode(arr[i++]);
+            root.left = BstFromPreOrderHelper(arr, root.val, ref i);
+            root.right = BstFromPreOrderHelper(arr, bound, ref i);
+            return root;
+        }
+    }
+}

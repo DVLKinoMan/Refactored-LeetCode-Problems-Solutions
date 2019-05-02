08cf74a7e41005a588129d04b8cdf575e1926d50
DVL_KinoMan <dvali.daviti@gmail.com>
2019-05-02 21:27:20 +0400

    Maximum Difference Between Node and Ancestor

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Maximum Difference Between Node and Ancestor.cs
@@ -0,0 +1,29 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Maximum Difference Between Node and Ancestor (Not mine Solution)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int MaxAncestorDiff(TreeNode root)
+        {
+            return MaxAncestorDiffHelper(root, root.val, root.val);
+        }
+
+        public static int MaxAncestorDiffHelper(TreeNode node, int max, int min)
+        {
+            if (node == null)
+                return max - min;
+
+            max = Math.Max(max, node.val);
+            min = Math.Min(min, node.val);
+
+            return Math.Max(MaxAncestorDiffHelper(node.left, max, min), MaxAncestorDiffHelper(node.right, max, min));
+        }
+    }
+}

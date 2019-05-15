ecb0ce649f8eaaa29f5d667fb8ce05ad3dc8fa82
D.Dvali <dvali.daviti@gmail.com>
2019-05-15 18:35:00 +0400

    Longest Dupliate SubString (Solved but not mine)

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Longest Duplicate Substring.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Longest Duplicate Substring.cs
@@ -1,47 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
-        /// <summary>
-        /// Do not Works
-        /// </summary>
-        /// <param name="S"></param>
-        /// <returns></returns>
-        public static string LongestDupSubstring(string S)
+        ///// <summary>
+        ///// Do not Works
+        ///// </summary>
+        ///// <param name="S"></param>
+        ///// <returns></returns>
+        //public static string LongestDupSubstring(string S)
+        //{
+        //    string result = string.Empty;
+
+        //    int start = 1, end = S.Length - 1;
+        //    while (start <= end)
+        //    {
+        //        int currLen = (start + end) / 2;
+        //        var set = new HashSet<string>();
+        //        int i = 0;
+        //        bool isWithLen = false;
+        //        while (i + currLen <= S.Length)
+        //        {
+        //            string subString = S.Substring(i, currLen);
+        //            if (!set.Add(subString))
+        //            {
+        //                isWithLen = true;
+        //                result = subString;
+        //                start = currLen + 1;
+        //                break;
+        //            }
+
+        //            i++;
+        //        }
+
+        //        if (!isWithLen)
+        //            end = currLen - 1;
+        //    }
+
+        //    return result;
+        //}
+
+        public static string LongestDupSubString(string S)
         {
-            string result = string.Empty;
-
-            int start = 1, end = S.Length - 1;
-            while (start <= end)
+            int lo = 0, hi = S.Length;
+            // binary search to find SMALLEST length of string that cannot pass test
+            while (lo < hi)
             {
-                int currLen = (start + end) / 2;
-                var set = new HashSet<string>();
-                int i = 0;
-                bool isWithLen = false;
-                while (i + currLen <= S.Length)
+                int mid = lo + (hi - lo) / 2;
+                int index = test(S, mid);
+                if (index < 0) hi = mid;
+                else
                 {
-                    string subString = S.Substring(i, currLen);
-                    if (!set.Add(subString))
-                    {
-                        isWithLen = true;
-                        result = subString;
-                        start = currLen + 1;
-                        break;
-                    }
-                    i++;
+                    lo = mid + 1;
                 }
+            }
+            // lo-1 is the LARGEST length of string that CAN pass test
+            int checkLen = lo - 1;
+            if (checkLen <= 0) return "";
+            int start = test(S, checkLen);
+            return S.Substring(start, checkLen);
+        }
 
-                if (!isWithLen)
-                    end = currLen - 1;
+        private static readonly long q = 2147483647;
+
+        private static long hash(string key, int m)
+        {
+            long h = 0;
+            for (int j = 0; j < m; j++)
+            {
+                h = (256 * h + key[j]) % q;
+            }
+            return h;
+        }
+
+        private static bool compare(string s, int i1, int i2, int m)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                if (s[i1 + i] != s[i2 + i]) return false;
+            }
+            return true;
+        }
+
+        private static int test(string s, int m)
+        {
+            var map = new Dictionary<long, List<int>>();
+            long h = hash(s, m);
+            map.Add(h, new List<int>());
+            map[h].Add(0);
+
+            long RM = 1; // R^(m-1) % q
+            for (int i = 1; i <= m - 1; i++)
+            {
+                RM = (256 * RM) % q;
             }
 
-            return result;
+            // NOTE i is the ending index of current string
+            for (int i = m; i < s.Length; i++)
+            {
+                // remove leading digit, add trailing digit, check for match
+                h = (h + q - RM * s[i - m] % q) % q;
+                h = (h * 256 + s[i]) % q;
+                int startIndex = i - m + 1;
+                if (map.ContainsKey(h))
+                {
+                    foreach (int prev in map[h])
+                    {
+                        if (compare(s, startIndex, prev, m)) return startIndex;
+                    }
+                }
+                else
+                {
+                    map.Add(h, new List<int>());
+                }
+                map[h].Add(startIndex);
+            }
+
+            return -1;
         }
+
     }
 }

ea0d5753271cb9e674ee7340b9acccdbeac3ee9e
D.Dvali <dvali.daviti@gmail.com>
2019-05-17 15:05:01 +0400

    Broken Calculator

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/Broken Calculator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+
+namespace DVL_LC_Problems.Tests
+{
+    /// <summary>
+    /// Summary description for Broken_Calculator
+    /// </summary>
+    [TestClass]
+    public class Broken_Calculator
+    {
+        public Broken_Calculator()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int answer = BrokenCalc(2, 3);
+            Assert.AreEqual(2, answer);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int answer = BrokenCalc(5, 8);
+            Assert.AreEqual(2, answer);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int answer = BrokenCalc(3,10);
+            Assert.AreEqual(3, answer);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int answer = BrokenCalc(1024, 1);
+            Assert.AreEqual(1023, answer);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int answer = BrokenCalc(8, 10);
+            Assert.AreEqual(4, answer);
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Broken Calculator.cs
@@ -0,0 +1,21 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Broken Calculator (Mine)
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <returns></returns>
+        public static int BrokenCalc(int X, int Y)
+        {
+            if (Y <= X)
+                return X - Y;
+
+            if (Y % 2 == 1)
+                return BrokenCalc(X, Y + 1) + 1;
+            else return BrokenCalc(X, Y / 2) + 1;
+        }
+    }
+}

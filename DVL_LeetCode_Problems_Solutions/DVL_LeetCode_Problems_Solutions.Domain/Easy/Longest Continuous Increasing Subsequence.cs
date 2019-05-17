543aac4ffba65f33fb2f7cb7e63a8d0aafe165aa
D.Dvali <dvali.daviti@gmail.com>
2019-05-17 15:12:45 +0400

    Longest Continuous Increasing Subsequence

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Longest Continuous Increasing Subsequence.cs
@@ -0,0 +1,34 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Longest Continuous Increasing Subsequence (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindLengthOfLCIS(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int max = 1, curr = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                    curr++;
+                else
+                {
+                    if (max < curr)
+                        max = curr;
+                    curr = 1;
+                }
+            }
+
+            if (curr > max)
+                max = curr;
+
+            return max;
+        }
+    }
+}

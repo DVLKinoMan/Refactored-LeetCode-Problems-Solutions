ee6da27e45ea9efffa6ca461162bdfd603868243
DVL_KinoMan <dvali.daviti@gmail.com>
2019-05-17 07:33:20 +0400

    Fibonacci Number

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Fibonacci Number.cs
@@ -0,0 +1,20 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Fibonacci Number
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public static int Fib(int N)
+        {
+            if (N == 0)
+                return 0;
+            if (N == 1)
+                return 1;
+
+            return Fib(N - 2) + Fib(N - 1);
+        }
+    }
+}

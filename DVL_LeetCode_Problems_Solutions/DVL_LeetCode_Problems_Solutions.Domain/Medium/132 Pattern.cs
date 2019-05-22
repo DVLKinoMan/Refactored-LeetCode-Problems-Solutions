11fbe0c824bcd3778369201f321403f1ad0c358b
DVL_KinoMan <dvali.daviti@gmail.com>
2019-05-22 08:39:25 +0400

    132 pattern

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/132 Pattern.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// 132 Pattern (Not Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static bool Find132pattern(int[] nums)
+        {
+            int s3 = int.MinValue;
+            var st = new Stack<int>();
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] < s3) return true;
+                else while (st.Count!=0 && nums[i] > st.Peek())
+                {
+                    s3 = st.Pop();
+                }
+                st.Push(nums[i]);
+            }
+            return false;
+        }
+    }
+}

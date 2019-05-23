346bf56bd1fbc666c232950a5c24443b50502e9c
DVL_KinoMan <dvali.daviti@gmail.com>
2019-05-23 08:41:50 +0400

    Next Greater Node In Linked List

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Next Greater Node In Linked List.cs
@@ -0,0 +1,31 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Next Greater Node In Linked List (Not Mine)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static int[] NextLargerNodes(ListNode head)
+        {
+            var list = new List<int>();
+            for (var curr = head; curr != null; curr = curr.next)
+                list.Add(curr.val);
+
+            var indexes=new Stack<int>();
+            int[] res=new int[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                while (indexes.Count != 0 && list[indexes.Peek()] < list[i])
+                    res[indexes.Pop()] = list[i];
+                indexes.Push(i);
+            }
+
+            return res;
+        }
+    }
+}

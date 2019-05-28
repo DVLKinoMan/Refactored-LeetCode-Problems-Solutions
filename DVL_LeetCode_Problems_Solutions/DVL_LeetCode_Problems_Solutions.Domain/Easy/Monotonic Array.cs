a6148fecd4612898c79ba9328d15fa364496762d
DVL_KinoMan <dvali.daviti@gmail.com>
2019-05-28 07:54:54 +0400

    Monotonic Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Monotonic Array.cs
@@ -0,0 +1,33 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Monotonic Array (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static bool IsMonotonic(int[] A)
+        {
+            if (A.Length <= 1)
+                return true;
+
+            bool increase = false;
+            int i = 1;
+            while (i < A.Length && A[i] == A[i - 1])
+                i++;
+
+            if (i == A.Length)
+                return true;
+
+            if (A[i] > A[i - 1])
+                increase = true;
+
+            for (; i < A.Length; i++)
+                if ((increase && A[i] < A[i - 1]) || (!increase && A[i] > A[i - 1]))
+                    return false;
+
+            return true;
+        }
+    }
+}

7ba66133f91f3f498e25fcca76e427138ab05fee
D.Dvali <dvali.daviti@gmail.com>
2019-05-28 16:27:52 +0400

    Find Minimum in Rotated Sorted Array II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Find Minimum in Rotated Sorted Array II.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find Minimum in Rotated Sorted Array II (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindMin(int[] nums)
+        {
+            nums = nums.Distinct().ToArray();
+            int start = 0, end = nums.Length - 1;
+            if (nums.Length == 1 || nums[start] < nums[end])
+                return nums[start];
+
+            int middle = 0;
+            while (start <= end)
+            {
+                middle = (start + end) / 2;
+                if (nums[0] <= nums[middle])
+                    start = middle + 1;
+                else
+                    end = middle - 1;
+            }
+
+            return nums[start >= nums.Length ? nums.Length - 1 : start];
+        }
+    }
+}

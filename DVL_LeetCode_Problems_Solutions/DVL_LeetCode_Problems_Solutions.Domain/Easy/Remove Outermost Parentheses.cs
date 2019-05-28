a9aad8c48ecea3fb02c8ba412aad55e8ca8396fb
D.Dvali <dvali.daviti@gmail.com>
2019-05-28 15:26:47 +0400

    Remove Outermost Parentheses

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Remove Outermost Parentheses.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Remove Outermost Parentheses (Mine)
+        /// </summary>
+        /// <param name="S"></param>
+        /// <returns></returns>
+        public static string RemoveOuterParentheses(string S)
+        {
+            int index = 0, prevIndex = 0;
+            var builder = new StringBuilder();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '(')
+                    index++;
+                else index--;
+
+                if (index == 0)
+                {
+                    if (i != prevIndex + 1)
+                        builder.Append(S.Substring(prevIndex + 1, i - prevIndex - 1));
+                    prevIndex = i + 1;
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}

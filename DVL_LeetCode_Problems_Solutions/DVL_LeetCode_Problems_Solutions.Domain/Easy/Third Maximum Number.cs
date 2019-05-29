915ec96a98237ade8cc4888148ef7a1d4ce6b048
DVL_KinoMan <dvali.daviti@gmail.com>
2019-05-29 08:21:36 +0400

    Third Maximum Number

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Third Maximum Number.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Third Maximum Number (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int ThirdMax(int[] nums)
+        {
+            nums = nums.OrderByDescending(n=>n).Distinct().ToArray();
+            if (nums.Length < 3)
+                return nums[0];
+
+            return nums[2];
+        }
+    }
+}

db385c7786ed6b70e1296a22c9f8d372148be2d8
DVL_KinoMan <dvali.daviti@gmail.com>
2019-05-30 08:35:39 +0400

    Rabbits in Forest

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Rabbits in Forest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Rabbits in Forest (Mine)
+        /// </summary>
+        /// <param name="answers"></param>
+        /// <returns></returns>
+        public static int NumRabbits(int[] answers)
+        {
+            var dic=new Dictionary<int, int>();
+            foreach (var ans in answers)
+            {
+                if (dic.ContainsKey(ans))
+                    dic[ans]++;
+                else dic.Add(ans, 1);
+            }
+
+            int count = 0;
+            foreach (var pair in dic)
+                count += (pair.Value / (pair.Key + 1)) * (pair.Key + 1) + (pair.Value % (pair.Key) > 0 ? pair.Key + 1 : 0);
+
+            return count;
+        }
+    }
+}

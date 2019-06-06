7b6516518bfcb5bc55bca7d093b0986ec0bcd136
D.Dvali <dvali.daviti@gmail.com>
2019-06-06 11:35:05 +0400

    Add to Array-Form of Integer

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Add to Array-Form of Integer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Add to Array-Form of Integer (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static IList<int> AddToArrayForm(int[] A, int K)
+        {
+            var list = new List<int>();
+            var secondArray = K.ToString().Select(n => n - 48).ToArray();
+            int carry = 0;
+            for (int i = 1; i <= A.Length || i <= secondArray.Length || carry > 0; i++)
+            {
+                int index1 = A.Length - i;
+                int index2 = secondArray.Length - i;
+                int sum = index1 >= 0 ? A[index1] : 0 + index2 >= 0 ? secondArray[index2] : 0 + carry;
+                list.Add(sum % 10);
+                carry = sum / 10;
+            }
+
+            list.Reverse();
+            return list.ToArray();
+        }
+    }
+}

66e5e345aa88f460eec6883125036e116860287e
DVL_KinoMan <dvali.daviti@gmail.com>
2019-06-06 08:48:52 +0400

    Do not works

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Add to Array-Form of Integer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        public IList<int> AddToArrayForm(int[] A, int K)
+        {
+            var list = new List<int>();
+            var arr = K.ToString().Select(i => (int) i - 48).ToArray();
+            int carry = 0;
+            for (int i = 1; i <= A.Length && i <= arr.Length; i++)
+            {
+                int num = A[A.Length - i] + arr[arr.Length - i] + carry;
+                list.Add(num % 10);
+                carry = num / 10;
+            }
+
+            if (carry != 0)
+                list.Add(carry);
+
+            list.Reverse();
+
+            return list.ToArray();
+        }
+    }
+}

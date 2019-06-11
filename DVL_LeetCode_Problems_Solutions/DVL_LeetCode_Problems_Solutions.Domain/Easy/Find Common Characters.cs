3d5ea33907ed08679042d568490d0be3c10d5df6
D.Dvali <dvali.daviti@gmail.com>
2019-06-11 17:22:15 +0400

    Find Common Characters

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Find Common Characters.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find Common Characters (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static IList<string> CommonChars(string[] A)
+        {
+            var intersection = A[0].ToCharArray().ToList();
+            for (int i = 1; i < A.Length; i++)
+            for (int k = 0; k < intersection.Count; k++)
+            {
+                int index = A[i].IndexOf(intersection[k]);
+                if (index >= 0)
+                    A[i] = A[i].Remove(index, 1);
+                else
+                {
+                    intersection.RemoveAt(k);
+                    k--;
+                }
+            }
+
+            return intersection.Select(ch => ch.ToString()).ToList();
+        }
+    }
+}

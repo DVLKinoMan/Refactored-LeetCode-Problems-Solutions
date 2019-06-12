926e0038cdbfebd6ab4c9b690693bd8ff35861e6
D.Dvali <dvali.daviti@gmail.com>
2019-06-12 10:19:23 +0400

    N-ary Tree Level Order Traversal

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Classes/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Classes
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node()
+        {
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/N-ary Tree Level Order Traversal.cs
@@ -0,0 +1,45 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// N-ary Tree Level Order Traversal (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<IList<int>> LevelOrder(Node root)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            if (root == null)
+                return result;
+
+            var nodeLists = new List<List<Node>>() { new List<Node>() { root } };
+            while (nodeLists.Count != 0)
+            {
+                var nodesInOneLevel = nodeLists.First();
+                nodeLists.RemoveAt(0);
+
+                var list = new List<int>();
+                var nodesList = new List<Node>();
+                foreach (var node in nodesInOneLevel)
+                {
+                    list.Add(node.val);
+                    foreach (var child in node.children)
+                        nodesList.Add(child);
+                }
+
+                if (nodesList.Count != 0)
+                    nodeLists.Add(nodesList);
+
+                result.Add(list);
+            }
+
+            return result;
+        }
+    }
+}

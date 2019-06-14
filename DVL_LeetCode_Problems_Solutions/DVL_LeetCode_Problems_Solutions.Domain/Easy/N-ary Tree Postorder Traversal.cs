5a05b2cb93330a3bd015ea50d423b1984e0c85b3
D.Dvali <dvali.daviti@gmail.com>
2019-06-14 10:38:18 +0400

    N-ary Tree Postorder Traversal

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/N-ary Tree Postorder Traversal.cs
@@ -0,0 +1,35 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// N-ary Tree Postorder Traversal
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> Postorder(Node root)
+        {
+            var list = new List<int>();
+            if (root == null)
+                return list;
+
+            var stack = new Stack<Node>();
+            stack.Push(root);
+
+            while (stack.Count!=0)
+            {
+                root = stack.Pop();
+                list.Add(root.val);
+                foreach (var node in root.children)
+                    stack.Push(node);
+            }
+
+            list.Reverse();
+
+            return list;
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Rectangle Area.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        //public static int ComputeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+        //{
+        //    int firstArea = Math.Abs(D - B) * Math.Abs(C - A);
+        //    int secondArea = Math.Abs(H - F) * Math.Abs(G - E);
+        //}
+    }
+}

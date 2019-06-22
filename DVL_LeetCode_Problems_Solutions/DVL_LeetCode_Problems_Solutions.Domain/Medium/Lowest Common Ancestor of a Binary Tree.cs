5a2aec4a36e100304399ad6fe6455520db9b5467
DVL_KinoMan <dvali.daviti@gmail.com>
2019-06-22 11:48:43 +0400

    Lowest Common Ancestor of a Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Lowest Common Ancestor of a Binary Tree.cs
@@ -0,0 +1,30 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Lowest Common Ancestor of a Binary Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || root == p || root == q)
+                return root;
+
+            var left = LowestCommonAncestor(root.left, p, q);
+            var right = LowestCommonAncestor(root.right, p, q);
+
+            if ((left, right) == (p, q) || (left, right) == (q, p))
+                return root;
+            //if ((left == p && right == q) || (left == q && right == p))
+            //    return root;
+
+            return left??right;
+        }
+    }
+}

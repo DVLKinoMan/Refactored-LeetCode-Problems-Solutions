9132a8f5b8bbeb13f83c3a411f36670695ab3907
DVL_KinoMan <dvali.daviti@gmail.com>
2019-06-29 13:02:18 +0400

    Flip Equivalent Binary Trees

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Flip Equivalent Binary Trees.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Flip Equivalent Binary Trees.cs
@@ -1,31 +1,51 @@
 ﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
+        /// <summary>
+        /// Flip Equivalent Binary Trees (Mine)
+        /// </summary>
+        /// <param name="root1"></param>
+        /// <param name="root2"></param>
+        /// <returns></returns>
         public static bool FlipEquiv(TreeNode root1, TreeNode root2)
         {
-
+            return FlipEquivHelper(root1, root2, null, null);
         }
 
-        private static bool FlipEquivHelper(TreeNode tree1Parent, TreeNode tree1Node1, TreeNode tree2Node1, TreeNode tree2Parent, TreeNode tree1Node2, TreeNode tree2Node2)
+        private static bool FlipEquivHelper(TreeNode tree1LeftNode, TreeNode tree2LeftNode, TreeNode tree1RightNode,
+            TreeNode tree2RightNode)
         {
-            if (tree1Node1?.val != tree2Node1?.val)
-            {
-                if (tree1Node1?.val != tree2Node2?.val)
-                    return false;
+            bool tree1NodesAreNull = (tree1LeftNode, tree1RightNode) == (null, null);
+            bool tree2NodesAreNull = (tree2LeftNode, tree2RightNode) == (null, null);
+            //bool tree1NodesAreNull = tree1LeftNode == null && tree1RightNode == null;
+            //bool tree2NodesAreNull = tree2LeftNode == null && tree2RightNode == null;
+            if (tree1NodesAreNull && tree2NodesAreNull)
+                return true;
+            if (tree2NodesAreNull || tree2NodesAreNull)
+                return false;
 
-                tree1Parent.left = tree2Node2;
-                tree2Parent.right = tree1Node1;
+            if ((tree1LeftNode?.val, tree1RightNode?.val) == (tree2LeftNode?.val, tree2RightNode?.val))
+                return FlipEquivHelper(tree1LeftNode?.left, tree2LeftNode?.left,
+                           tree1LeftNode?.right, tree2LeftNode?.right) &&
+                       FlipEquivHelper(tree1RightNode?.left, tree2RightNode?.left,
+                           tree1RightNode?.right, tree2RightNode?.right);
 
-                return 
-            }
+            //if (tree1LeftNode?.val == tree2LeftNode?.val && tree1RightNode?.val == tree2RightNode?.val)
+            //    return FlipEquivHelper(tree1LeftNode?.left, tree2LeftNode?.left,
+            //               tree1LeftNode?.right, tree2LeftNode?.right) &&
+            //           FlipEquivHelper(tree1RightNode?.left, tree2RightNode?.left,
+            //               tree1RightNode?.right, tree2RightNode?.right);
+
+            if (tree1LeftNode?.val != tree2RightNode?.val || tree1RightNode?.val != tree2LeftNode?.val)
+                return false;
+
+            return FlipEquivHelper(tree1LeftNode?.left, tree2RightNode?.left,
+                       tree1LeftNode?.right, tree2RightNode?.right) &&
+                   FlipEquivHelper(tree1RightNode?.left, tree2LeftNode?.left,
+                       tree1RightNode?.right, tree2LeftNode?.right);
         }
     }
 }

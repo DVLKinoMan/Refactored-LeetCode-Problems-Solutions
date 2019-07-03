47b66029c486a9b35de07657886881a48e4ee52b
DVL_KinoMan <dvali.daviti@gmail.com>
2019-07-03 08:56:59 +0400

    Valid Square (Not Implemented)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Valid Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static bool ValidSquare(int[] p1, int[] p2, int[] p3, int[] p4)
+        {
+
+        }
+
+        public static double DistanceBettweenTwoPoints(int[] p1, int[] p2) =>
+            Math.Sqrt(Math.Pow(Math.Abs(p1[0] - p2[0]), 2) + Math.Pow(Math.Abs(p1[1] - p2[1]), 2));
+    }
+}

792482992f38032537ad6aa708af12e99fb914bb
DVL_KinoMan <dvali.daviti@gmail.com>
2019-07-04 08:38:35 +0400

    Complement of Base 10 Integer

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Complement of Base 10 Integer.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Complement of Base 10 Integer (Mine)
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public static int BitwiseComplement(int N)
+        {
+            var binaryArr = System.Convert.ToString(N, 2).ToArray();
+            for(int i = 0; i<binaryArr.Length; i++)
+                if (binaryArr[i] == '1')
+                    binaryArr[i] = '0';
+                else
+                    binaryArr[i] = '1';
+            if (!binaryArr.Contains('1'))
+                return 0;
+
+            return System.Convert.ToInt32(string.Join("", binaryArr.SkipWhile(i => i == '0')), 2);
+        }
+    }
+}

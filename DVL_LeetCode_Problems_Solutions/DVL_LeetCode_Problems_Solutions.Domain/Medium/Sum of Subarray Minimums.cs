939f95683afe3704043013acbc0cc199b394e598
DVL_KinoMan <dvali.daviti@gmail.com>
2019-07-05 22:01:03 +0400

    Sum of Subarray Minimums

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Sum of Subarray Minimums.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Sum of Subarray Minimums.cs
@@ -1,34 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
+        //public static int modulo =(int) Math.Pow(10, 9) + 7;
+        //public static int SumSubarrayMins(int[] A, int n = -1)
+        //{
+        //    if (n == -1)
+        //        n = A.Length;
+        //    if (n == 0)
+        //        return 0;
+
+        //    int result = A[0];
+
+        //    for (int i = 1; i < n; i++)
+        //    {
+        //        A[i - 1] = Math.Min(A[i - 1], A[i]);
+        //        result += A[i];
+        //        result %= modulo;
+        //    }
+
+        //    return result + SumSubarrayMins(A, n - 1);
+        //}
+
+        /// <summary>
+        /// Sum of Subarray Minimums (Not Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
         public static int SumSubarrayMins(int[] A)
         {
-            if (A.Length == 0)
-                return 0;
-
-            bool isOdd = A.Length % 2 == 1;
-            int[] arr = new int[A.Length / 2 + (isOdd ? 1 : 0)];
-            int result = 0;
-
-            int j = 0;
-            for (int i = 1; i < A.Length; i += 2, j++)
+            int res = 0, n = A.Length, mod = (int) 1e9 + 7;
+            int[] left = new int[n], right = new int[n];
+            Stack<(int, int)> s1 = new Stack<(int, int)>(), s2 = new Stack<(int, int)>();
+            for (int i = 0; i < n; ++i)
             {
-                int k = A[i] + A[i - 1];
-                arr[j] = k;
-                result += k;
+                int count = 1;
+                while (s1.Count != 0 && s1.Peek().Item1 > A[i])
+                    count += s1.Pop().Item2;
+                s1.Push((A[i], count));
+                left[i] = count;
             }
 
-            if (isOdd)
-                arr[arr.Length - 1] = A[A.Length - 1];
+            for (int i = n - 1; i >= 0; --i)
+            {
+                int count = 1;
+                while (s2.Count != 0 && s2.Peek().Item1 >= A[i])
+                    count += s2.Pop().Item2;
+                s2.Push((A[i], count));
+                right[i] = count;
+            }
 
-            return result + SumSubarrayMins(arr);
+            for (int i = 0; i < n; ++i)
+                res = (res + A[i] * left[i] * right[i]) % mod;
+            return res;
         }
     }
 }

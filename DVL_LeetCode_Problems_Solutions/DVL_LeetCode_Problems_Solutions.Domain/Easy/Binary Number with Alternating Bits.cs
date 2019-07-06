0b63b4e2bb3569b4d07f74619147c381f4010d9d
DVL_KinoMan <dvali.daviti@gmail.com>
2019-07-06 15:00:25 +0400

    Binary Number with Alternating Bits

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Binary Number with Alternating Bits.cs
@@ -0,0 +1,25 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Binary Number with Alternating Bits (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool HasAlternatingBits(int n)
+        {
+            //My Version Which is Faster in leetcode
+            string str = System.Convert.ToString(n, 2);
+            for (int i = 1; i < str.Length; i++)
+                if (str[i] == str[i - 1])
+                    return false;
+
+            return true;
+
+            //This is Slower
+            //n ^= (n >> 1);
+            //return (n & n + 1) == 0;
+        }
+    }
+}

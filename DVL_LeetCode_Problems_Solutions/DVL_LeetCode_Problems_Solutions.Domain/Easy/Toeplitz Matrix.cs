14dc0d3c4dbf782466292c1646ef229dc0e03279
DVL_KinoMan <dvali.daviti@gmail.com>
2019-07-18 07:55:03 +0400

    Toeplitz Matrix

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Toeplitz Matrix.cs
@@ -0,0 +1,48 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        ///// <summary>
+        /////  Toeplitz Matrix (Mine)
+        ///// </summary>
+        ///// <param name="matrix"></param>
+        ///// <returns></returns>
+        //public static bool IsToeplitzMatrix(int[][] matrix)
+        //{
+        //    int len = matrix.Length + matrix[0].Length;
+        //    for (int count = 1; count < len; count++)
+        //    {
+        //        int i = matrix.Length - count < 0 ? 0 : matrix.Length - count;
+        //        int j = matrix.Length - count < 0 ? Math.Abs(matrix.Length - count) : 0;
+        //        int val = matrix[i][j];
+        //        while (i < matrix.Length && j < matrix[0].Length)
+        //        {
+        //            if (matrix[i][j] != val)
+        //                return false;
+        //            i++;
+        //            j++;
+        //        }
+        //    }
+
+        //    return true;
+        //}
+
+        /// <summary>
+        ///  Toeplitz Matrix (Not Mine)
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static bool IsToeplitzMatrix(int[][] matrix)
+        {
+            for (int i = 1; i < matrix.Length; i++)
+            {
+                for (int j = 1; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] != matrix[i - 1][j - 1]) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

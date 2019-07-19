db06c802f5a6e06ea4699f431878cd0f62e08a11
D.Dvali <dvali.daviti@gmail.com>
2019-07-19 17:17:01 +0400

    Shortest Subarray with Sum at Least K

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/Shortest Subarray with Sum at Least K Tester.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestClass]
+    public class Shortest_Subarray_with_Sum_at_Least_K_Tester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var res = ShortestSubarray(new[] { 84, -37, 32, 40, 95 }, 167);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Shortest Subarray with Sum at Least K.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Shortest Subarray with Sum at Least K (Not Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static int ShortestSubarray(int[] A, int K)
+        {
+            int N = A.Length, res = N + 1;
+            int[] B = new int[N + 1];
+            for (int i = 0; i < N; i++) B[i + 1] = B[i] + A[i];
+            var d = new List<int>();
+            for (int i = 0; i < N + 1; i++)
+            {
+                while (d.Count > 0 && B[i] - B[d.First()] >= K)
+                {
+                    res = Math.Min(res, i - d.First());
+                    d.RemoveAt(0);
+                }
+                while (d.Count > 0 && B[i] <= B[d.Last()])
+                    d.RemoveAt(d.Count-1);
+                d.Add(i);
+            }
+            return res <= N ? res : -1;
+        }
+    }
+}

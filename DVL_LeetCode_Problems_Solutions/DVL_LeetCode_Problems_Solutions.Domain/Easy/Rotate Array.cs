f0326716862708cf0ea05093a4054863be093c5e
D.Dvali <dvali.daviti@gmail.com>
2019-07-19 18:20:53 +0400

    Rotate Array (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Rotate Array.cs
@@ -0,0 +1,28 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Rotate Array (Not Working)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        public static void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+            for(int i=0; i<k; i++)
+            {
+                int remember = nums[i];
+                int len = nums.Length / (k - 1) + (nums.Length % (k - 1) > 0 ? 1 : 0);
+                for (int j = 1; j < len; j++)
+                {
+                    //Swap
+                    int index = (i + j * k) % nums.Length;
+                    nums[index] += remember;
+                    remember = nums[index] - remember;
+                    nums[index] -= remember;
+                }
+            }
+        }
+    }
+}

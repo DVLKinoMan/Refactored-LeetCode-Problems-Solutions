05832460621dc9e221f348fd6aa8230addaec82c
D.Dvali <dvali.daviti@gmail.com>
2019-07-24 10:47:54 +0400

    Reordered Power of 2

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Reordered Power of 2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Reordered Power of 2 (Not Mine)
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public bool ReorderedPowerOf2(int N)
+        {
+            //Slower
+            //string str = ReorderedPowerOf2Helper(N);
+            //for (int i = 0; i < 32; i++)
+            //    if (ReorderedPowerOf2Helper(1 << i) == str)
+            //        return true;
+
+            //return false;
+
+            //Faster
+            long num = ReorderedPowerOf2Helper2(N);
+            for (int i = 0; i < 32; i++)
+                if (ReorderedPowerOf2Helper2(1 << i) == num)
+                    return true;
+
+            return false;
+        }
+
+        private string ReorderedPowerOf2Helper(int num)
+        {
+            var arr = num.ToString().ToCharArray();
+            Array.Sort(arr);
+            return string.Join("", arr);
+        }
+
+        private long ReorderedPowerOf2Helper2(int num)
+        {
+            long res = 0;
+            for (; num > 0; num /= 10) res += (int)Math.Pow(10, num % 10);
+            return res;
+        }
+    }
+}

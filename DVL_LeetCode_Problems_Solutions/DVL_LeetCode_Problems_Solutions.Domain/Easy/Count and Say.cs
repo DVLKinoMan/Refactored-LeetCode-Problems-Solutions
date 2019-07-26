7f8cd928a653e7d65a90b488cca330b02750514b
D.Dvali <dvali.daviti@gmail.com>
2019-07-26 10:40:54 +0400

    Count and Say

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Count and Say.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Count and Say (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static string CountAndSay(int n)
+        {
+            var prevStr = new StringBuilder("1");
+
+            for (int i = 1; i < n; i++)
+            {
+                var currStr = new StringBuilder();
+
+                char currChar = prevStr[0];
+                int count = 1;
+                for (int k = 1; k <= prevStr.Length; k++)
+                {
+                    if (k == prevStr.Length)
+                    {
+                        currStr.Append(count);
+                        currStr.Append(currChar);
+                    }
+                    else if (prevStr[k] != currChar)
+                    {
+                        currStr.Append(count);
+                        currStr.Append(currChar);
+                        count = 1;
+                        currChar = prevStr[k];
+                    }
+                    else if (prevStr[k] == currChar)
+                        count++;
+                }
+
+                prevStr = currStr;
+            }
+
+            return prevStr.ToString();
+        }
+    }
+}

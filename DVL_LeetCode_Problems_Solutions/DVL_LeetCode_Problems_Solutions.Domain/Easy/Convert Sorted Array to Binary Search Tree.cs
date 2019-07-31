e9c00c9350fc05a0c9b0c1f23a6254d945afe4b0
DVL_KinoMan <dvali.daviti@gmail.com>
2019-07-31 07:51:15 +0400

    Convert Sorted Array to Binary Search Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Convert Sorted Array to Binary Search Tree.cs
@@ -0,0 +1,30 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Convert Sorted Array to Binary Search Tree (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static TreeNode SortedArrayToBST(int[] nums)
+        {
+            return SortedArrayToBSTHelper(nums, 0, nums.Length - 1);
+        }
+
+        private static TreeNode SortedArrayToBSTHelper(int[] nums, int i, int j)
+        {
+            if (i >= j)
+                return null;
+            int index = (i + j) / 2;
+
+            var node = new TreeNode(nums[index]);
+            node.left = SortedArrayToBSTHelper(nums, i, index);
+            node.right = SortedArrayToBSTHelper(nums, index, j);
+
+            return node;
+        }
+    }
+}

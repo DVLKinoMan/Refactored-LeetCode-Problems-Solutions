f68645b9d593f1908cd77d823b8351375163bd9f
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-02 07:47:51 +0400

    Maximum Depth of Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Maximum Depth of Binary Tree.cs
@@ -0,0 +1,22 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Maximum Depth of Binary Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="h"></param>
+        /// <returns></returns>
+        public static int MaxDepth(TreeNode root, int h = 0)
+        {
+            if (root == null)
+                return h;
+
+            return Math.Max(MaxDepth(root.left, h + 1), MaxDepth(root.right, h + 1));
+        }
+    }
+}

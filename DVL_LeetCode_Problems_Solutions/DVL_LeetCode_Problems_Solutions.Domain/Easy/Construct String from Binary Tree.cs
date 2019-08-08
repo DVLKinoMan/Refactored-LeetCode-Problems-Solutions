fda28a00ad4b9ecc3188a5495f9a43d4792da80d
D.Dvali <dvali.daviti@gmail.com>
2019-08-08 11:00:47 +0400

    Construct String from Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Construct String from Binary Tree.cs
@@ -0,0 +1,31 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Construct String from Binary Tree (Mine)
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static string Tree2str(TreeNode t)
+        {
+            if (t == null)
+                return string.Empty;
+
+            string left = Tree2str(t.left);
+            string right = Tree2str(t.right);
+
+            string result = "";
+
+            if (!string.IsNullOrEmpty(left) || !string.IsNullOrEmpty(right))
+                result+=$"({left})";
+
+            if (!string.IsNullOrEmpty(right))
+                result += $"({right})";
+
+            return $"{t.val}{result}";
+        }
+    }
+}

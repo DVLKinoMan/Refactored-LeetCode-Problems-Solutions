aa8230d78f8fade0a45c5a7de55aca17a858b75f
D.Dvali <dvali.daviti@gmail.com>
2019-08-08 17:43:55 +0400

    Maximum Depth of N-ary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Maximum Depth of N-ary Tree.cs
@@ -0,0 +1,21 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Maximum Depth of N-ary Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int MaxDepth(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            return (root.children.Count == 0 ? 0 : root.children.Max(n => MaxDepth(n))) + 1;
+        }
+    }
+}

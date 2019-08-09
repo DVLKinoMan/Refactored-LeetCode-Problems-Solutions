30ec919cb056e9352a52c2ef10d99c973beb3068
D.Dvali <dvali.daviti@gmail.com>
2019-08-09 15:50:52 +0400

    Find Bottom Left Tree Value, Minimum Cost For Tickets

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Find Bottom Left Tree Value.cs
@@ -0,0 +1,36 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find Bottom Left Tree Value (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int FindBottomLeftValue(TreeNode root)
+        {
+            return FindBottomLeftValueHelper(root, 0).val;
+        }
+
+        private static (int val, int depth) FindBottomLeftValueHelper(TreeNode node, int depth)
+        {
+            if (node.left == null && node.right == null)
+                return (node.val, depth);
+
+            (int val, int depth) tuple = (0,0);
+            if (node.left != null)
+                tuple = FindBottomLeftValueHelper(node.left, depth + 1);
+
+            if (node.right != null)
+            {
+                var v = FindBottomLeftValueHelper(node.right, depth + 1);
+                if (v.depth > tuple.depth)
+                    tuple = v;
+            }
+
+            return tuple;
+        }
+    }
+}

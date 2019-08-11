2b66608371e85fdf55d787ef55e395f01639b1b8
davit dvali <dvali.daviti@gmail.com>
2019-08-11 08:07:35 +0400

    Day of the Year

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Weekly Contests/Weekly Contest 149.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Day of the Year (Mine)
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static int DayOfYear(string date)
+        {
+            var arr = date.Split('-');
+            var dateTime = new DateTime(int.Parse(arr[0]), int.Parse(arr[1]), int.Parse(arr[2]));
+            return dateTime.DayOfYear;
+        }
+    }
+}

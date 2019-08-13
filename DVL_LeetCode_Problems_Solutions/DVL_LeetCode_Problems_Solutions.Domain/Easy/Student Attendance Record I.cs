c97c04c0f4218622ce3141623bb8f687d0050afc
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-13 09:35:23 +0400

    Student Attendance Record I

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Student Attendance Record I.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        ///  Student Attendance Record I (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool CheckRecord(string s)
+        {
+            if (s.Count(ch => ch == 'A') > 1)
+                return false;
+            int count = 0;
+            for(int i=0;i<s.Length;i++)
+                if (s[i] == 'L')
+                {
+                    count++;
+                    if (count > 2)
+                        return false;
+                }
+                else
+                    count = 0;
+
+            return true;
+        }
+    }
+}

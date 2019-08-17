474d2d2a565d5a9cff5d6d4a972838716aa248b3
davit dvali <dvali.daviti@gmail.com>
2019-08-17 12:55:54 +0300

    Longest Turbulent Subarray

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Longest Turbulent Subarray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Longest Turbulent Subarray (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int MaxTurbulenceSize(int[] A)
+        {
+            int maxLength = 0, currLength = 0;
+            bool? prevWasGreater = null;
+            for (int i = 1; i < A.Length; i++)
+            {
+                bool? currIsGreater = A[i] == A[i - 1] ? (bool?)null : A[i] > A[i - 1];
+
+                if (currIsGreater != prevWasGreater && currIsGreater != null)
+                    currLength++;
+                else
+                {
+                    maxLength = Math.Max(maxLength, currLength);
+                    currLength = currIsGreater == null ? 0 : 1;
+                }
+                prevWasGreater = currIsGreater;
+            }
+
+            maxLength = Math.Max(maxLength, currLength);
+
+            return maxLength + 1;
+        }
+    }
+}

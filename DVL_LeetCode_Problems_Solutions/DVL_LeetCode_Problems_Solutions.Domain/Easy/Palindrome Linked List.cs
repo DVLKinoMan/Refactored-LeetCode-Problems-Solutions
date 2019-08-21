249aef817c18b114397764c7736bd0db54860cd0
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-21 08:05:04 +0400

    Palindrome Linked List

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Palindrome Linked List.cs
@@ -0,0 +1,33 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Palindrome Linked List (Mine)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static bool IsPalindrome(ListNode head)
+        {
+            if (head == null)
+                return false;
+
+            return IsPalindromeHelper(ref head, head);
+        }
+
+        private static bool IsPalindromeHelper(ref ListNode fNode, ListNode currNode)
+        {
+            if (currNode.next != null && !IsPalindromeHelper(ref fNode, currNode.next))
+                return false;
+
+            if (fNode.val != currNode.val)
+                return false;
+
+            fNode = fNode.next;
+
+            return true;
+        }
+    }
+}

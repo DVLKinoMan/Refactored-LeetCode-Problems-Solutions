c0761e395222db3a29232fdfb9a74fa59f68ebb9
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-22 08:01:23 +0400

    Excel Sheet Column Number

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Excel Sheet Column Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Excel Sheet Column Number (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int TitleToNumber(string s)
+        {
+            int res = 0, mul = (int)Math.Pow(26, s.Length - 1);
+            for (int i = 0; i < s.Length; i++)
+            {
+                res += (s[i] - 'A' + 1) * mul;
+                mul /= 26;
+            }
+
+            return res;
+        }
+    }
+}

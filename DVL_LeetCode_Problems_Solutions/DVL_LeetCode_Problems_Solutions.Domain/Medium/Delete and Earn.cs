945e3977f4da5e8d6dab6a0afbf9bc1b09499e07
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-23 08:11:41 +0400

    Delete and Earn

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Delete and Earn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Delete and Earn (Not Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int DeleteAndEarn(int[] nums)
+        {
+            int n = 10001;
+            int[] values = new int[n];
+            foreach (var num in nums)
+                values[num] += num;
+
+            int take = 0, skip = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int takei = skip + values[i];
+                int skipi = Math.Max(skip, take);
+                take = takei;
+                skip = skipi;
+            }
+
+            return Math.Max(take, skip);
+        }
+    }
+}

6f1609ee3d7612c4e8456bef11592b25b4a2034d
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-24 16:33:55 +0400

    Intersection of Two Arrays

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Intersection of Two Arrays.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        ///  Intersection of Two Arrays (Mine)
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="nums2"></param>
+        /// <returns></returns>
+        public static int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var set = new HashSet<int>(nums1);
+            return nums2.Where(n => set.Contains(n)).Distinct().ToArray();
+        }
+    }
+}

f3258d9e9770f6f47829951d1713e25422440cc4
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-26 07:53:11 +0400

    Repeated Substring Pattern

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Repeated Substring Pattern.cs
@@ -0,0 +1,27 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Repeated Substring Pattern (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool RepeatedSubstringPattern(string s)
+        {
+            for (int len = 1; len <= s.Length / 2; len++)
+            {
+                if (s.Length % len != 0)
+                    continue;
+                string str = s.Substring(0, len);
+                int i = len;
+                while (i < s.Length && s.Substring(i, len) == str)
+                    i += len;
+                if (i >= s.Length)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}

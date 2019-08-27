6e4b29c5ff1bc019b42feba828cb37ba860933a7
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-27 08:10:35 +0400

    Sum Root to Leaf Numbers

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Sum Root to Leaf Numbers.cs
@@ -0,0 +1,29 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Sum Root to Leaf Numbers (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int SumNumbers(TreeNode root, string s = "")
+        {
+            if (root == null)
+                return 0;
+            if (root.left == null && root.right == null)
+                return int.Parse(s + root.val);
+
+            int sum = 0;
+            if (root.left != null)
+                sum += SumNumbers(root.left, s + root.val);
+            if (root.right != null)
+                sum += SumNumbers(root.right, s + root.val);
+
+            return sum;
+        }
+    }
+}

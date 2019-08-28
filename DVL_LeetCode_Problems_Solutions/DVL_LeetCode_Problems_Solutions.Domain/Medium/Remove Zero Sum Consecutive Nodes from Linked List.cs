4547694ccb217b056c242e35d7bed3e81f978f05
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-28 09:00:07 +0400

    Remove Zero Sum Consecutive Nodes from Linked List

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Remove Zero Sum Consecutive Nodes from Linked List.cs
@@ -0,0 +1,49 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        ///  Remove Zero Sum Consecutive Nodes from Linked List (Mine)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static ListNode RemoveZeroSumSublists(ListNode head)
+        {
+            while (true)
+            {
+                var dic = new Dictionary<int, ListNode>();
+                var currNode = head;
+                int sum = 0;
+                bool wasRemovedAnything = false;
+                while (currNode != null)
+                {
+                    sum += currNode.val;
+
+                    if (sum == 0)
+                    {
+                        head = currNode.next;
+                        wasRemovedAnything = true;
+                    }
+
+                    if (dic.ContainsKey(sum))
+                    {
+                        dic[sum].next = currNode.next;
+                        wasRemovedAnything = true;
+                    }
+                    else if (sum != 0)
+                        dic.Add(sum, currNode);
+
+                    currNode = currNode.next;
+                }
+
+                if (wasRemovedAnything == false)
+                    break;
+            }
+
+            return head;
+        }
+    }
+}

37992696155647cb13260078c4f37f39e11acd97
DVL_KinoMan <dvali.daviti@gmail.com>
2019-08-31 10:06:49 +0400

    Construct Binary Tree from Preorder and Postorder Traversal

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Construct Binary Tree from Preorder and Postorder Traversal.cs
@@ -0,0 +1,37 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Construct Binary Tree from Preorder and Postorder Traversal (Not Mine)
+        /// </summary>
+        /// <param name="pre"></param>
+        /// <param name="post"></param>
+        /// <returns></returns>
+        public static TreeNode ConstructFromPrePost(int[] pre, int[] post)
+        {
+            var head = new TreeNode(pre[0]);
+            var stack = new Stack<TreeNode>();
+            stack.Push(head);
+            for (int i = 1, j = 0; i < pre.Length; i++)
+            {
+                var node = new TreeNode(pre[i]);
+                while (stack.Peek().val == post[j])
+                {
+                    stack.Pop();
+                    j++;
+                }
+
+                if (stack.Peek().left == null)
+                    stack.Peek().left = node;
+                else stack.Peek().right = node;
+                stack.Push(node);
+            }
+
+            return head;
+        }
+    }
+}

3f92102d76399a890cf4b3e00e9c2c14f437f35a
D.Dvali <dvali.daviti@gmail.com>
2019-09-02 13:26:28 +0400

    Design Hashset

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Design HashSet.cs
@@ -0,0 +1,32 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    /// <summary>
+    /// Design Hashset (Mine)
+    /// </summary>
+    public class MyHashSet
+    {
+        int[] structure;
+        /** Initialize your data structure here. */
+        public MyHashSet()
+        {
+            structure = new int[1000001];
+        }
+
+        public void Add(int key)
+        {
+            structure[key] = 1;
+        }
+
+        public void Remove(int key)
+        {
+            structure[key] = 0;
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            return structure[key] != 0;
+        }
+    }
+
+}

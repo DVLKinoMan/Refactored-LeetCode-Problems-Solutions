e22ddc070176668b4aee0cf851aab0005b421615
D.Dvali <dvali.daviti@gmail.com>
2019-09-02 13:39:45 +0400

    Binary Tree Preorder Traversal

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Binary Tree Preorder Traversal.cs
@@ -0,0 +1,34 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        ///  Binary Tree Preorder Traversal (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            if (root != null)
+                list = PreorderTraversalHelper(root).ToList();
+
+            return list;
+        }
+
+        private static IEnumerable<int> PreorderTraversalHelper(TreeNode node)
+        {
+            yield return node.val;
+            if (node.left != null)
+                foreach (var nod in PreorderTraversalHelper(node.left))
+                    yield return nod;
+            if (node.right != null)
+                foreach (var nod in PreorderTraversalHelper(node.right))
+                    yield return nod;
+        }
+    }
+}

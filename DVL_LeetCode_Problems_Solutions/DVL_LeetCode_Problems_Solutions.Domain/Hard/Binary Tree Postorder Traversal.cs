1e428b9a5679c17ce8e4d4c09ead5da2644bc812
D.Dvali <dvali.daviti@gmail.com>
2019-09-02 17:26:20 +0400

    Binary Tree Inorder and Postorder Traversals

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Binary Tree Postorder Traversal.cs
@@ -0,0 +1,34 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Hard
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Binary Tree Postorder Traversal (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            if (root != null)
+                list = PostorderTraversalHelper(root).ToList();
+
+            return list;
+        }
+
+        private static IEnumerable<int> PostorderTraversalHelper(TreeNode node)
+        {
+            if (node.left != null)
+                foreach (var nod in PostorderTraversalHelper(node.left))
+                    yield return nod;
+            if (node.right != null)
+                foreach (var nod in PostorderTraversalHelper(node.right))
+                    yield return nod;
+            yield return node.val;
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Binary Tree Inorder Traversal.cs
@@ -0,0 +1,34 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Binary Tree Inorder Traversal (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            if (root != null)
+                list = InorderTraversalHelper(root).ToList();
+
+            return list;
+        }
+
+        private static IEnumerable<int> InorderTraversalHelper(TreeNode node)
+        {
+            if (node.left != null)
+                foreach (var nod in InorderTraversalHelper(node.left))
+                    yield return nod;
+            yield return node.val;
+            if (node.right != null)
+                foreach (var nod in InorderTraversalHelper(node.right))
+                    yield return nod;
+        }
+    }
+}

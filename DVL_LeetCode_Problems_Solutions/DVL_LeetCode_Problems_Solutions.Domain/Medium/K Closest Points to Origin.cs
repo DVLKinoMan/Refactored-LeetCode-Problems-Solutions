d08ebffc85e15699a1a3042bc15f17955cb2483f
D.Dvali <dvali.daviti@gmail.com>
2019-09-03 17:14:06 +0400

    K Closest Points to Origin

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/K Closest Points to Origin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// K Closest Points to Origin (Mine)
+        /// </summary>
+        /// <param name="points"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static int[][] KClosest(int[][] points, int K)
+        {
+            double distance(int[] point) => Math.Sqrt(Math.Pow(point[0], 2) + Math.Pow(point[1], 2));
+            return points.OrderBy(arr => distance(arr)).Take(K).ToArray();
+        }
+    }
+}

04381295f9e8366e379de86ee7a151d2acaa2494
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-04 07:55:17 +0400

    Sum of Square Numbers

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Sum of Square Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Sum of Square Numbers (Not Mine)
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool JudgeSquareSum(int c)
+        {
+            var set = new HashSet<int>();
+
+            for (int i = 0; i <= Math.Sqrt(c); i++)
+            {
+                set.Add(i * i);
+                if (set.Contains(c - i * i))
+                    return true;
+            }
+            return false;
+        }
+    }
+}

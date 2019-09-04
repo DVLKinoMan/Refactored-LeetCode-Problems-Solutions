76fdf41cd5bd3a028585b214b1f0992cedc2d06b
D.Dvali <dvali.daviti@gmail.com>
2019-09-04 17:14:08 +0400

    todo problem

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/Increasing Order Search Tree Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestClass]
+    public class Increasing_Order_Search_Tree_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var node = IncreasingBST(
+                new TreeNode(1)
+                {
+                    left = new TreeNode(2)
+                    {
+                        left = new TreeNode(3),
+                        right = new TreeNode(4)
+                    },
+                    right = new TreeNode(5)
+                }
+                );
+
+            Assert.AreEqual(1, -1);
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Increasing Order Search Tree.cs
@@ -0,0 +1,30 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        //todo???
+        public static TreeNode IncreasingBST(TreeNode root, TreeNode parent = null)
+        {
+            TreeNode node;
+            if (root.left != null)
+                node = IncreasingBST(root.left, root);
+            else
+                node = root;
+
+            if (root.right == null)
+                root.right = parent ?? root.right;
+            else root.right = IncreasingBST(root.right);
+
+            root.left = null;
+
+            return node;
+        }
+    }
+}

71e78ec85b2076cf287cabfe497ab95927df3842
D.Dvali <dvali.daviti@gmail.com>
2019-09-05 14:37:44 +0400

    Sort Array By Parity

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Sort Array By Parity.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Sort Array By Parity (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int[] SortArrayByParity(int[] A)
+        {
+            return A.Where(num => num % 2 == 0).Concat(A.Where(num => num % 2 == 1)).ToArray();
+        }
+    }
+}

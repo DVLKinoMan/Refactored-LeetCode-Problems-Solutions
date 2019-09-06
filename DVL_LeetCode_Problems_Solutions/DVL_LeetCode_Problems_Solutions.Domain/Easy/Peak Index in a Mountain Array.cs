12552ec182242ef53b15c25a87fa2156eb01ece8
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-06 07:58:12 +0400

    Peak Index in a Mountain Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Peak Index in a Mountain Array.cs
@@ -0,0 +1,19 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Peak Index in a Mountain Array (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int PeakIndexInMountainArray(int[] A)
+        {
+            int i = 1;
+            while (A[i] > A[i - 1])
+                i++;
+
+            return i - 1;
+        }
+    }
+}

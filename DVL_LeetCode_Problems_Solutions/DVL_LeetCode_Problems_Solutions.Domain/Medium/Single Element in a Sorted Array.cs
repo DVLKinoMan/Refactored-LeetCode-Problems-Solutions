0b4755e4dfd8a39ce7751dcbc9ee83ccf9145f43
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-10 07:53:26 +0400

    Single Element in a Sorted Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Single Element in a Sorted Array.cs
@@ -0,0 +1,19 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Single Element in a Sorted Array (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int SingleNonDuplicate(int[] nums)
+        {
+            int xor = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+                xor ^= nums[i];
+
+            return xor;
+        }
+    }
+}

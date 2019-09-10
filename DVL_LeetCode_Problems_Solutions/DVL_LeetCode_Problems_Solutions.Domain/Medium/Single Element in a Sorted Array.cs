eb591dce998f8e89a28924482e21af418e534f3e
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-10 08:01:45 +0400

    Single Element in a Sorted Array (Second Solution)

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Single Element in a Sorted Array.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Single Element in a Sorted Array.cs
@@ -15,5 +15,19 @@
 
             return xor;
         }
+
+        /// <summary>
+        /// Single Element in a Sorted Array (Mine - better)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int SingleNonDuplicate2(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i += 2)
+                if (nums[i] != nums[i - 1])
+                    return i == 1 ? nums[i - 1] : nums[i];
+
+            return nums[nums.Length - 1];
+        }
     }
 }

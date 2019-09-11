14109a2f8904495c7b6b077b49dbdb57dc4873e7
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-11 08:03:46 +0400

    Binary Prefix Divisible By 5 (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Binary Prefix Divisible By 5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        public static IList<bool> PrefixesDivBy5(int[] A)
+        {
+            var str = new StringBuilder();
+            bool[] answers = new bool[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                str.Append(A[i]);
+                answers[i] = int.Parse(Convert.ToString(int.Parse(str.ToString()), 10)) % 5 == 0;
+            }
+
+            return answers;
+        }
+    }
+}

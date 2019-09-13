7402aff954110207bad5d98ce7135814110fd6f7
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-13 08:13:57 +0400

    Minimum Index Sum of Two Lists

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Minimum Index Sum of Two Lists.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Minimum Index Sum of Two Lists (Mine)
+        /// </summary>
+        /// <param name="list1"></param>
+        /// <param name="list2"></param>
+        /// <returns></returns>
+        public static string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            var list = new List<string>();
+            int sum = int.MaxValue;
+            for (int i = 0; i < list1.Length; i++)
+            for (int j = 0; j < list2.Length; j++)
+                if (list1[i] == list2[j])
+                {
+                    if (i + j < sum)
+                    {
+                        list = new List<string>();
+                        list.Add(list1[i]);
+                        sum = i + j;
+                    }
+                    else if (i + j == sum)
+                        list.Add(list1[i]);
+                }
+
+            return list.ToArray();
+        }
+    }
+}

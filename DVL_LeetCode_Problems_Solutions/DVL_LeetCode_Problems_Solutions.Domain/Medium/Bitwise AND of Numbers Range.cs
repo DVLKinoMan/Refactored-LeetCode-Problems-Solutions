1c1fe93bac786eaa4e3113290fd3756ddffe0180
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-16 07:56:05 +0400

    Bitwise AND of Numbers Range

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Bitwise AND of Numbers Range.cs
@@ -0,0 +1,26 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Bitwise AND of Numbers Range (Not Mine)
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int RangeBitwiseAnd(int m, int n)
+        {
+            if (m == 0)
+                return 0;
+
+            int moveFactor = 1;
+            while (m != n)
+            {
+                m >>= 1;
+                n >>= 1;
+                moveFactor <<= 1;
+            }
+            return m * moveFactor;
+        }
+    }
+}

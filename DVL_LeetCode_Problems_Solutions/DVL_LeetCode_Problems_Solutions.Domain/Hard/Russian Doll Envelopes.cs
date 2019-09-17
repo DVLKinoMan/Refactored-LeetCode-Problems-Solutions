2d264e253af47df120055ae66e21e4d91d20cf9a
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-17 07:59:57 +0400

    Russian Doll Envelope (Not Solved)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Russian Doll Envelopes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static int MaxEnvelopes(int[][] envelopes)
+        {
+            envelopes.OrderBy(env => env[0])
+        }
+    }
+}

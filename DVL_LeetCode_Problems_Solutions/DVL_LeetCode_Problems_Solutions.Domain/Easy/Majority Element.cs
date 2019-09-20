9f538cd9a1517ca1e4f2f088979fe43a3eb799f2
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-20 07:54:26 +0400

    Majority Element

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Majority Element.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Majority Element (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MajorityElement(int[] nums)
+        {
+            var dic = new Dictionary<int,int>();
+            int n = nums.Length / 2;
+            foreach (var num in nums)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    if (dic[num] > n)
+                        return num;
+                    dic[num]++;
+                }
+                else dic.Add(num, 1);
+            }
+
+            int maxValue = dic.Max(d => d.Value);
+            return dic.FirstOrDefault(d => d.Value == maxValue).Key;
+        }
+    }
+}

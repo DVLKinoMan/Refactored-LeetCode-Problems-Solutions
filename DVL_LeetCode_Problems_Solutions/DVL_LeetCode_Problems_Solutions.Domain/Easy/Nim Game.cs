6c9dfa584d6ac23318a3cb9c8923422c07632480
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-27 08:08:38 +0400

    Nim Game (Time Limit)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Nim Game.cs
@@ -0,0 +1,22 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Nim Game (Time Limit)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="isYourTurn"></param>
+        /// <returns></returns>
+        public static bool CanWinNim(int n, bool isYourTurn = true)
+        {
+            if (n <= 3)
+                return isYourTurn;
+
+            if (!isYourTurn)
+                return CanWinNim(n - 1, true) && CanWinNim(n - 2, true) && CanWinNim(n - 3, true);
+
+            return CanWinNim(n - 1, false) || CanWinNim(n - 2, false) || CanWinNim(n - 3, false);
+        }
+    }
+}

7dc18e94e713f2ed868ccf4f8865e15d624dd478
D.Dvali <dvali.daviti@gmail.com>
2019-09-27 13:24:47 +0400

    Nin Game

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Nim Game.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Nim Game.cs
@@ -3,20 +3,11 @@
     partial class ProblemSolver
     {
         /// <summary>
-        /// Nim Game (Time Limit)
+        /// Nim Game (Not Mine)
         /// </summary>
         /// <param name="n"></param>
         /// <param name="isYourTurn"></param>
         /// <returns></returns>
-        public static bool CanWinNim(int n, bool isYourTurn = true)
-        {
-            if (n <= 3)
-                return isYourTurn;
-
-            if (!isYourTurn)
-                return CanWinNim(n - 1, true) && CanWinNim(n - 2, true) && CanWinNim(n - 3, true);
-
-            return CanWinNim(n - 1, false) || CanWinNim(n - 2, false) || CanWinNim(n - 3, false);
-        }
+        public static bool CanWinNim(int n) => n % 4 != 0;
     }
 }

346cfd217d2d96ada6d8de251c81f5c2a97d6052
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-29 07:48:56 +0400

    Weekly Contest 156

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Weekly Contests/Weekly Contest 156.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Weekly_Contests
+{
+    partial class ProblemSolver
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            var dic = new Dictionary<int, int>();
+
+            foreach (var num in arr)
+            {
+                if (dic.ContainsKey(num))
+                    dic[num]++;
+                else dic.Add(num, 1);
+            }
+
+            return dic.Values.Distinct().Count() == dic.Values.Count;
+        }
+    }
+}

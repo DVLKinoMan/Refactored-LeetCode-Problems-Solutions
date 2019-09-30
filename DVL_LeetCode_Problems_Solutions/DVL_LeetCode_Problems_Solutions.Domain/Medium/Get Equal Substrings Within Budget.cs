8f3c2a70bc76d9a66606601926cb33c81c706e10
DVL_KinoMan <dvali.daviti@gmail.com>
2019-09-30 08:11:29 +0400

    Get Equal Substrings Within Budget (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Get Equal Substrings Within Budget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static int EqualSubstring(string s, string t, int maxCost)
+        {
+            int[] costs = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+                costs[i] = Math.Abs(s[i] - t[i]);
+
+        }
+    }
+}

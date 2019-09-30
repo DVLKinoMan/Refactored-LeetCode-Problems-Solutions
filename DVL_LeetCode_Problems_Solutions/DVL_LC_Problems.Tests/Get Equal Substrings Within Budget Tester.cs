ca2c2c5afb6c8148ac0d82b584f3b0cd1d8f8988
D.Dvali <dvali.daviti@gmail.com>
2019-09-30 11:33:26 +0400

    Get Equal Substrings Within Budget

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/Get Equal Substrings Within Budget Tester.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestClass]
+    public class Get_Equal_Substrings_Within_Budget_Tester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            EqualSubstring("thjdoffka",
+"qhrnlntls",
+11);
+        }
+    }
+}
--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Get Equal Substrings Within Budget.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Get Equal Substrings Within Budget.cs
@@ -1,19 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
+        /// <summary>
+        /// Get Equal Substrings Within Budget (Not Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="maxCost"></param>
+        /// <returns></returns>
         public static int EqualSubstring(string s, string t, int maxCost)
         {
-            int[] costs = new int[s.Length];
-            for (int i = 0; i < s.Length; i++)
-                costs[i] = Math.Abs(s[i] - t[i]);
-
+            int n = s.Length, i = 0, j;
+            for (j = 0; j < n; ++j)
+            {
+                maxCost -= Math.Abs(s[j] - t[j]);
+                if (maxCost < 0)
+                {
+                    maxCost += Math.Abs(s[i] - t[i]);
+                    ++i;
+                }
+            }
+            return j - i;
         }
     }
 }

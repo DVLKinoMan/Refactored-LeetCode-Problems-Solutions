5f2eefaf86610300b23005d73b199842574fe328
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-01 08:04:20 +0400

    Remove All Adjacent Duplicates in String II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Remove All Adjacent Duplicates in String II.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Remove All Adjacent Duplicates in String II (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static string RemoveDuplicates(string s, int k)
+        {
+            var str = new StringBuilder(s);
+            var stack = new Stack<(char ch, int count)>();
+
+            foreach (var ch in s)
+            {
+                str.Append(ch);
+                if (stack.Count != 0 && stack.Peek().ch == ch)
+                {
+                    if (stack.Peek().count == k - 1)
+                    {
+                        str.Remove(str.Length - k, k);
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        var tup = stack.Pop();
+                        stack.Push((ch, tup.count + 1));
+                    }
+                }
+                else stack.Push((ch, 1));
+            }
+
+            return str.ToString();
+        }
+    }
+}

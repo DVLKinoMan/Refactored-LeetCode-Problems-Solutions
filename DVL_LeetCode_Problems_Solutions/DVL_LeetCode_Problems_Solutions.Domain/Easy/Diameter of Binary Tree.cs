e803eec0c168d6cf75470cdff6a366832dc2b7ee
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-04 08:23:41 +0400

    DiameterOfBinaryTree (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Diameter of Binary Tree.cs
@@ -0,0 +1,25 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static int DiameterOfBinaryTree(TreeNode root)
+        {
+            return MaxHeight(root.left) + MaxHeight(root.right);
+
+            int MaxHeight(TreeNode node)
+            {
+                if (node == null)
+                    return 0;
+
+                return Math.Max(MaxHeight(node.left) + 1, MaxHeight(node.right) + 1);
+            }
+        }
+    }
+}

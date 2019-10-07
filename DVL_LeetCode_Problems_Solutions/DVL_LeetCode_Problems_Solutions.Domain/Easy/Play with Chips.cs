77aca1b788b4545b50d3f8a644bb2a4864ee5b27
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-07 07:59:04 +0400

    Play with Chips

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Play with Chips.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Play with Chips (Not Mine)
+        /// </summary>
+        /// <param name="chips"></param>
+        /// <returns></returns>
+        public static int MinCostToMoveChips(int[] chips)
+        {
+            int odd = 0, even = 0;
+            foreach (int a in chips)
+                if (a % 2 == 0)
+                    ++even;
+                else
+                    ++odd;
+
+            return Math.Min(odd, even);
+        }
+    }
+}

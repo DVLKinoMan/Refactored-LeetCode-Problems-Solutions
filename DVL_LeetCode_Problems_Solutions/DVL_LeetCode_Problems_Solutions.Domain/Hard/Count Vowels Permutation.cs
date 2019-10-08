9b27f20472bfbd874bfd624ffe6b28eb258510e4
D.Dvali <dvali.daviti@gmail.com>
2019-10-08 10:11:46 +0400

    Count Vowels Permutation

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Count Vowels Permutation.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Count Vowels Permutation.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
+        /// <summary>
+        /// Count Vowels Permutation (Mine with hints)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
         public static int CountVowelPermutation(int n)
         {
             int mod = (int)Math.Pow(10, 9) + 7;
@@ -19,14 +20,16 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
 
             for (int i = 1; i < n; i++)
             {
-                dp[i, 0] = (dp[i - 1, 1] + dp[i - 1, 2] + dp[i - 1, 4])%mod;
-                dp[i, 1] = (dp[i - 1, 0] + dp[i - 1, 2]) % mod;
-                dp[i, 2] = (dp[i - 1, 1] + dp[i - 1, 3]) % mod;
-                dp[i, 3] = (dp[i - 1, 2])% mod;
-                dp[i, 4] = (dp[i - 1, 2] + dp[i - 1, 3]) % mod;
+                dp[i, 0] = ModFunc(ModFunc(dp[i - 1, 1], dp[i - 1, 2]), dp[i - 1, 4]);
+                dp[i, 1] = ModFunc(dp[i - 1, 0], dp[i - 1, 2]);
+                dp[i, 2] = ModFunc(dp[i - 1, 1], dp[i - 1, 3]);
+                dp[i, 3] = dp[i - 1, 2];
+                dp[i, 4] = ModFunc(dp[i - 1, 2], dp[i - 1, 3]);
             }
 
-            return (dp[n - 1, 0] + dp[n - 1, 1] + dp[n - 1, 2] + dp[n - 1, 3] + dp[n - 1, 4]) % mod;
+            return ModFunc(ModFunc(ModFunc(dp[n - 1, 0], dp[n - 1, 1]),  ModFunc(dp[n - 1, 2], dp[n - 1, 3])), dp[n - 1, 4]);
+
+            int ModFunc(int a, int b) => (a + b) % mod;
         }
     }
 }

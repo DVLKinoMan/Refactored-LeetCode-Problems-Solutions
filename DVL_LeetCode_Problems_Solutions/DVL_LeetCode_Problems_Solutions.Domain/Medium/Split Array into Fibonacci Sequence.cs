98304c8deccb578de2436de6e715873eb7fff871
D.Dvali <dvali.daviti@gmail.com>
2019-10-09 11:06:38 +0400

    Split Array into Fibonacci Sequence

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Split Array into Fibonacci Sequence.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Split Array into Fibonacci Sequence.cs
@@ -1,33 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
         /// <summary>
-        /// Split Array into Fibonacci Sequence (Not Working)
+        /// Split Array into Fibonacci Sequence (Mine)
         /// </summary>
         /// <param name="S"></param>
         /// <returns></returns>
         public static IList<int> SplitIntoFibonacci(string S)
         {
             var list = new List<int>();
-            for (int i = 0; i < S.Length; i++)
-            for (int len = 1; len <= S.Length - i - 2; len++)
+            for (int len = 1; len <= S.Length - 2; len++)
             {
-                if (int.TryParse(S.Substring(i, len), out int num))
+                string str = S.Substring(0, len);
+                if ((str=="0" || str[0]!='0') && int.TryParse(str, out int num))
                 {
                     list.Add(num);
                     for (int len2 = 1; len2 <= S.Length - len - 1; len2++)
                     {
-                        if (int.TryParse(S.Substring(i + len, len2), out int num2))
+                        string str2 = S.Substring(len, len2);
+                        if ((str2=="0" || str2[0]!='0') && int.TryParse(str2, out int num2))
                         {
                             list.Add(num2);
-                            if (IsFibonnaciSequence(num, num2, i + len + len2))
+                            if (IsFibonnaciSequence(num, num2, len + len2))
                                 return list;
                             list.RemoveAt(list.Count - 1);
                         }
@@ -44,11 +41,15 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
             bool IsFibonnaciSequence(int num1, int num2, int index)
             {
                 if (index >= S.Length)
-                    return true;
+                    return list.Count >= 3;
 
                 int num3 = num1 + num2;
                 string str = num3.ToString();
-                if(S.Substring(index, str.Length) != str)
+
+                if ((str.Length != 1 && str[0] == '0') || index + str.Length > S.Length)
+                    return false;
+
+                if (S.Substring(index, str.Length) != str)
                     return false;
 
                 list.Add(num3);

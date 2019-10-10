5b94c1c7b6ac8e4222bd1ed95b009c7c98ac8990
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-10 08:12:53 +0400

    Longest Mountain in Array (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Longest Mountain in Array.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Longest Mountain in Array (Not Working)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int LongestMountain(int[] A)
+        {
+            int maxCount = 0, currCount = 0;
+            bool isMountain = false;
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (currCount > 0)
+                {
+                    if (A[i] < A[i - 1])
+                    {
+                        currCount++;
+                        isMountain = true;
+                    }
+                    else if (A[i] > A[i - 1])
+                    {
+                        if (isMountain)
+                        {
+                            maxCount = Math.Max(maxCount, currCount + 1);
+                            isMountain = false;
+                        }
+
+                        currCount = 1;
+                    }
+                    else
+                    {
+                        if (isMountain)
+                        {
+                            maxCount = Math.Max(maxCount, currCount + 1);
+                            isMountain = false;
+                        }
+
+                        currCount = 0;
+                    }
+                }
+                else
+                {
+                    if (A[i] > A[i - 1])
+                        currCount++;
+                }
+            }
+
+            if (isMountain)
+                maxCount = Math.Max(maxCount, currCount + 1);
+
+            return maxCount;
+        }
+    }
+}

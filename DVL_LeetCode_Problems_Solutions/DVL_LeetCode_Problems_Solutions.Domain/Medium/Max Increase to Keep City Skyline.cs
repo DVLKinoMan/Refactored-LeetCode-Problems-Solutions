51c0c3b61a6f8bc19bfcd9321de91ebe5cde35d5
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-12 08:44:11 +0400

    Max Increase to Keep City Skyline

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Max Increase to Keep City Skyline.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Max Increase to Keep City Skyline (Mine)
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public static int MaxIncreaseKeepingSkyline(int[][] grid)
+        {
+            int m = grid.Length, n = grid[0].Length;
+            int[] topOrBottom = grid.SelectMany(g => g.Select((el, j) => (el, j)))
+                                    .GroupBy(t => t.j)
+                                    .Select(gr => gr.Select(g=>g.el).Max())
+                                    .ToArray();
+            int[] leftOrRight = grid.Select(row => row.Max()).ToArray();
+
+            int count = 0;
+            for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                count += Math.Min(topOrBottom[i], leftOrRight[j]) - grid[i][j];
+
+            return count;
+        }
+    }
+}

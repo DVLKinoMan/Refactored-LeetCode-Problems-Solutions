f3c3b007c853383ea56d7081903fe8ea59310931
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-17 08:04:55 +0400

    Binary Search

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Binary Search.cs
@@ -0,0 +1,27 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Binary Search (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int Search(int[] nums, int target)
+        {
+            int st = 0, end = nums.Length - 1;
+            while (st < end)
+            {
+                int index = (st + end) / 2;
+                if (nums[index] == target)
+                    return index;
+                if (nums[index] > target)
+                    end = index - 1;
+                else st = index + 1;
+            }
+
+            return -1;
+        }
+    }
+}

edc3313b04af8a9c45b75190a92de4099c8978a6
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-25 08:15:01 +0400

    Sort Array By Parity II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Sort Array By Parity II.cs
@@ -0,0 +1,31 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Sort Array By Parity II (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int[] SortArrayByParityII(int[] A)
+        {
+            int evenIndex = 0, oddIndex = 1;
+            int[] res = new int[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    res[evenIndex] = A[i];
+                    evenIndex += 2;
+                }
+                else
+                {
+                    res[oddIndex] = A[i];
+                    oddIndex += 2;
+                }
+            }
+
+            return res;
+        }
+    }
+}

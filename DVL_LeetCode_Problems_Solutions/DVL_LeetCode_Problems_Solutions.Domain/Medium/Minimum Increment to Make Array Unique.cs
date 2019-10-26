42bad8e567a78c52495fd78da3b6f83925ca56f7
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-26 11:31:57 +0400

    Minimum Increment to Make Array Unique

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Minimum Increment to Make Array Unique.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Minimum Increment to Make Array Unique (Not Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int MinIncrementForUnique(int[] A)
+        {
+            int count = 0, need = 0;
+            Array.Sort(A);
+            foreach (var a in A)
+            {
+                count += Math.Max(need - a, 0);
+                need = Math.Max(need, a) + 1;
+            }
+
+            return count;
+        }
+    }
+}

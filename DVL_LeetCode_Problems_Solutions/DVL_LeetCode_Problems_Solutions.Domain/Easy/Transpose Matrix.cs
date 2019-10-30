334e26d48a13a0efeed2317f50b21e0161a2327e
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-30 08:09:12 +0400

    Transpose Matrix

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Transpose Matrix.cs
@@ -0,0 +1,24 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Transpose Matrix (Not Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int[][] Transpose(int[][] A)
+        {
+            int M = A.Length, N = A[0].Length;
+            int[][] B = new int[N][];
+            for (int j = 0; j < N; j++)
+            {
+                B[j] = new int[M];
+                for (int i = 0; i < M; i++)
+                    B[j][i] = A[i][j];
+            }
+
+            return B;
+        }
+    }
+}

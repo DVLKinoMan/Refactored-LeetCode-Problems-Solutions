97d85ed8e5179ba225520f2c4fbbc0b8e0c47eab
DVL_KinoMan <dvali.daviti@gmail.com>
2019-10-31 08:04:05 +0400

    Mirror Reflaction

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Mirror Reflection.cs
@@ -0,0 +1,26 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Mirror Reflaction (Not Mine)
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public static int MirrorReflection(int p, int q)
+        {
+            while (p % 2 == 0 && q % 2 == 0)
+            {
+                p /= 2;
+                q /= 2;
+            }
+
+            if (p % 2 == 0)
+                return 2;
+            else if (q % 2 == 0)
+                return 0;
+            else return 1;
+        }
+    }
+}

b6bcccfe93cc3c007736d17ceb7be61784a8deaa
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-01 08:04:34 +0400

    Binary Gap

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Binary Gap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Binary Gap (Mine)
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public static int BinaryGap(int N)
+        {
+            string str = System.Convert.ToString(N, 2);
+            int prevIndex = -1, max = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '1')
+                {
+                    if (prevIndex != -1)
+                    {
+                        max = Math.Max(max, i - prevIndex);
+                    }
+
+                    prevIndex = i;
+                }
+            }
+
+            return max;
+        }
+    }
+}

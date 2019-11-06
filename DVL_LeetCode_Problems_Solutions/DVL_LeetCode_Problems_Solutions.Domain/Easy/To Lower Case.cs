c7defe7d88fad56f24716041b189ffcf97da6da9
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-06 08:06:28 +0400

    To Lower Case

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/To Lower Case.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// To Lower Case (Mine)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToLowerCase(string str)
+        {
+            var res = new StringBuilder();
+            foreach (var ch in str)
+            {
+                if (char.IsUpper(ch))
+                {
+                    int diff = ch - 'A';
+                    res.Append((char)('a' + diff));
+                }
+                else res.Append(ch);
+            }
+
+            return res.ToString();
+        }
+    }
+}

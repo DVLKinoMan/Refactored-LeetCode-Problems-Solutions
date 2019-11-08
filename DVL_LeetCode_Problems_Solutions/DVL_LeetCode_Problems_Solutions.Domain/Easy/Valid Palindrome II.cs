24a63ed4078aee75f3e527d35b2d10b6254f36fd
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-08 08:15:38 +0400

    Valid Palindrome II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Valid Palindrome II.cs
@@ -0,0 +1,31 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Valid Palindrome II (Not Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool ValidPalindrome(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+                if (s[i] != s[j])
+                {
+                    int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
+                    while (i1 < j1 && s[i1] == s[j1])
+                    {
+                        i1++;
+                        j1--;
+                    };
+                    while (i2 < j2 && s[i2] == s[j2])
+                    {
+                        i2++;
+                        j2--;
+                    };
+                    return i1 >= j1 || i2 >= j2;
+                }
+            return true;
+        }
+    }
+}

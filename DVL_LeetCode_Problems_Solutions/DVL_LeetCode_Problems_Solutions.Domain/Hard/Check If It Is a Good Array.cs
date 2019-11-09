47cd2b8d84287f54fc08e8219c777cf1ed129fd4
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-09 13:01:16 +0400

    Check If It Is a Good Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Check If It Is a Good Array.cs
@@ -0,0 +1,27 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Check If It Is a Good Array (Not Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static bool IsGoodArray(int[] nums)
+        {
+            int x = nums[0], y;
+            foreach (var num in nums)
+            {
+                int a = num;
+                while (a > 0)
+                {
+                    y = x % a;
+                    x = a;
+                    a = y;
+                }
+            }
+
+            return x == 1;
+        }
+    }
+}

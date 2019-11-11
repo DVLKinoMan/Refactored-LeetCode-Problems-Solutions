94f64dc2726338e6a7e9cf1c7df80be554662d9e
D.Dvali <dvali.daviti@gmail.com>
2019-11-11 18:32:40 +0400

    Maximum Score Words Formed by Letters (Not Working)

--- DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/StoneGameIITester.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/StoneGameIITester.cs
@@ -18,6 +18,9 @@ namespace DVL_LC_Problems.Tests
             //CircularPermutation(2, 3);
             //MinRemoveToMakeValid("lee(t(c)o)de)");
             //ReconstructMatrix(9, 2, new int[] {0, 1, 2, 0, 0, 0, 0, 0, 2, 1, 2, 1, 2});
+            MaxScoreWords(new String[] {"dog", "cat", "dad", "good"}
+                , new char[] {'a', 'a', 'c', 'd', 'd', 'd', 'g', 'o', 'o'},
+                new int[] {1, 0, 9, 5, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
 
             Assert.AreEqual(10, d);
         }
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Maximum Score Words Formed by Letters.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Maximum Score Words Formed by Letters (Not Working)
+        /// </summary>
+        /// <param name="words"></param>
+        /// <param name="letters"></param>
+        /// <param name="score"></param>
+        /// <returns></returns>
+        public static int MaxScoreWords(string[] words, char[] letters, int[] score)
+        {
+            var dict = new Dictionary<char, int>();
+            foreach (var letter in letters)
+            {
+                if (dict.ContainsKey(letter))
+                    dict[letter]++;
+                else dict.Add(letter, 1);
+            }
+
+            var listWords = words.ToList();
+            var listOfScores = new List<int>();
+            for (int i = 0; i < listWords.Count; i++)
+            {
+                var (isValid, sc) = IsValidWord(listWords[i]);
+                if (!isValid)
+                {
+                    listWords.RemoveAt(i);
+                    i--;
+                }
+                else listOfScores.Add(sc);
+            }
+
+            int res = 0;
+
+            return MaxScore(0);
+
+            int MaxScore(int currScore)
+            {
+                for (int i = 0; i < listWords.Count; i++)
+                {
+                    string currWord = listWords[i];
+                    foreach (var ch in currWord)
+                    {
+                        if (dict[ch] == 0)
+                            return currScore;
+                        dict[ch]--;
+                    }
+                    listWords.RemoveAt(i);
+                    int prevScore = listOfScores[i];
+                    listOfScores.RemoveAt(i);
+                    res = Math.Max(res, MaxScore(currScore + prevScore));
+                    listWords.Insert(i, currWord);
+                    listOfScores.Insert(i, prevScore);
+                }
+
+                return res;
+            }
+
+            (bool isValid, int sc) IsValidWord(string word)
+            {
+                var dict2 = new Dictionary<char,int>();
+                foreach (var ch in word)
+                {
+                    if (dict2.ContainsKey(ch))
+                        dict2[ch]++;
+                    else dict2.Add(ch, 1);
+                }
+
+                int sum = 0;
+                foreach (var ch in dict2)
+                {
+                    if (!dict.ContainsKey(ch.Key) || dict[ch.Key] < ch.Value)
+                        return (false, -1);
+                    sum += (dict[ch.Key] * score[ch.Key - 'a']);
+                }
+
+                return (true, sum);
+            }
+        }
+    }
+}

a2149920b55fcf8b2bf645010b634530570d3b9c
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-13 08:05:58 +0400

    Search in a Binary Search Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Search in a Binary Search Tree.cs
@@ -0,0 +1,24 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Search in a Binary Search Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return null;
+
+            if (root.val == val)
+                return root;
+
+            return SearchBST(root.left, val) ?? SearchBST(root.right, val);
+        }
+    }
+}

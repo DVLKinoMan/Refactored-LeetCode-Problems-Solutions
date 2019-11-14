fa2b71db3b270cb588565f5c0be065fb25ede821
D.Dvali <dvali.daviti@gmail.com>
2019-11-14 17:15:16 +0400

    Valid Triangle Number

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Valid Triangle Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Valid Triangle Number (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int TriangleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int count = 0;
+            for (int i = 0; i < nums.Length - 2; i++)
+            for (int j = i + 1; j < nums.Length - 1; j++)
+            for (int k = j + 1; k < nums.Length; k++)
+            {
+                if (nums[i] + nums[j] <= nums[k])
+                    break;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}

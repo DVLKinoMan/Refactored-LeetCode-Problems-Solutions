a0aca768f514ea8271b237eaef6ac43e9d11051d
D.Dvali <dvali.daviti@gmail.com>
2019-11-14 15:58:17 +0400

    Product of Array Except Self

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Product of Array Except Self.cs
@@ -0,0 +1,39 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Product of Array Except Self (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            int product = 1, zeroesCount = 0, zeroIndex = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zeroesCount++;
+                    zeroIndex = i;
+                }
+                else product *= nums[i];
+            }
+
+            int[] result = new int[nums.Length];
+            if (zeroesCount > 1)
+                return result;
+
+            if (zeroesCount == 1)
+            {
+                result[zeroIndex] = product;
+                return result;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+                result[i] = product / nums[i];
+
+            return result;
+        }
+    }
+}

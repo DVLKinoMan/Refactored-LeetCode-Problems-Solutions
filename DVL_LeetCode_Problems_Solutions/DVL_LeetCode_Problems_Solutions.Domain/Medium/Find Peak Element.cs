ca3cf0d0cd8d82c4770e36f4e62d2e07f5e63680
D.Dvali <dvali.daviti@gmail.com>
2019-11-14 14:45:04 +0400

    Find Peak Element

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Find Peak Element.cs
@@ -0,0 +1,19 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find Peak Element (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindPeakElement(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                if ((i - 1 < 0 || nums[i] > nums[i - 1]) && (i + 1 == nums.Length || nums[i + 1] < nums[i]))
+                    return i;
+
+            return -1;
+        }
+    }
+}

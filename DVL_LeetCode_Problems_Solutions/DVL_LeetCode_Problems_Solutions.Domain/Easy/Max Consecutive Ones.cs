f35415a93c948d3a3c7ef3c217985547359be046
D.Dvali <dvali.daviti@gmail.com>
2019-11-14 16:16:12 +0400

    Max Consecutive Ones

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Max Consecutive Ones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Max Consecutive Ones (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int lastSeenOneIndex = -1, res = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1 && lastSeenOneIndex == -1)
+                    lastSeenOneIndex = i;
+                else if (nums[i] == 0)
+                {
+                    res = Math.Max(res, lastSeenOneIndex == -1 ? 0 : i - lastSeenOneIndex);
+                    lastSeenOneIndex = -1;
+                }
+            }
+
+            if (lastSeenOneIndex != -1)
+                res = Math.Max(res, nums.Length - lastSeenOneIndex);
+
+            return res;
+        }
+    }
+}

32b16a90dd02dbf78a22b4e3ac469d3ee7aaee1f
D.Dvali <dvali.daviti@gmail.com>
2019-11-14 15:15:53 +0400

    Two Sum II - Input array is sorted

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Two Sum II - Input array is sorted.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Two Sum II - Input array is sorted (Mine)
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            var leftToTargetNumberIndexes = new Dictionary<int,int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (leftToTargetNumberIndexes.ContainsKey(target - numbers[i]))
+                    return new int[] {leftToTargetNumberIndexes[target - numbers[i]], i + 1};
+                if (!leftToTargetNumberIndexes.ContainsKey(numbers[i]))
+                    leftToTargetNumberIndexes.Add(numbers[i], i + 1);
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}

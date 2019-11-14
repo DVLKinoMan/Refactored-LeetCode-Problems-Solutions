58015d748cc8394a439a20381f2408978fda3378
D.Dvali <dvali.daviti@gmail.com>
2019-11-14 16:51:33 +0400

    Array Nesting

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Array Nesting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver 
+    {
+        /// <summary>
+        /// Array Nesting (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int ArrayNesting(int[] nums)
+        {
+            int maxLen = 0;
+            var visitedIndexes = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+                if (!visitedIndexes.Contains(i))
+                    maxLen = Math.Max(maxLen, dfs(i, new HashSet<int>(), visitedIndexes));
+
+            return maxLen;
+
+            int dfs(int index, ISet<int> set, ISet<int> vSet)
+            {
+                if (!set.Add(nums[index]))
+                    return set.Count;
+                visitedIndexes.Add(index);
+                return dfs(nums[index], set, vSet);
+            }
+        }
+    }
+}

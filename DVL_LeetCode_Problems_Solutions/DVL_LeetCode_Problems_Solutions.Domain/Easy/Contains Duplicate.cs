7ee43181f7d9960b9951bf8c9401487ed26a5cce
D.Dvali <dvali.daviti@gmail.com>
2019-11-14 15:36:18 +0400

    Contains Duplicate

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Contains Duplicate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Contains Duplicate (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            var set = new HashSet<int>();
+            return !nums.All(num => set.Add(num));
+        }
+    }
+}

969d4e67ccd5d7ef5c51f055f269f69af161e17d
D.Dvali <dvali.daviti@gmail.com>
2019-11-15 17:17:35 +0400

    Find All Duplicates in an Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Find All Duplicates in an Array.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find All Duplicates in an Array (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static IList<int> FindDuplicates(int[] nums)
+        {
+            var set = new HashSet<int>();
+            return nums.Where(num => !set.Add(num)).ToList();
+        }
+    }
+}

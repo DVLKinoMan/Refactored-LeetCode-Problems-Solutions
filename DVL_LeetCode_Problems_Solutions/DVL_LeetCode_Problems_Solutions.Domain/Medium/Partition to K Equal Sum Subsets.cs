2e05da1ac5f6bd23f63c7a4570394a1b801add97
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-16 13:42:09 +0400

    Partition to K Equal Sum Subsets

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Partition to K Equal Sum Subsets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Partition to K Equal Sum Subsets (Not Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static bool CanPartitionKSubsets(int[] nums, int k)
+        {
+            if (k > nums.Length)
+                return false;
+            int sum = nums.Sum();
+            if (sum % k != 0)
+                return false;
+            Array.Sort(nums);
+            return dfs(nums, 0, nums.Length - 1, new bool[nums.Length], sum / k, k);
+
+            bool dfs(int[] A, int currSum, int st, bool[] visited, int target, int round)
+            {
+                if (round == 0)
+                    return true;
+                if (currSum == target && dfs(A, 0, A.Length - 1, visited, target, round - 1))
+                    return true;
+                for (int i = st; i >= 0; --i)
+                {
+                    if (!visited[i] && currSum + A[i] <= target)
+                    {
+                        visited[i] = true;
+                        if (dfs(A, currSum + A[i], i - 1, visited, target, round))
+                            return true;
+                        visited[i] = false;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}

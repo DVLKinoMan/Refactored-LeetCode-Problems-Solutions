16f90ddd1fd827b9a34a05574f8859c22a3c8f9d
D.Dvali <dvali.daviti@gmail.com>
2019-11-18 17:40:12 +0400

    Sort Characters By Frequency

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Sort Characters By Frequency.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Sort Characters By Frequency (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string FrequencySort(string s) => string.Join("",s.GroupBy(ch=>ch).OrderByDescending(gr=>gr.Count()).SelectMany(gr=>gr));
+    }
+}

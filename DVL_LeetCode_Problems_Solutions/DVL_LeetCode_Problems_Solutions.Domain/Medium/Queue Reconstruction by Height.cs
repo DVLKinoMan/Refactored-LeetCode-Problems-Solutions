a2c46124e13dcc7637ad222d4cca0b9a7302b6bb
D.Dvali <dvali.daviti@gmail.com>
2019-11-19 11:51:12 +0400

    Queue Reconstruction by Height (Refactored)

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Queue Reconstruction by Height.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Queue Reconstruction by Height.cs
@@ -15,42 +15,27 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
             var list = people.OrderBy(p => p[1]).ToList();
             var result = new List<int[]>();
 
-            while (list.Count != 0)
+            foreach (var arr in list)
             {
-                var first = list.First();
-                list.RemoveAt(0);
-
                 int count = 0;
-                bool hasFound = false;
-                for (int i = 0; i < result.Count; i++)
+                for (int i = 0; i <= result.Count; i++)
                 {
-                    if (count > first[1])
+                    if (count > arr[1])
                     {
-                        result.Insert(i - 1, first);
-                        hasFound = true;
+                        result.Insert(i - 1, arr);
                         break;
                     }
 
-                    if (result[i][0] >= first[0])
+                    if (i == result.Count)
+                    {
+                        if (count == arr[1])
+                            result.Add(arr);
+                        break;
+                    }
+
+                    if (result[i][0] >= arr[0])
                         count++;
                 }
-
-                if (!hasFound)
-                {
-                    if (count == first[1])
-                    {
-                        hasFound = true;
-                        result.Add(first);
-                    }
-                    else if (count > first[1])
-                    {
-                        hasFound = true;
-                        result.Insert(result.Count - 1, first);
-                    }
-                }
-
-                if (!hasFound)
-                    list.Add(first);
             }
 
             return result.ToArray();

8417917319265cc8f39e60a39981e1bac2ef45da
D.Dvali <dvali.daviti@gmail.com>
2019-11-19 16:13:24 +0400

    Non-overlapping Intervals

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Non-overlapping Intervals.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        ///  Non-overlapping Intervals (Not Mine)
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <returns></returns>
+        public static int EraseOverlapIntervals(int[][] intervals)
+        {
+            if (intervals.Length == 0)
+                return 0;
+
+            int count = 1;
+            intervals = intervals.OrderBy(arr => arr[1]).ToArray();
+            int end = intervals[0][1];
+            for (int i = 1; i < intervals.Length; i++)
+                if (intervals[i][0] >= end)
+                {
+                    end = intervals[i][1];
+                    count++;
+                }
+
+            return intervals.Length - count;
+        }
+    }
+}

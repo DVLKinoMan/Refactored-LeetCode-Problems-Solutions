1d63d1964b23fc4f1f455d0e57cdfd58bfa607b5
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-21 08:17:04 +0400

    Increasing Subsequences (Not Implemented)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Increasing Subsequences.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static IList<IList<int>> FindSubsequences(int[] nums)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            Array.Sort(nums);
+
+            void Dfs(List<int> list, int currIndex)
+            {
+
+                if (currIndex == nums.Length)
+                {
+                    res.Add(list);
+                    return;
+                }
+
+                for (int i = currIndex + 1; i < nums.Length; i++)
+                {
+
+                    Dfs(list, i);
+                }
+            }
+        }
+    }
+}

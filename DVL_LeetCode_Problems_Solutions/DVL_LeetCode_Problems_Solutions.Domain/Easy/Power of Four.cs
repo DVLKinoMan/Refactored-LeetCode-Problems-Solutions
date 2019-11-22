935c4d8f528d085c87f60650a6303e98a42c1fac
D.Dvali <dvali.daviti@gmail.com>
2019-11-22 17:41:39 +0400

    Power of Four

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Power of Four.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Power of Four (Mine)
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsPowerOfFour(int num)
+        {
+           var binaryString = System.Convert.ToString(num, 2);
+           var oneIndexes = binaryString.Reverse().Select((ch, i) => (ch, i)).Where(tup => tup.ch == '1').ToArray();
+           return oneIndexes.Length == 1 && oneIndexes[0].i % 2 == 0;
+            //Not Mine
+           //return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+        }
+    }
+}

cdf70682326851c216d645ed2479f7e7db2c0df8
D.Dvali <dvali.daviti@gmail.com>
2019-11-22 14:44:21 +0400

    Reverse String

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Reverse String.cs
@@ -0,0 +1,19 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Reverse String (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        public static void ReverseString(char[] s)
+        {
+            for (int i = 0; i < s.Length/2; i++)
+            {
+                char ch = s[i];
+                s[i] = s[s.Length - i - 1];
+                s[s.Length - i - 1] = ch;
+            }
+        }
+    }
+}

cc70efdc60c31cde18e595d5788babb20e72dc97
D.Dvali <dvali.daviti@gmail.com>
2019-11-22 15:03:43 +0400

    Reverse Vowels of a String

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Reverse Vowels of a String.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Reverse Vowels of a String (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ReverseVowels(string s)
+        {
+            var set = new HashSet<char>() {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            var indexes = s.Select((ch, i) => (ch: ch, index: i)).Where(tup => set.Contains(tup.ch))
+                .Select(tup => tup.index).ToList();
+            var builder = new StringBuilder(s);
+            for (int i = 0; i < indexes.Count/2; i++)
+            {
+                var ch = builder[indexes[i]];
+                builder[indexes[i]] = builder[indexes[indexes.Count-i-1]];
+                builder[indexes[indexes.Count - i - 1]] = ch;
+            }
+
+            return builder.ToString();
+        }
+    }
+}

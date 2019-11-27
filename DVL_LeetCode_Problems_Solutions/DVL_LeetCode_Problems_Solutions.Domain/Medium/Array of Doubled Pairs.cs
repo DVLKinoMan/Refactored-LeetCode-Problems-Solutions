ffd0d31d2bea6b8402be7422af29b7a49c6f604a
DVL_KinoMan <dvali.daviti@gmail.com>
2019-11-27 12:34:47 +0400

    Array of Doubled Pairs

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Array of Doubled Pairs.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Array of Doubled Pairs (Not Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static bool CanReorderDoubled(int[] A)
+        {
+            var dict = new SortedDictionary<int, int>();
+
+            foreach (var a in A)
+            {
+                if (dict.ContainsKey(a))
+                    dict[a]++;
+                else dict.Add(a, 1);
+            }
+
+            for(int i = 0; i<dict.Count; i++)
+            {
+                var d = dict.ElementAt(i);
+                if(d.Value == 0)
+                    continue;
+
+                int want = d.Key < 0 ? d.Key / 2 : d.Key * 2;
+                if (d.Key < 0 && d.Key % 2 != 0 || !dict.TryGetValue(want, out int wantValue) || d.Value > wantValue)
+                    return false;
+
+                dict[want] -= d.Value;
+            }
+
+            return true;
+        }
+    }
+}

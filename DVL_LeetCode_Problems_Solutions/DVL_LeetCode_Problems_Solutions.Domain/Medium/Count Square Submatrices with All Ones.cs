7e639f995b4cf35241ce97537ee2fdac1003f424
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-02 08:23:48 +0400

    Count Square Submatrices with All Ones

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Count Square Submatrices with All Ones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Count Square Submatrices with All Ones (Not Mine)
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static int CountSquares(int[][] matrix)
+        {
+            int res = 0;
+            for (int i = 0; i < matrix.Length; ++i)
+            for (int j = 0; j < matrix[0].Length; ++j)
+            {
+                if (matrix[i][j] > 0 && i > 0 && j > 0)
+                    matrix[i][j] = Math.Min(matrix[i - 1][j - 1], Math.Min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
+
+                res += matrix[i][j];
+            }
+
+            return res;
+        }
+    }
+}

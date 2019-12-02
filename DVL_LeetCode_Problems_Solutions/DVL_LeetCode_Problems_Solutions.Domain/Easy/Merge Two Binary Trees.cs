ec5d0fdc2d4977f94e64b56e2a54d4c8748f224e
D.Dvali <dvali.daviti@gmail.com>
2019-12-02 17:56:57 +0400

    Merge Two Binary Trees

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Merge Two Binary Trees.cs
@@ -0,0 +1,25 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Merge Two Binary Trees (Mine)
+        /// </summary>
+        /// <param name="t1"></param>
+        /// <param name="t2"></param>
+        /// <returns></returns>
+        public static TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null)
+                return null;
+
+            return new TreeNode((t1 != null ? t1.val : 0) + (t2 != null ? t2.val : 0))
+            {
+                left = MergeTrees(t1?.left, t2?.left),
+                right = MergeTrees(t1?.right, t2?.right)
+            };
+        }
+    }
+}

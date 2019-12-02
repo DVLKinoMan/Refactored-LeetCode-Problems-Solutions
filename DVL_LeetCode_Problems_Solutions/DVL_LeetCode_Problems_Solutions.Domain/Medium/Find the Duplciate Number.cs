1698587bcc43565d4c15c70ebed25a3db76f5430
D.Dvali <dvali.daviti@gmail.com>
2019-12-02 17:36:18 +0400

    Find the Duplicate Number

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Find the Duplciate Number.cs
@@ -0,0 +1,44 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find the Duplicate Number (Not Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindDuplicate(int[] nums)
+        {
+            if (nums.Length > 1)
+            {
+                int slow = nums[0];
+                int fast = nums[nums[0]];
+                while (slow != fast)
+                {
+                    slow = nums[slow];
+                    fast = nums[nums[fast]];
+                }
+
+                fast = 0;
+                while (fast != slow)
+                {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+
+                return slow;
+            }
+
+            return -1;
+
+            //My Version (Slow)
+            //for (int i = 0; i < nums.Length; i++)
+            //    for (int j = i + 1; j < nums.Length; j++)
+            //        if (nums[i] == nums[j])
+            //            return nums[i];
+
+            //throw new NotImplementedException("Problem Description was not right");
+
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Find the Duplicate Number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static int FindDuplicate(int[] nums)
+        {
+            //for (int i = 0; i < nums.Length; i++)
+            //    for (int j = i + 1; j < nums.Length; j++)
+            //        if (nums[i] == nums[j])
+            //            return nums[i];
+
+            //throw new NotImplementedException("Problem Description was not right");
+
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+
+            fast = 0;
+            while (fast != slow)
+            {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+
+            return slow;
+        }
+    }
+}

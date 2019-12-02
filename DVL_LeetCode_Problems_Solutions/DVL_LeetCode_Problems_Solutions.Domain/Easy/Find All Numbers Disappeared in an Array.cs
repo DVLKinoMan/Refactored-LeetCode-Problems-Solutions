da7eadd575866f5d915a4f828c3397f2843da7fd
D.Dvali <dvali.daviti@gmail.com>
2019-12-02 18:13:15 +0400

    Find All Numbers Disappeared in an Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Find All Numbers Disappeared in an Array.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find All Numbers Disappeared in an Array (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            var set = new HashSet<int>(Enumerable.Range(1, nums.Length));
+            foreach (var num in nums)
+                set.Remove(num);
+
+            return set.ToList();
+        }
+    }
+}

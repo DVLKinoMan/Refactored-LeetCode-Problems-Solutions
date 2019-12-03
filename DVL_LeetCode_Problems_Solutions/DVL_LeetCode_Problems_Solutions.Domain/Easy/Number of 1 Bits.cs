74725608c2ace3e2523e8a75a0f6236014842582
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-03 08:07:05 +0400

    Number of 1 Bits

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Number of 1 Bits.cs
@@ -0,0 +1,21 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Number of 1 Bits (Not Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int HammingWeight(uint n)
+        {
+            long ones = 0;
+            while (n != 0)
+            {
+                ones = ones + (n & 1);
+                n = n >> 1;
+            }
+            return (int)ones;
+        }
+    }
+}

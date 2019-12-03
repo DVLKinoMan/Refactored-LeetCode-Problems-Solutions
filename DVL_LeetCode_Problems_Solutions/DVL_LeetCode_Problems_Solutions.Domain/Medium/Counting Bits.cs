208fa9d0a4624f5a0b3a172e2acc6762d6e55a01
D.Dvali <dvali.daviti@gmail.com>
2019-12-03 14:37:00 +0400

    Counting Bits

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Counting Bits.cs
@@ -0,0 +1,18 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Counting Bits (Not Mine)
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int[] CountBits(int num)
+        {
+            int[] arr = new int[num + 1];
+            for (int i = 1; i <= num; i++)
+                arr[i] = arr[i >> 1] + (i & 1);
+            return arr;
+        }
+    }
+}

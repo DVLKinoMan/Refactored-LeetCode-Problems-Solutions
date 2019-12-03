37b3395ff0a2c692db94d19a5aceb3018f2b484c
D.Dvali <dvali.daviti@gmail.com>
2019-12-03 15:23:03 +0400

    Reverse LInked List

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Reverse Linked List.cs
@@ -0,0 +1,31 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Reverse LInked List (Mine)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            ListNode last;
+            Func(head, null);
+
+            return last;
+
+            void Func(ListNode node, ListNode parentNode)
+            {
+                if (node.next != null)
+                    Func(node.next, node);
+                else last = node;
+                node.next = parentNode;
+            }
+        }
+    }
+}

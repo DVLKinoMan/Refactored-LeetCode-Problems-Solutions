94ea49c434314c4758ea3192abeeaab88fdcee21
D.Dvali <dvali.daviti@gmail.com>
2019-12-03 17:06:46 +0400

    Invert Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Invert Binary Tree.cs
@@ -0,0 +1,26 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Invert Binary Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            var leftNode = root.left;
+            root.left = root.right;
+            root.right = leftNode;
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}

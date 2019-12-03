11578f4514db14950e8c9654f832a3c697b0522d
D.Dvali <dvali.daviti@gmail.com>
2019-12-03 16:55:29 +0400

    Kth Largest Element in an Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Kth Largest Element in an Array.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Kth Largest Element in an Array (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int FindKthLargest(int[] nums, int k)
+        {
+            return nums.OrderByDescending(n => n).ElementAt(k - 1);
+        }
+    }
+}

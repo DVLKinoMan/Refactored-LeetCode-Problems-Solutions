ec7743bf1b90028226cad0fe6d9f451ec9e82a9a
D.Dvali <dvali.daviti@gmail.com>
2019-12-04 12:44:22 +0400

    Populating Next Right Pointers in Each Node II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Populating Next Right Pointers in Each Node II.cs
@@ -0,0 +1,47 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Populating Next Right Pointers in Each Node II (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static Node Connect2(Node root)
+        {
+            var leftNode = root;
+            while (leftNode != null)
+            {
+                var across = leftNode;
+                while (across != null)
+                {
+                    var nextLeftNode = GetNodeLeftNode(across.next);
+
+                    if (across.left != null)
+                        across.left.next = across.right ?? nextLeftNode;
+                    if (across.right != null)
+                        across.right.next = nextLeftNode;
+                    across = across.next;
+                }
+
+                leftNode = leftNode.left ?? leftNode.right ?? GetNodeLeftNode(leftNode.next);
+            }
+
+            return root;
+
+            Node GetNodeLeftNode(Node nd)
+            {
+                var f = nd;
+                while (f != null)
+                {
+                    var res = f.left ?? f.right;
+                    if (res != null)
+                        return res;
+                    f = f.next;
+                }
+
+                return null;
+            }
+        }
+    }
+}

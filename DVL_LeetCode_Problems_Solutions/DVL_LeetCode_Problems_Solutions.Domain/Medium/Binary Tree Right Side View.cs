b0143ac7749e8d115d8a846fc6086cb0727fe9ba
D.Dvali <dvali.daviti@gmail.com>
2019-12-04 10:45:56 +0400

    Binary Tree Right Side View (New Version)

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Binary Tree Right Side View.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Binary Tree Right Side View.cs
@@ -12,32 +12,52 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
         /// <returns></returns>
         public static IList<int> RightSideView(TreeNode root)
         {
-            if(root == null)
-                return new List<int>();
-            var res = new List<int>() {root.val};
-            var list = new List<TreeNode>() {root};
-            while (list.Count != 0)
+            //My Version:
+            //if(root == null)
+            //    return new List<int>();
+            //var res = new List<int>() {root.val};
+            //var list = new List<TreeNode>() {root};
+            //while (list.Count != 0)
+            //{
+            //    var newList = new List<TreeNode>();
+            //    bool wasFoundLast = false;
+            //    foreach (var node in list)
+            //    {
+            //        var n = node.right ?? node.left;
+            //        if (!wasFoundLast && n != null)
+            //        {
+            //            res.Add(n.val);
+            //            wasFoundLast = true;
+            //        }
+            //        if(node.right!=null)
+            //            newList.Add(node.right);
+            //        if(node.left!=null)
+            //            newList.Add(node.left);
+            //    }
+
+            //    list = newList;
+            //}
+
+            //return res;
+
+            //My implementation (not my solution):
+            var result = new List<int>();
+            if (root != null)
             {
-                var newList = new List<TreeNode>();
-                bool wasFoundLast = false;
-                foreach (var node in list)
-                {
-                    var n = node.right ?? node.left;
-                    if (!wasFoundLast && n != null)
-                    {
-                        res.Add(n.val);
-                        wasFoundLast = true;
-                    }
-                    if(node.right!=null)
-                        newList.Add(node.right);
-                    if(node.left!=null)
-                        newList.Add(node.left);
-                }
-
-                list = newList;
+                result.Add(root.val);
+                Rec(root, 1, result);
             }
+            return result;
 
-            return res;
+            void Rec(TreeNode node, int level, List<int> res)
+            {
+                if(node == null)
+                    return;
+                if (res.Count < level)
+                    res.Add(node.val);
+                Rec(node.right, level + 1, res);
+                Rec(node.left, level + 1, res);
+            }
         }
     }
 }

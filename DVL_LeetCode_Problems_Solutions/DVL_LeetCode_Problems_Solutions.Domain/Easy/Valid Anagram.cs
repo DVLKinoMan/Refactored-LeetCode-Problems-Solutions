de9de90579d9fb1fb5cc352efe2db90f8c7d5d16
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-05 08:04:40 +0400

    Valid Anagram

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Valid Anagram.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Valid Anagram (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            var dict1 = GetDict(s);
+            var dict2 = GetDict(t);
+
+            foreach (var keyVal in dict1)
+                if (!dict2.ContainsKey(keyVal.Key) || dict2[keyVal.Key] != keyVal.Value)
+                    return false;
+
+            return true;
+
+            Dictionary<char, int> GetDict(string str)
+            {
+                var dict = new Dictionary<char, int>();
+                foreach (var ch in str)
+                {
+                    if (dict.ContainsKey(ch))
+                        dict[ch]++;
+                    else dict.Add(ch, 1);
+                }
+
+                return dict;
+            }
+        }
+    }
+}

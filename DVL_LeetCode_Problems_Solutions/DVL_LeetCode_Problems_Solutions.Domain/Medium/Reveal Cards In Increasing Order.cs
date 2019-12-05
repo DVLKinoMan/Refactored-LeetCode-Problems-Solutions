f41f6716924476c14a6449f78b7597afe1dbb5f1
D.Dvali <dvali.daviti@gmail.com>
2019-12-05 17:50:18 +0400

    Reveal Cards In Increasing Order

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Reveal Cards In Increasing Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Reveal Cards In Increasing Order (Not Mine)
+        /// </summary>
+        /// <param name="deck"></param>
+        /// <returns></returns>
+        public static int[] DeckRevealedIncreasing(int[] deck)
+        {
+            int[] arr = new int[deck.Length];
+            Array.Sort(deck);
+            var queue = new Queue<int>();
+            for (int i = 0; i < deck.Length; i++)
+                queue.Enqueue(i);
+
+            for (int i = 0; i < deck.Length; i++)
+            {
+                arr[queue.Dequeue()] = deck[i];
+                if (i != deck.Length - 1)
+                    queue.Enqueue(queue.Dequeue());
+            }
+
+            return arr;
+        }
+    }
+}

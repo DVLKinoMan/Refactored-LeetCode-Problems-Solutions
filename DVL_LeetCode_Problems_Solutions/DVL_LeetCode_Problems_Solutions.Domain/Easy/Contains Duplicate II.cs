b2612e9a3c18e28731d597bbb6580e6de882fb9c
D.Dvali <dvali.daviti@gmail.com>
2019-12-05 18:15:18 +0400

    Contains Duplicate II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Contains Duplicate II.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Contains Duplicate II (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    if (i - dict[nums[i]] <= k)
+                        return true;
+                    else dict[nums[i]] = i;
+                }
+                else dict.Add(nums[i], i);
+            }
+
+            return false;
+        }
+    }
+}

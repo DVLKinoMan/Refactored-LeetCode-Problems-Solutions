72587c956372ed888e2591aefe43c0ee8e81ab41
D.Dvali <dvali.daviti@gmail.com>
2019-12-05 10:19:40 +0400

    Valid Anagram (Another Version)

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Valid Anagram.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Valid Anagram.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
@@ -12,30 +13,44 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
         /// <returns></returns>
         public static bool IsAnagram(string s, string t)
         {
+            //My version:
+            //if (s.Length != t.Length)
+            //    return false;
+
+            //var dict1 = GetDict(s);
+            //var dict2 = GetDict(t);
+
+            //foreach (var keyVal in dict1)
+            //    if (!dict2.ContainsKey(keyVal.Key) || dict2[keyVal.Key] != keyVal.Value)
+            //        return false;
+
+            //return true;
+
+            //Dictionary<char, int> GetDict(string str)
+            //{
+            //    var dict = new Dictionary<char, int>();
+            //    foreach (var ch in str)
+            //    {
+            //        if (dict.ContainsKey(ch))
+            //            dict[ch]++;
+            //        else dict.Add(ch, 1);
+            //    }
+
+            //    return dict;
+            //}
+
+            //My implementation (idea not mine)
             if (s.Length != t.Length)
                 return false;
 
-            var dict1 = GetDict(s);
-            var dict2 = GetDict(t);
-
-            foreach (var keyVal in dict1)
-                if (!dict2.ContainsKey(keyVal.Key) || dict2[keyVal.Key] != keyVal.Value)
-                    return false;
-
-            return true;
-
-            Dictionary<char, int> GetDict(string str)
+            char[] arr = new char[26];
+            for (int i = 0; i < s.Length; i++)
             {
-                var dict = new Dictionary<char, int>();
-                foreach (var ch in str)
-                {
-                    if (dict.ContainsKey(ch))
-                        dict[ch]++;
-                    else dict.Add(ch, 1);
-                }
-
-                return dict;
+                arr[s[i] - 'a']++;
+                arr[t[i] - 'a']--;
             }
+
+            return arr.All(c => c == 0);
         }
     }
 }

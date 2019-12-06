3ee63e17f26c6132da3e45d11f28ed3bdee5c189
D.Dvali <dvali.daviti@gmail.com>
2019-12-06 15:07:20 +0400

    Integer Replacement

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Integer Replacement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Integer Replacement (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="opCount"></param>
+        /// <returns></returns>
+        public static int IntegerReplacement(int n, int opCount = 0)
+        {
+            switch (n)
+            {
+                case int.MaxValue:
+                    return 32;
+                case 1:
+                    return opCount;
+                default:
+                    opCount++;
+                    return (n & 1) == 0 ? IntegerReplacement(n >> 1, opCount) :
+                        Math.Min(IntegerReplacement(n - 1, opCount), IntegerReplacement(n + 1, opCount));
+            }
+        }
+    }
+}

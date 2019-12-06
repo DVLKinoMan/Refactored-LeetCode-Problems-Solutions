be7f41e716311281888d9648aab01797545576ff
D.Dvali <dvali.daviti@gmail.com>
2019-12-06 15:43:24 +0400

    Range Sum Query - Mutable

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Range Sum Query - Mutable.cs
@@ -0,0 +1,38 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Range Sum Query - Mutable (Mine)
+        /// </summary>
+        public class NumArray
+        {
+            private int[] sums;
+            private int[] originalNums;
+
+            public NumArray(int[] nums)
+            {
+                sums = new int[nums.Length];
+                originalNums = nums;
+                if (nums.Length == 0)
+                    return;
+                sums[0] = nums[0];
+                for (int i = 1; i < sums.Length; i++)
+                    sums[i] = sums[i - 1] + originalNums[i];
+            }
+
+            public void Update(int i, int val)
+            {
+                int diff = val - originalNums[i];
+                originalNums[i] = val;
+                for (; i < sums.Length; i++)
+                    sums[i] += diff;
+            }
+
+            public int SumRange(int i, int j)
+            {
+                return sums[j] - (i == 0 ? 0 : sums[i - 1]);
+            }
+        }
+    }
+}

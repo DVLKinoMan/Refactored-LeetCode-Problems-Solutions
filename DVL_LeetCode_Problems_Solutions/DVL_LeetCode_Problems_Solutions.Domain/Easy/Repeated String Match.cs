7d27c4c7334b2677357d31e249455151be125d7e
D.Dvali <dvali.daviti@gmail.com>
2019-12-10 11:36:21 +0400

    Repeated String Match

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Repeated String Match.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Repeated String Match.cs
@@ -1,49 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
         /// <summary>
-        /// Repeated String Match (Not Working)
+        /// Repeated String Match (Mine)
         /// </summary>
         /// <param name="A"></param>
         /// <param name="B"></param>
         /// <returns></returns>
         public static int RepeatedStringMatch(string A, string B)
         {
-            int res = int.MaxValue;
-            var indexes = A.Select((a, i) => (a, i)).Where(t => t.a == B[0]).Select(t => t.i);
-            foreach (var index in indexes)
-            {
-                int aIndex = index, count = 1;
-                bool can = true;
-                for (int i = 0; i < B.Length; i++)
-                {
-                    if (A[aIndex] != B[i])
-                    {
-                        can = false;
-                        break;
-                    }
-
-                    if (A.Length - 1 == aIndex)
-                    {
-                        aIndex = 0;
-                        count++;
-                    }
-                    else aIndex++;
-                }
-
-                if (can)
-                    res = Math.Min(res, count);
-            }
-
-            return res == int.MaxValue ? -1 : res;
+            int count = (int)Math.Ceiling((double) B.Length / A.Length);
+            string str = string.Concat(Enumerable.Repeat(A, count));
+            return str.Contains(B) ? count : (str + A).Contains(B) ? count + 1 : -1;
         }
     }
 }

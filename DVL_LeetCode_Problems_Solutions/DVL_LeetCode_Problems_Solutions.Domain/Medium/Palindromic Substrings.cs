9ac8924d151ac3cfbebd431c2868605a4ec46909
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-12 06:51:48 +0400

    Palindromic Substrings (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Palindromic Substrings.cs
@@ -0,0 +1,31 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Palindromic Substrings (Not Working)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int CountSubstrings(string s)
+        {
+            int count = 0;
+            for (int index = 0; index < s.Length; index++)
+                for (int len = 1; len <= s.Length - index; len++)
+                    if (IsPalindrome(index, index + len - 1))
+                        count++;
+
+            return count;
+
+            bool IsPalindrome(int st, int end)
+            {
+                int len = (end - st + 1) / 2;
+                for (int i = st; i < len; i++)
+                    if (s[i] != s[end - (i - st)])
+                        return false;
+
+                return true;
+            }
+        }
+    }
+}

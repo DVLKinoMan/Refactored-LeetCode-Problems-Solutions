8eb4b4d414cde650bf5b1e525b08cd09c3a028d4
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-13 06:45:40 +0400

    Average of Levels in Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Average of Levels in Binary Tree.cs
@@ -0,0 +1,36 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Average of Levels in Binary Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<double> AverageOfLevels(TreeNode root)
+        {
+            var res = new List<double>();
+            var list = new List<TreeNode>(){ root };
+            while (list.Count != 0)
+            {
+                var newList = new List<TreeNode>();
+                res.Add(list.Average(l => l.val));
+                foreach (var l in list)
+                {
+                    if (l.left != null)
+                        newList.Add(l.left);
+                    if (l.right != null)
+                        newList.Add(l.right);
+                }
+
+                list = newList;
+            }
+
+            return res;
+        }
+    }
+}

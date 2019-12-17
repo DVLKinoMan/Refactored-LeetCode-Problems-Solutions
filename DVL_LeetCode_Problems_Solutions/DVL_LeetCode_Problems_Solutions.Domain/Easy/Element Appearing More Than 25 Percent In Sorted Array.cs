7700269f4110b5487354fb7b86f4787789b1a504
D.Dvali <dvali.daviti@gmail.com>
2019-12-17 15:11:24 +0400

    Element Appearing More Than 25% In Sorted Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Element Appearing More Than 25 Percent In Sorted Array.cs
@@ -0,0 +1,20 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Element Appearing More Than 25% In Sorted Array (Mine)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int FindSpecialInteger(int[] arr)
+        {
+            int len = arr.Length / 4;
+            for (int i = len; i < arr.Length; i++)
+                if (arr[i] == arr[i - len])
+                    return arr[i];
+
+            return arr[arr.Length - 1];
+        }
+    }
+}

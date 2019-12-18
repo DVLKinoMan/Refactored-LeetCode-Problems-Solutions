a21c4e5af858d328ba6d105f31ec0be950f5d6da
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-18 06:41:00 +0400

    Find Smallest Letter Greater Than Target

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Find Smallest Letter Greater Than Target.cs
@@ -0,0 +1,24 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find Smallest Letter Greater Than Target (Mine)
+        /// </summary>
+        /// <param name="letters"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static char NextGreatestLetter(char[] letters, char target)
+        {
+            char minChar = char.MaxValue, firstMax = char.MaxValue;
+            foreach (var letter in letters)
+            {
+                minChar = minChar < letter ? minChar : letter;
+                if (letter > target && letter < firstMax)
+                    firstMax = letter;
+            }
+
+            return firstMax != char.MaxValue ? firstMax : minChar;
+        }
+    }
+}

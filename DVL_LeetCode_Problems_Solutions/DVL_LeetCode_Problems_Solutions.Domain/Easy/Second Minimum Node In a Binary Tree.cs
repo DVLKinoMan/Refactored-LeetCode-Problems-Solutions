99153d72a82de18c0ac1bbb01e10fca8f3761718
D.Dvali <dvali.daviti@gmail.com>
2019-12-19 15:29:37 +0400

    Second Minimum Node In a Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Second Minimum Node In a Binary Tree.cs
@@ -0,0 +1,36 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Second Minimum Node In a Binary Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int FindSecondMinimumValue(TreeNode root)
+        {
+            if (root == null)
+                return -1;
+
+            int min = root.val, res = -1;
+            Dfs(root);
+
+            return res;
+
+            void Dfs(TreeNode curr)
+            {
+                if (curr == null)
+                    return;
+
+                if (curr.val > min)
+                    res = Math.Min(res == -1 ? int.MaxValue : res, curr.val);
+
+                Dfs(curr.left);
+                Dfs(curr.right);
+            }
+        }
+    }
+}

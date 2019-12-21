e7b764ac8ef1810f1f6e44feb425e4db44c07c70
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-21 07:19:05 +0400

    Top K Frequent Words

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Top K Frequent Words.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Top K Frequent Words (Mine)
+        /// </summary>
+        /// <param name="words"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static IList<string> TopKFrequent(string[] words, int k)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var w in words)
+                dict[w] = dict.ContainsKey(w) ? dict[w] + 1 : 1;
+
+            return dict.OrderByDescending(d => d.Value)
+                       .ThenBy(d => d.Key)
+                       .Take(k)
+                       .Select(d => d.Key)
+                       .ToList();
+        }
+    }
+}

12e0ac619699582a6899ed3215afba70a99c34a8
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-24 06:50:03 +0400

    Subtree of Another Tree (Not working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Subtree of Another Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Subtree of Another Tree (Not Working)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="mustHaveEqualValues"></param>
+        /// <returns></returns>
+        public static bool IsSubtree(TreeNode s, TreeNode t, bool mustHaveEqualValues = false)
+        {
+            if (s == null && t == null)
+                return true;
+            if (s == null || t == null)
+                return false;
+            if (s.val == t.val && mustHaveEqualValues && IsSubtree(s.left, t.left) && IsSubtree(s.right, t.right))
+                    return true;
+
+            return IsSubtree(s.left, t) || IsSubtree(s.right, t);
+        }
+    }
+}

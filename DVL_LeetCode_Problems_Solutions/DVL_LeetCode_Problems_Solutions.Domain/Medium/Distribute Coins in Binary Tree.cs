d0d5bc4f6e10f127ad2eeac550843e1104d7e8f9
D.Dvali <dvali.daviti@gmail.com>
2019-12-25 13:44:48 +0400

    Distribute Coins in Binary Tree

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Distribute Coins in Binary Tree.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Distribute Coins in Binary Tree.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
+        /// <summary>
+        /// Distribute Coins in Binary Tree (Not Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
         public static int DistributeCoins(TreeNode root)
         {
+            int res = 0;
+            Dfs(root);
 
+            return res;
+
+            int Dfs(TreeNode node)
+            {
+                if (node == null)
+                    return 0;
+                int left = Dfs(node.left), right = Dfs(node.right);
+                res += Math.Abs(left) + Math.Abs(right);
+                return node.val + left + right - 1;
+            }
         }
     }
 }

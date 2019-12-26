0aa78f8448b81da3fdaaaafe72c288d39625d2e8
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-26 06:37:16 +0400

    Reverse Only Letters

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Reverse Only Letters.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Reverse Only Letters (Mine)
+        /// </summary>
+        /// <param name="S"></param>
+        /// <returns></returns>
+        public static string ReverseOnlyLetters(string S)
+        {
+            var builder = new StringBuilder(S);
+            int i = 0, j = S.Length - 1;
+            while (i < j)
+            {
+                bool iIsLetter = char.IsLetter(builder[i]), jIsLetter = char.IsLetter(builder[j]);
+                if (iIsLetter && jIsLetter)
+                {
+                    var ch = builder[i];
+                    builder[i] = builder[j];
+                    builder[j] = ch;
+                    i++;
+                    j--;
+                }
+                else if (!iIsLetter)
+                    i++;
+                else //!jIsLetter
+                    j--;
+            }
+
+            return builder.ToString();
+        }
+    }
+}

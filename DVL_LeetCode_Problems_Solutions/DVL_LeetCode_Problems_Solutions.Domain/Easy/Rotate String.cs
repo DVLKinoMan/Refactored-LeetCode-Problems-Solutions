946110601f89e551ef1807e209c318bc4f48e54f
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-27 06:41:00 +0400

    Rotate String

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Rotate String.cs
@@ -0,0 +1,26 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain.Easy
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Rotate String (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <returns></returns>
+        public static bool RotateString(string A, string B)
+        {
+            if (A == B)
+                return true;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                string str = A.Substring(i) + (i != 0 ? A.Substring(0, i) : "");
+                if (str == B)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}

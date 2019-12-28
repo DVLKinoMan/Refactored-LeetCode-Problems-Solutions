9d3f0c8507a9a9d44dfeae962e1cfce42d96aeea
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-28 07:38:24 +0400

    Minimum Swaps To Make Sequences Increasing

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Minimum Swaps To Make Sequences Increasing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain.Medium
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Minimum Swaps To Make Sequences Increasing (Not Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <returns></returns>
+        public static int MinSwap(int[] A, int[] B)
+        {
+            int fixRecord = 0, swapRecord = 1;
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (A[i - 1] >= B[i] || B[i - 1] >= A[i])
+                    swapRecord++;
+                else if (A[i - 1] >= A[i] || B[i - 1] >= B[i])
+                {
+                    int temp = swapRecord;
+                    swapRecord = fixRecord + 1;
+                    fixRecord = temp;
+                }
+                else
+                {
+                    int min = Math.Min(swapRecord, fixRecord);
+                    swapRecord = min + 1;
+                    fixRecord = min;
+                }
+            }
+
+            return Math.Min(fixRecord, swapRecord);
+        }
+    }
+}

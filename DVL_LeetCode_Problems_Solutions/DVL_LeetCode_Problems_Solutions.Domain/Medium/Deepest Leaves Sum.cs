ba947f7f91080943c614680d897e88a3b124d9e0
DVL_KinoMan <dvali.daviti@gmail.com>
2019-12-31 06:56:23 +0400

    Deepest Leaves Sum

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Deepest Leaves Sum.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Deepest Leaves Sum.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {

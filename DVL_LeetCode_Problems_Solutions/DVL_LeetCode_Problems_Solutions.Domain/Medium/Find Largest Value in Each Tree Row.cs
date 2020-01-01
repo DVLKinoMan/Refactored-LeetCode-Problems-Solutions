19ac2e010067e83426c3eb2fe638b65593179cf5
davit dvali <dvali.daviti@gmail.com>
2020-01-01 10:45:08 +0400

    Find Largest Value in Each Tree Row

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Find Largest Value in Each Tree Row.cs
@@ -0,0 +1,40 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find Largest Value in Each Tree Row (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> LargestValues(TreeNode root)
+        {
+            var result = new List<int>();
+            if (root == null)
+                return result;
+
+            var list = new List<TreeNode>() { root };
+            while (list.Count != 0)
+            {
+                int maxInRow = list[0].val;
+                var newList = new List<TreeNode>();
+                foreach (var n in list)
+                {
+                    maxInRow = Math.Max(maxInRow, n.val);
+                    if (n.left != null)
+                        newList.Add(n.left);
+                    if (n.right != null)
+                        newList.Add(n.right);
+                }
+                list = newList;
+                result.Add(maxInRow);
+            }
+
+            return result;
+        }
+    }
+}

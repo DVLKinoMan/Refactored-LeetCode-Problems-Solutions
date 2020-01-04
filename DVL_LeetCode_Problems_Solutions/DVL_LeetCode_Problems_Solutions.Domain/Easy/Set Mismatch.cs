a24d087f745ffcc3748ccfac9e3c5c1f28ed5777
DVL_KinoMan <dvali.daviti@gmail.com>
2020-01-04 18:26:49 +0400

    Set Mismatch

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Set Mismatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Set Mismatch (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int[] FindErrorNums(int[] nums)
+        {
+            var set = new HashSet<int>();
+            int missingNum = 0, twiceNum = 0;
+            foreach (var num in nums)
+                if (!set.Add(num))
+                    twiceNum = num;
+            for (int i = 1; i <= nums.Length; i++)
+                if (!set.Contains(i))
+                    missingNum = i;
+
+            return new int[] {twiceNum, missingNum};
+        }
+    }
+}

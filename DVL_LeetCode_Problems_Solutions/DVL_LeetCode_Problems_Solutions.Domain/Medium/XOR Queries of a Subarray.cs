d0339d896c523c9408f46fb323b8a59b4d557fd2
D.Dvali <dvali.daviti@gmail.com>
2020-01-06 18:05:36 +0400

    XOR Queries of a Subarray

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/XOR Queries of a Subarray.cs
@@ -0,0 +1,23 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// XOR Queries of a Subarray (Not Mine)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="queries"></param>
+        /// <returns></returns>
+        public static int[] XorQueries(int[] arr, int[][] queries)
+        {
+            var result = new int[queries.Length];
+            for (int i = 1; i < arr.Length; i++)
+                arr[i] ^= arr[i - 1];
+
+            for (int i = 0; i < queries.Length; i++)
+                result[i] = queries[i][0] > 0 ? arr[queries[i][0] - 1] ^ arr[queries[i][1]] : arr[queries[i][1]];
+
+            return result;
+        }
+    }
+}

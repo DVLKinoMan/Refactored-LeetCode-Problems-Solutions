3889ded8cdfba374ae85d8d9cbb570a839d4f8b9
DVL_KinoMan <dvali.daviti@gmail.com>
2020-01-12 11:11:15 +0400

    Minimum Flips to Make a OR b Equal to c

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Minimum Flips to Make a OR b Equal to c.cs
@@ -0,0 +1,38 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Minimum Flips to Make a OR b Equal to c (Mine)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static int MinFlips(int a, int b, int c)
+        {
+            int count = 0;
+            while (a != 0 || b != 0)
+            {
+                if (((a & 1) | (b & 1)) != (c & 1))
+                {
+                    if (((a & 1) & (b & 1)) == 1)
+                        count++;
+                    count++;
+                }
+                a >>= 1;
+                b >>= 1;
+                c >>= 1;
+            }
+
+            while (c != 0)
+            {
+                if ((c & 1) == 1)
+                    count++;
+                c >>= 1;
+            }
+
+            return count;
+        }
+    }
+}

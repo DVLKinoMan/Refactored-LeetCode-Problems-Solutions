52b205c6312b4b61e931f9982db5758e35707945
DVL_KinoMan <dvali.daviti@gmail.com>
2020-01-12 10:44:45 +0400

    Convert Integer to the Sum of Two No-Zero Integers

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Convert Integer to the Sum of Two No-Zero Integers.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Convert Integer to the Sum of Two No-Zero Integers (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int[] GetNoZeroIntegers(int n)
+        {
+            for (int i = 1; i < n; i++)
+                if (NotContainsZero(i))
+                {
+                    int j = n - i;
+                    if (NotContainsZero(j))
+                        return new int[] {i, j};
+                }
+
+            return new int[0];
+
+            bool NotContainsZero(int k) => !k.ToString().Contains('0');
+        }
+    }
+}

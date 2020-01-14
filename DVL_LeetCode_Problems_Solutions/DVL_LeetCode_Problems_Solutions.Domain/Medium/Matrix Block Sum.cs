97320e5ee7a8cad4624e5a8f0954623381818e10
D.Dvali <dvali.daviti@gmail.com>
2020-01-14 10:20:19 +0400

    Matrix Block Sum

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Matrix Block Sum.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Matrix Block Sum.cs
@@ -1,26 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
-        public int[][] MatrixBlockSum(int[][] mat, int K)
+        /// <summary>
+        /// Matrix Block Sum (Not Mine)
+        /// </summary>
+        /// <param name="mat"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static int[][] MatrixBlockSum(int[][] mat, int K)
         {
             int m = mat.Length, n = mat[0].Length;
-            int[,]
-                sum = new int[m + 1,
-                    n + 1]; // sum[i][j] is sum of all elements from rectangle (0,0,i,j) as left, top, right, bottom corresponding
+            int[,] sum = new int[m + 1, n + 1]; 
             for (int r = 1; r <= m; r++)
                 for (int c = 1; c <= n; c++)
                     sum[r, c] = mat[r - 1][c - 1] + sum[r - 1, c] + sum[r, c - 1] - sum[r - 1, c - 1];
 
-            int[,] ans = new int[m, n];
+            int[][] ans = new int[m][];
             for (int r = 0; r < m; r++)
             {
+                ans[r] = new int[n];
                 for (int c = 0; c < n; c++)
                 {
                     int r1 = Math.Max(0, r - K), c1 = Math.Max(0, c - K);
@@ -28,8 +29,8 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
                     r1++;
                     c1++;
                     r2++;
-                    c2++; // Since `sum` start with 1 so we need to increase r1, c1, r2, c2 by 1
-                    ans[r, c] = sum[r2, c2] - sum[r2, c1 - 1] - sum[r1 - 1, c2] + sum[r1 - 1, c1 - 1];
+                    c2++;
+                    ans[r][c] = sum[r2, c2] - sum[r2, c1 - 1] - sum[r1 - 1, c2] + sum[r1 - 1, c1 - 1];
                 }
             }
 

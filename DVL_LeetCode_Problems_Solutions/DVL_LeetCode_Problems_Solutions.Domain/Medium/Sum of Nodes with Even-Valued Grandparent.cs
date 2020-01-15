c587f3e827023571e82fe5e68817dec4234d3624
DVL_KinoMan <dvali.daviti@gmail.com>
2020-01-15 06:39:27 +0400

    Sum of Nodes with Even-Valued Grandparent

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Sum of Nodes with Even-Valued Grandparent.cs
@@ -0,0 +1,25 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Sum of Nodes with Even-Valued Grandparent (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int SumEvenGrandparent(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int sum = 0;
+            if (root.val % 2 == 0)
+                sum += (root.left?.left?.val ?? 0) + (root.left?.right?.val ?? 0)
+                       + (root.right?.left?.val ?? 0) + (root.right?.right?.val ?? 0);
+
+            return sum + SumEvenGrandparent(root.left) + SumEvenGrandparent(root.right);
+        }
+    }
+}

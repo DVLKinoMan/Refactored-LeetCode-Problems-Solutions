ffb53425add03661ae3a74672ed10ad3effd42c3
D.Dvali <dvali.daviti@gmail.com>
2020-01-17 10:18:11 +0400

    Maximum Product of Three Numbers

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Maximum Product of Three Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Maximum Product of Three Numbers (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            int product1 = 1;
+            for (int i = nums.Length - 3; i < nums.Length; i++)
+                product1 *= nums[i];
+            int product2 = nums[0] * nums[1] * nums[nums.Length - 1];
+            return Math.Max(product1, product2);
+        }
+    }
+}

8e7c31bbd3f0d35ea8ebb13921be80f219fae8bc
D.Dvali <dvali.daviti@gmail.com>
2020-01-23 10:34:07 +0400

    Range Addition II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Range Addition II.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Range Addition II (Mine)
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <param name="ops"></param>
+        /// <returns></returns>
+        public static int MaxCount(int m, int n, int[][] ops)
+        {
+            if (ops.Length == 0)
+                return m * n;
+
+            int minA = int.MaxValue, minB = int.MaxValue;
+            foreach (var op in ops)
+            {
+                minA = Math.Min(minA, op[0]);
+                minB = Math.Min(minB, op[1]);
+            }
+
+            return minA * minB;
+        }
+    }
+}

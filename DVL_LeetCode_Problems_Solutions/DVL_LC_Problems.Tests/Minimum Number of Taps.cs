b4474ad1d9c2b9817f03a035a458279b3db1ed0e
D.Dvali <dvali.daviti@gmail.com>
2020-01-23 13:38:21 +0400

    Minimum Number of Taps to Open to Water a Garden

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/Minimum Number of Taps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestClass]
+    public class Minimum_Number_of_Taps
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //MinTaps(7,
+            //    new int[] {1, 2, 1, 0, 2, 1, 0, 1});
+            //MinTaps(8,
+            //    new int[] {4, 0, 0, 0, 4, 0, 0, 0, 4});
+            MinTaps(9,
+                new int[]
+                {
+                    0, 5, 0, 3, 3, 3, 1, 4, 0, 4
+                });
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Minimum Number of Taps to Open to Water a Garden.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Minimum Number of Taps to Open to Water a Garden (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="ranges"></param>
+        /// <returns></returns>
+        public static int MinTaps(int n, int[] ranges)
+        {
+            var intervals = new (int x, int y)[ranges.Length];
+            for (int i = 0; i < ranges.Length; i++)
+                intervals[i] = (i - ranges[i] < 0 ? 0 : i - ranges[i],
+                    i + ranges[i] > n ? n : i + ranges[i]);
+
+            intervals = intervals.OrderBy(interval => interval.x).ToArray();
+            var prevInterval = intervals[0];
+            int count = 1;
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                var currInterval = intervals[i];
+                if (currInterval.x == prevInterval.x)
+                    prevInterval = currInterval.y > prevInterval.y ? currInterval : prevInterval;
+                else if (currInterval.y > prevInterval.y)
+                {
+                    if (currInterval.x > prevInterval.y)
+                        return -1;
+                    else if (currInterval.x <= prevInterval.x)
+                        prevInterval = (prevInterval.x, currInterval.y);
+                    else
+                    {
+                        count++;
+                        prevInterval = (Math.Max(prevInterval.y, currInterval.x), currInterval.y);
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}

aa03649dfc6fd4a423f76bf479ccaf9fdda9e39a
DVL_KinoMan <dvali.daviti@gmail.com>
2020-01-25 11:57:59 +0400

    Predict the Winner

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Predict the Winner.cs
@@ -0,0 +1,28 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Predict the Winner (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static bool PredictTheWinner(int[] nums)
+        {
+            return SlidingWindow(0, nums.Length - 1);
+
+            bool SlidingWindow(int sInd, int eInd, int fScore = 0, int sScore = 0, bool firstTurn = true)
+            {
+                if (sInd > eInd)
+                    return fScore >= sScore;
+
+                if (firstTurn)
+                    return SlidingWindow(sInd + 1, eInd, fScore+nums[sInd], sScore, false) || 
+                           SlidingWindow(sInd, eInd - 1, fScore+nums[eInd], sScore,false);
+
+                return SlidingWindow(sInd + 1, eInd, fScore, sScore+nums[sInd]) &&
+                       SlidingWindow(sInd, eInd - 1, fScore, sScore + nums[eInd]);
+            }
+        }
+    }
+}

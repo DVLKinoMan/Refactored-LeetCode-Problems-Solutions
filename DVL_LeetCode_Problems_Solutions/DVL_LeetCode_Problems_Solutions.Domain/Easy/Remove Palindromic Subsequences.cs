47c09d6c40a4e9f3eb045ac70195371422fd06ee
D.Dvali <dvali.daviti@gmail.com>
2020-01-27 11:01:46 +0400

    Remove Palindromic Subsequences

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Remove Palindromic Subsequences.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Remove Palindromic Subsequences (Not Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int RemovePalindromeSub(string s)
+        {
+            return string.IsNullOrEmpty(s) ? 0 :
+                s == string.Join("", new StringBuilder(s).ToString().Reverse()) ? 1 : 2;
+        }
+    }
+}

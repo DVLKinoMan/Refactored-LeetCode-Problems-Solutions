18c7a43052a52f85dcb7470541d80632d29c60cb
D.Dvali <dvali.daviti@gmail.com>
2020-01-28 11:31:42 +0400

    Iterator for Combination

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/CombinationIteratorTester.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestClass]
+    public class CombinationIteratorTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var iterator = new CombinationIterator("chp", 1);
+
+            var k1 = iterator.HasNext(); 
+            var k2 = iterator.Next(); // returns "ab"
+            var k3 = iterator.HasNext();// returns "ab"
+            var k4 = iterator.HasNext(); // returns true
+            var k5 = iterator.Next(); // returns true
+            var k6 = iterator.Next(); // returns "ac"
+            var k7 = iterator.HasNext(); // returns "bc"
+            var k8 = iterator.HasNext(); // returns false
+            var k9 = iterator.HasNext(); // returns false
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Iterator for Combination.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public class CombinationIterator
+        {
+            private readonly string _str;
+            private readonly int _len;
+            private int _index = 0;
+            private readonly List<string> _list;
+
+            public CombinationIterator(string characters, int combinationLength)
+            {
+                this._str = characters;
+                this._len = combinationLength;
+                this._list = Dfs().ToList();
+            }
+
+            public string Next()
+            {
+                return this._list[this._index++];
+            }
+
+            public bool HasNext()
+            {
+                return this._index < this._list.Count;
+            }
+
+            private IEnumerable<string> Dfs()
+            {
+                foreach (var it in InnerDfs(new StringBuilder()))
+                    yield return it;
+
+                IEnumerable<string> InnerDfs(StringBuilder builder, int stIndex = 0, int currLen = 0)
+                {
+                    if (currLen == this._len)
+                    {
+                        yield return builder.ToString();
+                        yield break;
+                    }
+
+                    for (int i2 = stIndex; i2 < this._str.Length; i2++)
+                    {
+                        builder.Append(this._str[i2]);
+
+                        foreach (var dfs in InnerDfs(builder, i2 + 1, currLen + 1))
+                            yield return dfs;
+
+                        builder.Remove(builder.Length - 1, 1);
+                    }
+                }
+            }
+        }
+    }
+}

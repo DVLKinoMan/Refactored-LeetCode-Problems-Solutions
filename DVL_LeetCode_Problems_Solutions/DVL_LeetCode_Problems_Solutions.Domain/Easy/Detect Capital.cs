ea17cb1a28f4fbc98461f4bd69cd379d2ff98a63
D.Dvali <dvali.daviti@gmail.com>
2020-01-30 10:13:50 +0400

    Detect Capital

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Detect Capital.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Detect Capital (Mine)
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static bool DetectCapitalUse(string word)
+        {
+            return word.All(char.IsUpper) || 
+                   word.All(char.IsLower) || 
+                   char.IsUpper(word[0]) && word.Skip(1).All(char.IsLower);
+        }
+    }
+}

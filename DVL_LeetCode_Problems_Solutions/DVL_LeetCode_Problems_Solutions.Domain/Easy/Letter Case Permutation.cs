80d640b7c73ddb59a6a9c56d80e7b3b124dbad31
DVL_KinoMan <dvali.daviti@gmail.com>
2020-02-01 14:19:27 +0400

    Letter Case Permutation

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Letter Case Permutation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Letter Case Permutation (Mine)
+        /// </summary>
+        /// <param name="S"></param>
+        /// <returns></returns>
+        public static IList<string> LetterCasePermutation(string S)
+        {
+            var list = new List<string>();
+            Dfs(new StringBuilder(), 0);
+            return list;
+
+            void Dfs(StringBuilder str, int i)
+            {
+                if (i >= S.Length)
+                {
+                    list.Add(str.ToString());
+                    return;
+                }
+
+                if (char.IsLetter(S[i]))
+                {
+                    str.Append(char.ToUpper(S[i]));
+                    Dfs(str, i + 1);
+                    str.Remove(i, 1);
+                    str.Append(char.ToLower(S[i]));
+                    Dfs(str, i + 1);
+                    str.Remove(i, 1);
+                }
+                else
+                {
+                    str.Append(S[i]);
+                    Dfs(str, i + 1);
+                    str.Remove(i, 1);
+                }
+            }
+        }
+    }
+}

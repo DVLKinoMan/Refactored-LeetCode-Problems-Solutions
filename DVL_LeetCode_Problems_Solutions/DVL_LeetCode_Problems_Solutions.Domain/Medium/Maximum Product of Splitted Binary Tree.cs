926be0ecc827d2c9d1a96b078489fa86dfd878a3
D.Dvali <dvali.daviti@gmail.com>
2020-02-03 10:54:24 +0400

    Maximum Product of Splitted Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Maximum Product of Splitted Binary Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Maximum Product of Splitted Binary Tree (Not Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int MaxProduct(TreeNode root)
+        {
+            int mod = (int) Math.Pow(10, 9) + 7;
+            long totalTreeSum = 0, result = 0;
+            totalTreeSum = SumUnder(root);
+            SumUnder(root);
+            return (int) (result % mod);
+
+            int SumUnder(TreeNode node)
+            {
+                if (node == null)
+                    return 0;
+                long sum = SumUnder(node.left) + SumUnder(node.right) + node.val;
+                result = Math.Max(result, sum * (totalTreeSum - sum));
+                return (int) (sum % mod);
+            }
+        }
+    }
+}

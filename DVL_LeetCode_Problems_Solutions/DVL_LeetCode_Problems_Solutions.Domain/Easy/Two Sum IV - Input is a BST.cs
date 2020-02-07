1bd6f86b724cce6d2d8b72d2e0fb193967fef6dc
D.Dvali <dvali.daviti@gmail.com>
2020-02-07 18:39:53 +0400

    Two Sum IV - Input is a BST

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Two Sum IV - Input is a BST.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Two Sum IV - Input is a BST (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static bool FindTarget(TreeNode root, int k) {
+            var set = new HashSet<int>();
+            return Dfs(root);
+            
+            bool Dfs(TreeNode node)
+            {
+                if (node == null)
+                    return false;
+
+                if (set.Contains(node.val))
+                    return true;
+
+                set.Add(k - node.val);
+                return Dfs(node.left) || Dfs(node.right);
+            }
+        }
+    }
+}

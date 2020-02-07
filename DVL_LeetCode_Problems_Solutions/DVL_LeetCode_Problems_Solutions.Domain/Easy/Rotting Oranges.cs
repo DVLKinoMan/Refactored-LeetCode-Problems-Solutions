8bc2ccfd14a3de2e527a142c6b431229124f18c2
D.Dvali <dvali.daviti@gmail.com>
2020-02-07 13:15:24 +0400

    Ref

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Rotting Oranges.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Rotting Oranges.cs
@@ -9,26 +9,29 @@
         /// <returns></returns>
         public static int OrangesRotting(int[][] grid)
         {
-            int fresh = 0, d = 0;
+            int freshCount = 0, d = 0;
             for (int i = 0; i < grid.Length; ++i)
             for (int j = 0; j < grid[i].Length; ++j)
                 if (grid[i][j] == 1) 
-                    fresh++;
+                    freshCount++;
             
-            for (int old_fresh = fresh; fresh > 0; ++d, old_fresh = fresh) {
+            for (int old_fresh = freshCount; freshCount > 0; ++d, old_fresh = freshCount) {
                 for (int i = 0; i < grid.Length; ++i)
                 for (int j = 0; j < grid[i].Length; ++j)
                     if (grid[i][j] == d + 2)
-                        fresh -= rot(grid, i + 1, j, d) + rot(grid, i - 1, j, d) + rot(grid, i, j + 1, d) + rot(grid, i, j - 1, d);
-                if (fresh == old_fresh) return -1;
+                        freshCount -= Rot(i + 1, j, d) + Rot(i - 1, j, d) + 
+                                      Rot(i, j + 1, d) + Rot(i, j - 1, d);
+                if (freshCount == old_fresh) 
+                    return -1;
             }
             
             return d;
 
-            int rot(int[][] g, int i, int j, int de)
+            int Rot(int i, int j, int de)
             {
-                if (i < 0 || j < 0 || i >= g.Length || j >= g[i].Length || g[i][j] != 1) return 0;
-                g[i][j] = de + 3;
+                if (i < 0 || j < 0 || i >= grid.Length || j >= grid[i].Length || grid[i][j] != 1)
+                    return 0;
+                grid[i][j] = de + 3;
                 return 1;
             }
         }

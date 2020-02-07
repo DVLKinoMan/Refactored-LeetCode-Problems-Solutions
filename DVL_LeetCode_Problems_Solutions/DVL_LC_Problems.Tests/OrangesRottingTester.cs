b4e54d39d75381f4c86e2cc4a05025489d6d6f27
D.Dvali <dvali.daviti@gmail.com>
2020-02-07 14:20:58 +0400

    Add TestCases for OrangesRottingTester.cs

--- DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/OrangesRottingTester.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/OrangesRottingTester.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
 
 namespace DVL_LC_Problems.Tests
@@ -6,17 +7,45 @@ namespace DVL_LC_Problems.Tests
     [TestFixture]
     public class OrangesRottingTester
     {
-        [Test]
-        public void TestMethod1()
+
+        private static readonly object[] CaseSource =
         {
-           int res= OrangesRotting(new int[][]
+            new object[]
             {
-                new int[] {2, 1, 1},
-                new int[] {1, 1, 0},
-                new int[] {0, 1, 1}
-            });
-            
-            Assert.That(res, Is.EqualTo(4));
+                new int[][]
+                {
+                    new int[] {2, 1, 1},
+                    new int[] {1, 1, 0},
+                    new int[] {0, 1, 1}
+                },
+                4
+            },
+            new object[]
+            {
+                new int[][]
+                {
+                    new int[] {2, 1, 1},
+                    new int[] {0, 1, 1},
+                    new int[] {1, 0, 1}
+                },
+                -1
+            },
+            new object[]
+            {
+                new int[][]
+                {
+                    new int[] {0, 2}
+                },
+                0
+            }
+        };
+        
+        [Test, TestCaseSource("CaseSource")]
+        public void TestMethod1(int[][] grid, int expected)
+        {
+            int res = OrangesRotting(grid);
+
+            Assert.That(res, Is.EqualTo(expected));
         }
     }
 }

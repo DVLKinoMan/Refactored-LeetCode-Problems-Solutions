ad56a7cc94216f6dd128f2b2c3a8ebe23d730d18
DVL_KinoMan <dvali.daviti@gmail.com>
2020-02-08 14:01:18 +0400

    Fizz Buzz

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Fizz Buzz.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Fizz Buzz (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static IList<string> FizzBuzz(int n)
+        {
+            var list = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                switch (i % 3)
+                {
+                    case 0 when i % 5 == 0:
+                        list.Add("FizzBuzz");
+                        break;
+                    case 0:
+                        list.Add("Fizz");
+                        break;
+                    default:
+                    {
+                        list.Add(i % 5 == 0 ? "Buzz" : i.ToString());
+                        break;
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}

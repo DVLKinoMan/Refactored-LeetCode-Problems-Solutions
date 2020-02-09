9f62d076c47b017aae4f1281938517c9566ac8a6
DVL_KinoMan <dvali.daviti@gmail.com>
2020-02-09 06:55:49 +0400

    Weekly Contest 175

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Weekly Contests/Weekly Contest 175.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Check If N and Its Double Exist (Did not work)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static bool CheckIfExist(int[] arr)
+        {
+            var set = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+                set.Add(arr[i] * 2);
+
+            for (int i = 0; i < arr.Length; i++)
+                if (set.Contains(arr[i]))
+                    return true;
+
+            return false;
+        }
+    }
+}

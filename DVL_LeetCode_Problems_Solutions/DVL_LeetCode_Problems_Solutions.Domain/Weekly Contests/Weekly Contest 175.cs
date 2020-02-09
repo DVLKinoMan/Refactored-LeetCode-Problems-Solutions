4c63e7592d44aef822aed1aee08ffc96a691a55c
DVL_KinoMan <dvali.daviti@gmail.com>
2020-02-09 16:17:20 +0400

    Check If N and Its Double Exist

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Weekly Contests/Weekly Contest 175.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Weekly Contests/Weekly Contest 175.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
         /// <summary>
-        /// Check If N and Its Double Exist (Did not work)
+        /// Check If N and Its Double Exist (Mine)
         /// </summary>
         /// <param name="arr"></param>
         /// <returns></returns>
         public static bool CheckIfExist(int[] arr)
         {
             var set = new HashSet<int>();
-            for (int i = 0; i < arr.Length; i++)
-                set.Add(arr[i] * 2);
-
-            for (int i = 0; i < arr.Length; i++)
-                if (set.Contains(arr[i]))
-                    return true;
-
-            return false;
+            int zerosCount = 0;
+            foreach (var num in arr)
+            {
+                if (num == 0)
+                    zerosCount++;
+                set.Add(num * 2);
+            }
+        
+            return zerosCount > 1 || arr.Any(num => set.Contains(num) && num != 0);
         }
     }
 }

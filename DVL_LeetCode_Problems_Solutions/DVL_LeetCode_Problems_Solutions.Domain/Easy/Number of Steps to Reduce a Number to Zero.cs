adc49ee19c4b47be99660cdae1cf28487daa7532
D.Dvali <dvali.daviti@gmail.com>
2020-02-11 10:16:03 +0400

    Number of Steps to Reduce a Number to Zero

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Number of Steps to Reduce a Number to Zero.cs
@@ -0,0 +1,24 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Number of Steps to Reduce a Number to Zero (Mine)
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int NumberOfSteps (int num)
+        {
+            int count = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                    num /= 2;
+                else num--;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}

1588c34bdee3c6928b8a43882e853be6c07165fe
D.Dvali <dvali.daviti@gmail.com>
2020-02-24 13:36:59 +0400

    Validate Binary Tree Nodes

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Validate Binary Tree Nodes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Validate Binary Tree Nodes (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="leftChild"></param>
+        /// <param name="rightChild"></param>
+        /// <returns></returns>
+        public static bool ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild)
+        {
+            var visitedNodes = new HashSet<int>() {0};
+            for (int i = 0; i < n; i++)
+            {
+                if (!visitedNodes.Contains(i))
+                    return false;
+
+                if (leftChild[i] != -1 && !visitedNodes.Add(leftChild[i]))
+                    return false;
+                if (rightChild[i] != -1 && !visitedNodes.Add(rightChild[i]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}

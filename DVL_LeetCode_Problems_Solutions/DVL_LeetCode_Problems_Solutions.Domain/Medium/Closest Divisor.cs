cfb21f2737e608569dde4a9390161d2abb01f0a0
D.Dvali <dvali.daviti@gmail.com>
2020-02-25 14:28:10 +0400

    Closest Divisor

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Closest Divisor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Closest Divisor (Not Mine)
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int[] ClosestDivisors(int num)
+        {
+            int num1 = num + 1, num2 = num + 2, dist = int.MaxValue;
+
+            int[] result = new int[2];
+            for (int i = (int) Math.Sqrt(num2); i >= 1; i--)
+                if (num2 % i == 0)
+                {
+                    result = new[] {i, num2 / i};
+                    dist = num2 / i - i;
+                    break;
+                }
+
+            for (int i = (int) Math.Sqrt(num1); i >= 1; i--)
+                if (num1 % i == 0)
+                {
+                    if (num1 / i - i < dist)
+                        result = new [] {i, num1 / i};
+                    break;
+                }
+
+            return result;
+        }
+    }
+}

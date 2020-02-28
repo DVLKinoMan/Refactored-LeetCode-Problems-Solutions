4cbfd02062ded2fee2eaa969a9f10f1b8b051e20
DVL_KinoMan <dvali.daviti@gmail.com>
2020-02-28 09:44:45 +0400

    Buddy Strings (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Buddy Strings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static bool BuddyStrings(string A, string B)
+        {
+            if (A.Length != B.Length)
+                return false;
+            
+            var notEqual1= new HashSet<char>();
+            var notEqual2=new HashSet<char>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] != B[i])
+                {
+                    if (notEqual1.Count > 2)
+                        return false;
+                    notEqual1.Add(A[i]);
+                    notEqual2.Add(B[i]);
+                }
+            }
+
+            return (notEqual1.Count == 0 && A.Distinct().Count() != A.Length) ||
+                   notEqual1.All(not => notEqual2.Contains(not));
+        }
+    }
+}

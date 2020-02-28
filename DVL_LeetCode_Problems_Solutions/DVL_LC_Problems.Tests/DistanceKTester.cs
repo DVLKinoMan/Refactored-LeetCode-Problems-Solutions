b66d75e3a1b36d556875ec2b84f3956c9f94fa4d
D.Dvali <dvali.daviti@gmail.com>
2020-02-28 17:16:36 +0400

    All Nodes Distance K in Binary Tree (Not Working)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/DistanceKTester.cs
@@ -0,0 +1,80 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+using NUnit.Framework;
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestFixture]
+    public class DistanceKTester
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var target =
+                new TreeNode(5)
+                {
+                    left = new TreeNode(6),
+                    right = new TreeNode(2)
+                    {
+                        left = new TreeNode(7),
+                        right = new TreeNode(4)
+                    }
+                };
+            
+            var root = new TreeNode(3)
+            {
+                left = target,
+                right = new TreeNode(1)
+                {
+                    left = new TreeNode(0),
+                    right = new TreeNode(8)
+                }
+            };
+            
+            DistanceK(root, target, 2);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            var target = new TreeNode(2);
+            
+            var root = new TreeNode(0)
+            {
+                left = new TreeNode(1)
+                {
+                    left = new TreeNode(3),
+                    right = target
+                },
+            };
+            
+            DistanceK(root, target, 1);
+        }
+
+        [Test]
+        public void TestMethod3()
+        {   
+            var target = new TreeNode(5)
+            {
+                left = new TreeNode(6),
+                right = new TreeNode(2)
+                {
+                    left = new TreeNode(7),
+                    right = new TreeNode(4)
+                }
+            };
+            
+            var root = new TreeNode(3)
+            {
+                left = target,
+                right = new TreeNode(1)
+                {
+                    left = new TreeNode(0),
+                    right = new TreeNode(8)
+                }
+            };
+            
+            DistanceK(root, target, 2);
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/All Nodes Distance K in Binary Tree.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// All Nodes Distance K in Binary Tree (Not Working)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="target"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static IList<int> DistanceK(TreeNode root, TreeNode target, int K) {
+            var dict = new Dictionary<int, List<int>>();
+            var result = new List<int>();
+
+            if (K == 0)
+            {
+                result.Add(target.val);
+                return result;
+            }
+            
+            Build(null, root);
+
+            if (dict.Count == 0)
+                return result;
+
+            Dfs(target.val);
+
+            return result.Where(r=>r != target.val)
+                         .Distinct()
+                         .ToList();
+
+            void AddOrUpdate(int key, int val)
+            {
+                if (dict.ContainsKey(key))
+                    dict[key].Add(val);
+                else dict[key] = new List<int>() {val};
+            }
+
+            void Build(TreeNode parent, TreeNode child)
+            {
+                if (parent != null)
+                {
+                    AddOrUpdate(parent.val, child.val);
+                    AddOrUpdate(child.val, parent.val);
+                }
+
+                if (child?.left != null)
+                    Build(child, child.left);
+                if (child?.right != null)
+                    Build(child, child.right);
+            }
+
+            void Dfs(int currVal, int height = 0)
+            {
+                if (height == K - 1)
+                {
+                    result.AddRange(dict[currVal]);
+                    return;
+                }
+
+                foreach (var val in dict[currVal])
+                    Dfs(val, height + 1);
+            }
+        }
+    }
+}

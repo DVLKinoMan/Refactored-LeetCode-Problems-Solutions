a708dcaa0b67a206aebc211fc75c5b7e16c19b1c
DVL_KinoMan <dvali.daviti@gmail.com>
2020-03-03 16:29:58 +0400

    Rank Transform of an Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Rank Transform of an Array.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+   partial class ProblemSolver
+   {
+       /// <summary>
+       /// Rank Transform of an Array (Mine)
+       /// </summary>
+       /// <param name="arr"></param>
+       /// <returns></returns>
+      public static int[] ArrayRankTransform(int[] arr) {
+        int[] result = new int[arr.Length];
+        
+        if (arr.Length == 0)
+            return result;
+        
+        var dict = new Dictionary<int,int>();
+        foreach (var num in arr)
+            if(!dict.ContainsKey(num))
+                dict.Add(num, 0);
+
+        int count = 1;
+        foreach (var num in dict.OrderBy(d=>d.Key).Select(d=>d.Key))
+            dict[num] += count++;
+
+        for (var i = 0; i < arr.Length; i++)
+            result[i] = dict[arr[i]];
+
+        return result;
+      }
+   }
+}

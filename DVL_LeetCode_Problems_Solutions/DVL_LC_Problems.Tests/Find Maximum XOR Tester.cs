835ccf14262f8fcb0c7531e38507257f2b2b951d
D.Dvali <dvali.daviti@gmail.com>
2020-03-04 11:48:40 +0400

    Maximum XOR of Two Numbers in an Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/Find Maximum XOR Tester.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using static DVL_LeetCode_Problems_Solutions.Domain.ProblemSolver;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestFixture]
+    public class Find_Maximum_XOR_Tester
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            FindMaximumXOR(new int[] {3, 10, 5, 25, 2, 8});
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Maximum XOR of Two Numbers in an Array.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+       /// <summary>
+       /// Maximum XOR of Two Numbers in an Array (Not mine - Do not understand solution)
+       /// </summary>
+       /// <param name="nums"></param>
+       /// <returns></returns>
+        public static int FindMaximumXOR(int[] nums)
+        {
+            int max = 0, mask = 0;
+            for (int i = 31; i >= 0; i--)
+            {
+                mask = mask | (1 << i);
+                var set = new HashSet<int>();
+                foreach (var num in nums)
+                    set.Add(num & mask);
+                int tmp = max | (1 << i);
+                foreach (var prefix in set)
+                    if (set.Contains(tmp ^ prefix))
+                    {
+                        max = tmp;
+                        break;
+                    }
+            }
+
+            return max;
+        }
+    }
+}

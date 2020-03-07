94e9452bcfc39c6194f735a2b72dcee4d9438aa5
DVL_KinoMan <dvali.daviti@gmail.com>
2020-03-07 19:09:56 +0400

    Partition List and Biweekly Contest 21 (where i do not solve anything)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Biweekly Contests/Biweekly Contest 21.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static int LongestZigZag(TreeNode root)
+        {
+            int max = 0;
+            Dfs(root, 0, false, true);
+            Dfs(root, 0, true, true);
+            return max;
+
+            int Dfs(TreeNode node, int depth = 0, bool goLeft = true, bool isRoot = false)
+            {
+                if (node == null)
+                    return depth - 1;
+
+                if (goLeft)
+                {
+                    max = Math.Max(max, Dfs(node.left, depth + 1, false));
+                    if (!isRoot)
+                        max = Math.Max(max, LongestZigZag(node.right));
+                }
+                else
+                {
+                    max = Math.Max(max, Dfs(node.right, depth + 1));
+                    if (!isRoot)
+                        max = Math.Max(max, LongestZigZag(node.left));
+                }
+
+                return max;
+            }
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Partition List.cs
@@ -0,0 +1,44 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Partition List (Mine)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static ListNode Partition(ListNode head, int x)
+        {
+            ListNode lessRoot = null, moreRoot = null, lessPointer = null, morePointer = null;
+            var current = head;
+            while (current != null)
+            {
+                if (current.val < x)
+                {
+                    if (lessRoot == null)
+                        lessRoot = current;
+                    else lessPointer.next = current;
+                    lessPointer = current;
+                }
+                else
+                {
+                    if (moreRoot == null)
+                        moreRoot = current;
+                    else morePointer.next = current;
+                    morePointer = current;
+                }
+
+                current = current.next;
+            }
+
+            if (lessPointer != null)
+                lessPointer.next = moreRoot;
+            if (morePointer != null)
+                morePointer.next = null;
+            return lessRoot ?? moreRoot;
+        }
+    }
+}

f25f91f65955044e22b7dfc693fc22fd8fd3d14b
DVL_KinoMan <dvali.daviti@gmail.com>
2020-03-08 13:40:24 +0400

    Generate a String With Characters That Have Odd Counts

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Generate a String With Characters That Have Odd Counts.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+   partial class ProblemSolver
+   {
+       /// <summary>
+       /// Generate a String With Characters That Have Odd Counts (Mine)
+       /// </summary>
+       /// <param name="n"></param>
+       /// <returns></returns>
+      public static string GenerateTheString(int n) {
+          if (n % 2 == 0)
+              return string.Join("", Enumerable.Repeat('a', n - 1)) + 'b';
+
+          return string.Join("", Enumerable.Repeat('a', n));
+      }
+   }
+}

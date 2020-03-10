8b2abbaa75b6cd6fc8bbda4cae9b05ec31c48cb4
D.Dvali <dvali.daviti@gmail.com>
2020-03-10 14:22:56 +0400

    Missing Number

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Missing Number.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Missing Number (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MissingNumber(int[] nums)
+        {
+            var set = new HashSet<int>(Enumerable.Range(0, nums.Length + 1));
+            foreach (var num in nums)
+                set.Remove(num);
+            return set.First();
+        }
+    }
+}

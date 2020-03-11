59fa1b1088d043830203657ff616d8bf055dd351
D.Dvali <dvali.daviti@gmail.com>
2020-03-11 10:54:06 +0400

    Longest ZigZag Path in a Binary Tree

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Biweekly Contests/Biweekly Contest 21.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Biweekly Contests/Biweekly Contest 21.cs
@@ -5,32 +5,25 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
 {
     partial class ProblemSolver
     {
+        /// <summary>
+        /// Longest ZigZag Path in a Binary Tree (Not Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
         public static int LongestZigZag(TreeNode root)
         {
-            int max = 0;
-            Dfs(root, 0, false, true);
-            Dfs(root, 0, true, true);
-            return max;
+            return Dfs(root).result;
 
-            int Dfs(TreeNode node, int depth = 0, bool goLeft = true, bool isRoot = false)
+            (int left, int right, int result) Dfs(TreeNode node)
             {
                 if (node == null)
-                    return depth - 1;
-
-                if (goLeft)
-                {
-                    max = Math.Max(max, Dfs(node.left, depth + 1, false));
-                    if (!isRoot)
-                        max = Math.Max(max, LongestZigZag(node.right));
-                }
-                else
-                {
-                    max = Math.Max(max, Dfs(node.right, depth + 1));
-                    if (!isRoot)
-                        max = Math.Max(max, LongestZigZag(node.left));
-                }
-
-                return max;
+                    return (-1, -1, -1);
+                (int left, int right, int result) left = Dfs(node.left), right = Dfs(node.right);
+                int res = Math.Max(
+                    Math.Max(left.right, right.left) + 1,
+                    Math.Max(left.result, right.result)
+                );
+                return (left.right + 1, right.left + 1, res);
             }
         }
     }

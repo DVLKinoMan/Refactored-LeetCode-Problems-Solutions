00160b45b09c68bf9f94d4a4c49b0890292a14a8
D.Dvali <dvali.daviti@gmail.com>
2020-03-13 16:07:18 +0400

    Largest Perimeter Triangle

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Largest Perimeter Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Largest Perimeter Triangle (Not Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int LargestPerimeter(int[] A)
+        {
+            Array.Sort(A);
+            for (int i = A.Length - 1; i > 1; --i)
+                if (A[i] < A[i - 1] + A[i - 2])
+                    return A[i] + A[i - 1] + A[i - 2];
+            return 0;
+        }
+    }
+}

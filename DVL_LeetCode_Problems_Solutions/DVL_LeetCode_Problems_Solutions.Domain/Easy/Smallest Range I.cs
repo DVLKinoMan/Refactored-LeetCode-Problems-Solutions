bd99f1950909be2d7bd35515342b075dd8bef0fc
D.Dvali <dvali.daviti@gmail.com>
2020-03-19 12:45:45 +0400

    Smallest Range I

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Smallest Range I.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Smallest Range I (Mine)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static int SmallestRangeI(int[] A, int K)
+        {
+            if (A.Length == 1)
+                return 0;
+
+            int max = A.Max(), min = A.Min();
+            int mid = (max + min) / 2;
+            int start = min + K > mid ? mid : min + K;
+            int end = max - K < mid ? mid : max - K;
+            return end - start;
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Maximum Performance of a Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        ///todo: not solved
+        // public static int MaxPerformance(int n, int[] speed, int[] efficiency, int k) 
+        // {
+        //     int[][] ess = new int[n][];
+        //     for (int i = 0; i < n; ++i)
+        //         ess[i] = new int[] {efficiency[i], speed[i]};
+        //     ;
+        //     PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
+        //     long res = 0, sumS = 0;
+        //     foreach (var es in ess.OrderBy(arr => arr[0])) {
+        //         pq.add(es[1]);
+        //         sumS = (sumS + es[1]);
+        //         if (pq.size() > k) sumS -= pq.poll();
+        //         res = Math.Max(res, (sumS * es[0]));
+        //     }
+        //     return (int) (res % (long)(1e9 + 7));
+        // }
+    }
+}

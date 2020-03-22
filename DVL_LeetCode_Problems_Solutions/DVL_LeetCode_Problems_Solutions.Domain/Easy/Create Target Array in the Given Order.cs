f27743913d6fc9f7b2fa740711cb30bbf1c93864
DVL_KinoMan <dvali.daviti@gmail.com>
2020-03-22 15:03:39 +0400

    Create Target Array in the Given Order

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Create Target Array in the Given Order.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Create Target Array in the Given Order (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            var target = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+                target.Insert(index[i], nums[i]);
+
+            return target.ToArray();
+        }
+    }
+}

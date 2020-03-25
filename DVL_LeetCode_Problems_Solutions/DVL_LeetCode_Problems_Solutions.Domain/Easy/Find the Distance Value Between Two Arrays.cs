04b901112222ab399273e006a489729300d01960
D.Dvali <dvali.daviti@gmail.com>
2020-03-25 10:45:33 +0400

    Find the Distance Value Between Two Arrays

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Find the Distance Value Between Two Arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find the Distance Value Between Two Arrays (Mine)
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <param name="arr2"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static int FindTheDistanceValue(int[] arr1, int[] arr2, int d)
+        {
+            int count = 0;
+            foreach (var num1 in arr1)
+            {
+                foreach (var num2 in arr2)
+                    if (Math.Abs(num1 - num2) <= d)
+                    {
+                        count--;
+                        break;
+                    }
+
+                count++;
+            }
+
+            return count;
+        }
+    }
+}

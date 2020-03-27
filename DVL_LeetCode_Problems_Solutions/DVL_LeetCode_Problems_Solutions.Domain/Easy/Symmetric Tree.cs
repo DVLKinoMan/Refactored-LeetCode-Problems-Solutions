31d6f9d90b23ef056cbdb16a2d9b143b623a2912
D.Dvali <dvali.daviti@gmail.com>
2020-03-27 13:48:35 +0400

    Symmetric Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Symmetric Tree.cs
@@ -0,0 +1,27 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Symmetric Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static bool IsSymmetric(TreeNode root)
+        {
+            return root == null || IsEqual(root.left, root.right);
+            
+            bool IsEqual(TreeNode node1, TreeNode node2)
+            {
+                if (node1 == null && node2 == null)
+                    return true;
+                if (node1 == null || node2 == null || node1.val != node2.val)
+                    return false;
+
+                return IsEqual(node1.left, node2.right) && IsEqual(node1.right, node2.left);
+            }
+        }
+    }
+}

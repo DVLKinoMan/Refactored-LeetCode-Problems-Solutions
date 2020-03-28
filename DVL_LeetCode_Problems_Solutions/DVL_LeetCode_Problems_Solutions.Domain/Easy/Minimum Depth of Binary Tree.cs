03b1cc719d7196fc37243c4f8a69e50e67913c56
DVL_KinoMan <dvali.daviti@gmail.com>
2020-03-28 17:24:29 +0400

    Minimum Depth of Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Minimum Depth of Binary Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Minimum Depth of Binary Tree (Not Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int left = MinDepth(root.left);
+            int right = MinDepth(root.right);
+            return (left == 0 || right == 0) ? left + right + 1 : Math.Min(left, right) + 1;
+        }
+    }
+}

5cb6047eb05b8dfa02822c953a89105ca7a3387e
D.Dvali <dvali.daviti@gmail.com>
2020-03-30 13:03:17 +0400

    Find Lucky Integer in an Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Find Lucky Integer in an Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find Lucky Integer in an Array (Mine)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int FindLucky(int[] arr)
+        {
+            var counts = new int[501];
+            foreach (var num in arr)
+                counts[num]++;
+
+            int res = -1;
+            for (int i = 1; i < counts.Length; i++)
+                if (counts[i] == i)
+                    res = Math.Max(res, counts[i]);
+
+            return res;
+        }
+    }
+}

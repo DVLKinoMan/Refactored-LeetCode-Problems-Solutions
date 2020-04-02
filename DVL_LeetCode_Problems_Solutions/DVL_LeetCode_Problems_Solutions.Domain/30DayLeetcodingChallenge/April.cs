d2e9aca56299dee388ae2211d0054b4b5e5376f5
D.Dvali <dvali.daviti@gmail.com>
2020-04-02 15:57:03 +0400

    Sum of Left Leaves and 30 Day Coding challenge second problem

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/30DayLeetcodingChallenge/April.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Single Number (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int SingleNumber(int[] nums) {
+            int k = 0;
+            foreach(var num in nums)
+                k^=num;
+            return k;
+        }
+        
+        /// <summary>
+        /// Happy Number (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool IsHappy(int n) 
+        {
+            var set = new HashSet<int>();
+            while (set.Add(n) && n != 1)
+                n = Sum(n);
+
+            return n == 1;
+            
+            int Sum(int k)
+            {
+                int res = 0;
+                while (k != 0)
+                {
+                    res += (int)Math.Pow(k % 10, 2);
+                    k /= 10;
+                }
+
+                return res;
+            }
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Sum of Left Leaves.cs
@@ -0,0 +1,21 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Sum of Left Leaves (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int SumOfLeftLeaves(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            
+            return SumOfLeftLeaves(root.left) + SumOfLeftLeaves(root.right) +
+                   (root.left != null && root.left.left == null && root.left.right == null ? root.left.val : 0);
+        }
+    }
+}

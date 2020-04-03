a01badd62668df5596fc2b71d51ac12006b380a7
D.Dvali <dvali.daviti@gmail.com>
2020-04-03 11:46:19 +0400

    Is Subsequence and 30 Day Leetcoding Challenge 3rd day

--- DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/30DayLeetcodingChallenge/April.cs
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/30DayLeetcodingChallenge/April.cs
@@ -10,37 +10,57 @@ namespace DVL_LeetCode_Problems_Solutions.Domain
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
-        public static int SingleNumber(int[] nums) {
+        public static int SingleNumber(int[] nums)
+        {
             int k = 0;
-            foreach(var num in nums)
-                k^=num;
+            foreach (var num in nums)
+                k ^= num;
             return k;
         }
-        
+
         /// <summary>
         /// Happy Number (Mine)
         /// </summary>
         /// <param name="n"></param>
         /// <returns></returns>
-        public static bool IsHappy(int n) 
+        public static bool IsHappy(int n)
         {
             var set = new HashSet<int>();
             while (set.Add(n) && n != 1)
                 n = Sum(n);
 
             return n == 1;
-            
+
             int Sum(int k)
             {
                 int res = 0;
                 while (k != 0)
                 {
-                    res += (int)Math.Pow(k % 10, 2);
+                    res += (int) Math.Pow(k % 10, 2);
                     k /= 10;
                 }
 
                 return res;
             }
+        }
+
+        /// <summary>
+        /// Maximum Subarray (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MaxSubArray(int[] nums)
+        {
+            int currSum = nums[0], maxSum = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (currSum + nums[i] < nums[i])
+                    currSum = nums[i];
+                else currSum += nums[i];
+                maxSum = Math.Max(maxSum, currSum);
+            }
+
+            return maxSum;
         }
     }
 }
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Is Subsequence.cs
@@ -0,0 +1,24 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Is Subsequence (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static bool IsSubsequence(string s, string t)
+        {
+            int si = 0, ti = 0;
+            while (si < s.Length && ti < t.Length)
+            {
+                if (s[si] == t[ti])
+                    si++;
+                ti++;
+            }
+
+            return si == s.Length;
+        }
+    }
+}

bbf2d0ed80265588170cac828a09c6939508beec
DVL_KinoMan <dvali.daviti@gmail.com>
2020-04-11 21:31:39 +0400

    Delete Node in a Linked List

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Delete Node in a Linked List.cs
@@ -0,0 +1,18 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        ///  Delete Node in a Linked List (Not Mine)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="node"></param>
+        public static void DeleteNode(ListNode node)
+        {
+            node.val=node.next.val;
+            node.next=node.next.next;
+        }
+    }
+}

cfc819a987d56362b471cc36314bf2ec8225d92b
DVL_KinoMan <dvali.daviti@gmail.com>
2020-04-12 11:25:35 +0400

    String Matching in an Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/String Matching in an Array.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// String Matching in an Array (Mine)
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public static IList<string> StringMatching(string[] words)
+        {
+            return words.Where(IsSubstringOfAny).ToList();
+
+            bool IsSubstringOfAny(string str)
+            {
+                foreach (var word in words)
+                {
+                    if (str.Length >= word.Length)
+                        continue;
+                    if (word.Contains(str))
+                        return true;
+                }
+
+                return false;
+            }
+        }
+    }
+}

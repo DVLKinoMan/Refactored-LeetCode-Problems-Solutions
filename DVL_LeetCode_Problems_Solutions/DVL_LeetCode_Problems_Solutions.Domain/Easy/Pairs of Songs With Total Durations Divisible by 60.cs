dfedb9b1c70c25be4ab82deb1965cb4b514e5a86
D.Dvali <dvali.daviti@gmail.com>
2020-04-14 12:54:05 +0400

    Pairs of Songs With Total Durations Divisible by 60

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Pairs of Songs With Total Durations Divisible by 60.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Pairs of Songs With Total Durations Divisible by 60 (Mine)
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static int NumPairsDivisibleBy60(int[] time) 
+        {
+            var dict = new Dictionary<int, int>();
+            int count = 0;
+            foreach (var t in time)
+            {
+                int num = 60 - t % 60;
+                if (dict.ContainsKey(t%60))
+                    count += dict[t%60];
+                dict[num] = dict.ContainsKey(num) ? dict[num] + 1 : 1;
+            }
+
+            return count;
+        }
+    }
+}

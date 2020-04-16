06feb17aa2feb98edfcf99ab8c8091aa117b08ba
D.Dvali <dvali.daviti@gmail.com>
2020-04-16 16:20:27 +0400

    Count Largest Group

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Count Largest Group.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Count Largest Group (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int CountLargestGroup(int n)
+        {
+            var list = Enumerable.Range(1, n)
+                .GroupBy(SumOfDigits)
+                .ToList();
+            int m = list.Max(gr => gr.Count());
+            return list.Count(gr => gr.Count() == m);
+            
+            int SumOfDigits(int k)
+            {
+                int num = 0;
+                while (k != 0)
+                {
+                    num += k % 10;
+                    k /= 10;
+                }
+
+                return num;
+            }
+        }
+    }
+}

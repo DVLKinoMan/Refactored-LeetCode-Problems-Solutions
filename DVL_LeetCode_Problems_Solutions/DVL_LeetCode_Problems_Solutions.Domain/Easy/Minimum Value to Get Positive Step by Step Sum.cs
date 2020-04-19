587ead7d42bd58692711c91e593ec8e4260459e3
DVL_KinoMan <dvali.daviti@gmail.com>
2020-04-19 13:37:44 +0400

    Minimum Value to Get Positive Step by Step Sum

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Minimum Value to Get Positive Step by Step Sum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+   partial class ProblemSolver
+   {
+      /// <summary>
+      /// Minimum Value to Get Positive Step by Step Sum (Mine)
+      /// </summary>
+      /// <param name="nums"></param>
+      /// <returns></returns>
+      public static int MinStartValue(int[] nums)
+      {
+         int sum = 0, minSum = int.MaxValue;
+         foreach (var num in nums)
+         {
+            sum += num;
+            minSum = Math.Min(minSum, sum);
+         }
+
+         return minSum > 0 ? 1 : 1 - minSum;
+      }
+   }
+}

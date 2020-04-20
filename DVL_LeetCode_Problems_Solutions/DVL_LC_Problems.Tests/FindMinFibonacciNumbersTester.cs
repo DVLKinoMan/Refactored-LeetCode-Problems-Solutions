0cac36b5d3de8b14d45446267d4324bd8cd051f4
DVL_KinoMan <dvali.daviti@gmail.com>
2020-04-20 12:28:30 +0400

    Find the Minimum Number of Fibonacci Numbers Whose Sum Is K

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LC_Problems.Tests/FindMinFibonacciNumbersTester.cs
@@ -0,0 +1,15 @@
+﻿using DVL_LeetCode_Problems_Solutions.Domain;
+using NUnit.Framework;
+
+namespace DVL_LC_Problems.Tests
+{
+    [TestFixture]
+    public class FindMinFibonacciNumbersTester
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            ProblemSolver.FindMinFibonacciNumbers(19);
+        }
+    }
+}
---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Find the Minimum Number of Fibonacci Numbers Whose Sum Is K.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Find the Minimum Number of Fibonacci Numbers Whose Sum Is K (Mine)
+        /// </summary>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int FindMinFibonacciNumbers(int k) 
+        {
+            var fibonacciNums = new List<int>(){1,1};
+            var set = new HashSet<int>() {1};
+            int index = 1;
+            while (fibonacciNums[index] < k)
+            {
+                fibonacciNums.Add(fibonacciNums[index - 1] + fibonacciNums[index++]);
+                set.Add(fibonacciNums[index]);
+            }
+
+            return Find(k);
+
+            int Find(int c)
+            {
+                if (set.Contains(c))
+                    return 1;
+
+                int ind = 1;
+                while (fibonacciNums[ind] < c)
+                    ind++;
+
+                return Find(c - fibonacciNums[ind - 1]) + 1;
+            }
+        }
+    }
+}

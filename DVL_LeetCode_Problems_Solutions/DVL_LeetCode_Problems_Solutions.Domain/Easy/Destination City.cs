cadb21147eb91c172a9751aad8491019c03a158e
D.Dvali <dvali.daviti@gmail.com>
2020-05-07 12:56:05 +0400

    Destination City

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Destination City.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Destination City (Mine)
+        /// </summary>
+        /// <param name="paths"></param>
+        /// <returns></returns>
+        public static string DestCity(IList<IList<string>> paths)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (var path in paths)
+                dict[path[0]] = path[1];
+            return dict.First(keyValue => !dict.ContainsKey(keyValue.Value)).Value;
+        }
+    }
+}

5109b90979c246abca88aac2d0e745099dab9f03
D.Dvali <dvali.daviti@gmail.com>
2020-05-13 10:43:44 +0400

    Combinations

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Combinations.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Combinations (Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static IList<IList<int>> Combine(int n, int k)
+        {
+            var result = new List<IList<int>>();
+            Dfs(1, new List<int>());
+            return result;
+
+            void Dfs(int j, List<int> list)
+            {
+                if (list.Count == k)
+                {
+                    result.Add(list.ToList());
+                    return;
+                }
+
+                for (int i = j; i <= n; i++)
+                {
+                    list.Add(i);
+                    Dfs(i + 1, list);
+                    list.RemoveAt(list.Count - 1);
+                }
+            }
+        }
+    }
+}

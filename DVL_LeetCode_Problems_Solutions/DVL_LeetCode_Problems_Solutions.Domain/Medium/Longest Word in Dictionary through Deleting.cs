072ef42f1d8c526eaff75f3c7c86481fd107176c
D.Dvali <dvali.daviti@gmail.com>
2020-05-14 10:51:25 +0400

    Longest Word in Dictionary through Deleting

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Longest Word in Dictionary through Deleting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static string FindLongestWord(string s, IList<string> d)
+        {
+            string res = string.Empty;
+            foreach (var st in d)
+                if (Can(st) && 
+                    (res.Length < st.Length || (res.Length == st.Length && string.Compare(res, st) > 0)))
+                    res = st;
+
+            return res;
+
+            bool Can(string str)
+            {
+                int i = 0, j = 0;
+                while (i < s.Length && j < str.Length)
+                {
+                    if (s[i] == str[j])
+                        i++;
+                    j++;
+                }
+
+                return i == s.Length;
+            }
+        }
+    }
+}

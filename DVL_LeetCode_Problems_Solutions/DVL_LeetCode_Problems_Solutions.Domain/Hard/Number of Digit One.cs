6270ef2a73c7068312f30053f3b9e7c15d8b4792
D.Dvali <dvali.daviti@gmail.com>
2020-05-15 13:01:47 +0400

    Number of Digit One

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Number of Digit One.cs
@@ -0,0 +1,18 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Number of Digit One (Not Mine)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int CountDigitOne(int n)
+        {
+            int ones = 0;
+            for (long m = 1; m <= n; m *= 10)
+                ones += (int) ((n / m + 8) / 10 * m + (n / m % 10 == 1 ? 1 : 0) * (n % m + 1));
+            return ones;
+        }
+    }
+}

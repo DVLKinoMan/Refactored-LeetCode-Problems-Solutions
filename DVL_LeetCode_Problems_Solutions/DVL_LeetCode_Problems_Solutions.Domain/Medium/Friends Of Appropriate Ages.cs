c0c7a69423480044f1e95173d869512c7c75f38b
DVL_KinoMan <dvali.daviti@gmail.com>
2020-05-16 11:51:14 +0400

    Friends Of Appropriate Ages

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Friends Of Appropriate Ages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Friends Of Appropriate Ages (Mine)
+        /// </summary>
+        /// <param name="ages"></param>
+        /// <returns></returns>
+        public static int NumFriendRequests(int[] ages)
+        {
+            Array.Sort(ages);
+            int lastIndex = 0, count = 0;
+            for (int A = 1; A < ages.Length; A++)
+            for (int B = lastIndex; B < A; B++)
+            {
+                if (ages[B] > 0.5 * ages[A] + 7)
+                {
+                    count += A - B;
+                    int j = A - 1;
+                    while (j >= 0 && ages[j--] == ages[A])
+                        count++;
+                    break;
+                }
+
+                lastIndex++;
+            }
+
+            return count;
+        }
+    }
+}

88c40b67f6d24e679c458ec1efb7cfa247ecb0d5
DVL_KinoMan <dvali.daviti@gmail.com>
2020-05-17 11:57:25 +0400

    Consecutive Characters

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Easy/Consecutive Characters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+   partial class ProblemSolver
+   {
+      /// <summary>
+      /// Consecutive Characters (Mine)
+      /// </summary>
+      /// <param name="s"></param>
+      /// <returns></returns>
+      public static int MaxPower(string s) {
+         int stIndex = 0, currLen = 1, max = 1;
+         for(int i=1; i<s.Length; i++){
+            if(s[i]!=s[stIndex])
+            {
+               stIndex = i;
+               max = Math.Max(max, currLen);
+               currLen = 1;
+            }
+            else currLen++;
+         }
+        
+         return Math.Max(max, currLen);
+      }
+   }
+}

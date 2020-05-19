34a613f6d3a8a89e4a2fa8346b9013f438e8bce8
D.Dvali <dvali.daviti@gmail.com>
2020-05-19 11:31:14 +0400

    Count Good Nodes in Binary Tree

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Medium/Count Good Nodes in Binary Tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DVL_LeetCode_Problems_Solutions.Domain.Classes;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Count Good Nodes in Binary Tree (Mine)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public static int GoodNodes(TreeNode root, int maxValue = -10001)
+        {
+            if (root == null)
+                return 0;
+
+            int max = Math.Max(maxValue, root.val);
+
+            return GoodNodes(root.left, max) + GoodNodes(root.right, max) + (root.val >= maxValue ? 1 : 0);
+        }
+    }
+}

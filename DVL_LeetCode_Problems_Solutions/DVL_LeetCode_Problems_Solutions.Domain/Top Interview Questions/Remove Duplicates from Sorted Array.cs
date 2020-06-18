91be6193bc4fb5716152cc4b7631a75ac710eab2
DVL_KinoMan <dvali.daviti@gmail.com>
2020-06-18 20:34:50 +0400

    Top Interview Questions (Remove Duplicates from Sorted Array)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Top Interview Questions/Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,20 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Remove Duplicates from Sorted Array
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int RemoveDuplicates2(int[] nums)
+        {
+            int j = 0;
+            for (int i = 1; i < nums.Length; i++)
+                if (nums[j] != nums[i])
+                    nums[++j] = nums[i];
+
+            return nums.Length == 0 ? 0 : j + 1;
+        }
+    }
+}

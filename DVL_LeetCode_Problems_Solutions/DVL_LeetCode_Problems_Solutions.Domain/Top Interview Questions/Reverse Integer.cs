a0c6f9d0cde087bf47c468f4b5d1f1db7db155e9
DVL_KinoMan <dvali.daviti@gmail.com>
2020-06-24 23:06:28 +0400

    Top Interview Questions (Reverse Integer)

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Top Interview Questions/Reverse Integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        public static int Reverse2(int x)
+        {
+            if (x == int.MinValue)
+                return 0;
+            int x2 = Math.Abs(x);
+            int overflow = int.MaxValue / 10, res = 0;
+            while (x2 != 0)
+            {
+                if (res > overflow)
+                    return 0;
+                res *= 10;
+                res += (x2 % 10);
+                x2 /= 10;
+            }
+
+            return x < 0 ? -res : res;
+        }
+    }
+}

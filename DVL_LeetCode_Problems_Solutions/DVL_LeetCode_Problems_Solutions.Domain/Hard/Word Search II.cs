e5641e74f828422b7e5c18162890d9b079c4ec18
DVL_KinoMan <dvali.daviti@gmail.com>
2021-01-10 17:12:59 +0400

    Word Search II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Word Search II.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Word Search II (Mine)
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public static IList<string> FindWords(char[][] board, string[] words)
+        {
+            int m = board.Length, n = board[0].Length;
+            var list = new List<string>();
+            foreach (var word in words)
+            {
+                bool isInBoard = false;
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                        if (Dfs(word, 0, i, j, new HashSet<(int, int)>()))
+                        {
+                            list.Add(word);
+                            isInBoard = true;
+                            break;
+                        }
+
+                    if (isInBoard)
+                        break;
+                }
+            }
+
+            return list;
+
+            bool Dfs(string w, int charIndex, int i, int j, HashSet<(int, int)> previousCoords)
+            {
+                if (previousCoords.Contains((i, j)) || i >= m || i < 0 || j >= n || j < 0 ||
+                    w[charIndex] != board[i][j])
+                    return false;
+
+                charIndex++;
+                if (charIndex == w.Length)
+                    return true;
+                previousCoords.Add((i, j));
+
+                bool result = Dfs(w, charIndex, i + 1, j, previousCoords) ||
+                              Dfs(w, charIndex, i, j + 1, previousCoords) ||
+                              Dfs(w, charIndex, i - 1, j, previousCoords) ||
+                              Dfs(w, charIndex, i, j - 1, previousCoords);
+
+                previousCoords.Remove((i, j));
+
+                return result;
+            }
+        }
+    }
+}

6ac5fab9601d8697e0a0d155fa66e26d60e7bca7
DVL_KinoMan <dvali.daviti@gmail.com>
2021-01-12 21:22:11 +0400

    Minimum Window Substring

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Minimum Window Substring.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Minimum Window Substring (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static string MinWindow(string s, string t)
+        {
+            var dict = new Dictionary<char, int>();
+            foreach (var ch in t)
+                dict[ch] = dict.ContainsKey(ch) ? dict[ch] + 1 : 1;
+
+            int curBegin = 0,
+                curEnd = 0,
+                needCharactersCount = t.Length,
+                begin = 0,
+                end = int.MaxValue;
+            while (curEnd < s.Length)
+            {
+                if (dict.ContainsKey(s[curEnd]))
+                {
+                    dict[s[curEnd]]--;
+                    if (dict[s[curEnd]] >= 0)
+                        needCharactersCount--;
+                }
+
+                while (needCharactersCount == 0)
+                {
+                    if (end - begin > curEnd - curBegin)
+                    {
+                        begin = curBegin;
+                        end = curEnd;
+                    }
+
+                    if (dict.ContainsKey(s[curBegin]))
+                    {
+                        dict[s[curBegin]]++;
+                        if (dict[s[curBegin]] > 0)
+                            needCharactersCount++;
+                    }
+
+                    curBegin++;
+                }
+
+                curEnd++;
+            }
+
+            return end == int.MaxValue ? "" : s.Substring(begin, end - begin + 1);
+        }
+    }
+}

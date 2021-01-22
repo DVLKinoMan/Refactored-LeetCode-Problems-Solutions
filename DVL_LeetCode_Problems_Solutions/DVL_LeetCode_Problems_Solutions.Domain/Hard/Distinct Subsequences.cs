8ae842153e64e42be4c9e451bffce6c9115e57aa
DVL_KinoMan <dvali.daviti@gmail.com>
2021-01-22 16:29:12 +0400

    Distinct Subsequences

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Distinct Subsequences.cs
@@ -0,0 +1,26 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Distinct Subsequences (Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static int NumDistinct(string s, string t)
+        {
+            if (s.Length == 0 || t.Length == 0)
+                return 0;
+
+            var din = new int[t.Length + 1,s.Length + 1];
+            for (int j = 0; j < s.Length + 1; j++)
+                din[0, j] = 1;
+            for (int i = 0; i < t.Length; i++)
+            for (int j = 0; j < s.Length; j++)
+                din[i + 1, j + 1] = t[i] == s[j] ? din[i, j] + din[i + 1, j] : din[i + 1, j];
+
+            return din[t.Length, s.Length];
+        }
+    }
+}

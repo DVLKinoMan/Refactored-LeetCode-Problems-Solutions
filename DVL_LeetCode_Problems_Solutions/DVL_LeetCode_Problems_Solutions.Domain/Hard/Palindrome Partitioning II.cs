a7ed5378c8e8eed61f63423fd4a2243958491b8c
DVL_KinoMan <dvali.daviti@gmail.com>
2021-01-23 17:08:28 +0400

    Palindrome Partitioning II

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Palindrome Partitioning II.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Palindrome Partitioning II (Not Mine)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int MinCut(string s)
+        {
+            var palindromes = new bool[s.Length, s.Length];
+            var minCuts = new int[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int min = i;
+                for (int j = 0; j <= i; j++)
+                    if (s[i] == s[j] && (j + 1 > i - 1 || palindromes[j + 1, i - 1]))
+                    {
+                        palindromes[j, i] = true;
+                        min = j == 0 ? 0 : Math.Min(min, minCuts[j - 1] + 1);
+                    }
+
+                minCuts[i] = min;
+            }
+
+            return minCuts[s.Length - 1];
+        }
+    }
+}

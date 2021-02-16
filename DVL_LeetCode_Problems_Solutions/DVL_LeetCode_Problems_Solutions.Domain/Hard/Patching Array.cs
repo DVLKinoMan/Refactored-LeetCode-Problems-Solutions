b6bd07db850887a20b9c01c8788d428b521d2be7
DVL_KinoMan <dvali.daviti@gmail.com>
2021-02-16 13:42:36 +0400

    Patching Array

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Patching Array.cs
@@ -0,0 +1,29 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Patching Array (Not Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int MinPatches(int[] nums, int n)
+        {
+            long miss = 1;
+            int added = 0, i = 0;
+            while (miss <= n)
+            {
+                if (i < nums.Length && nums[i] <= miss)
+                    miss += nums[i++];
+                else
+                {
+                    miss += miss;
+                    added++;
+                }
+            }
+
+            return added;
+        }
+    }
+}

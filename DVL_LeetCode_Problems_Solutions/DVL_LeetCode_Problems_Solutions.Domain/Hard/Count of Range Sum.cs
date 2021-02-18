917938284748a368b0ae3f4093616393ac02c43c
DVL_KinoMan <dvali.daviti@gmail.com>
2021-02-18 17:15:40 +0400

    Count of Range Sum

---
+++ DVL_LeetCode_Problems_Solutions/DVL_LeetCode_Problems_Solutions.Domain/Hard/Count of Range Sum.cs
@@ -0,0 +1,29 @@
+﻿namespace DVL_LeetCode_Problems_Solutions.Domain
+{
+    partial class ProblemSolver
+    {
+        /// <summary>
+        /// Count of Range Sum (Mine)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="lower"></param>
+        /// <param name="upper"></param>
+        /// <returns></returns>
+        public static int CountRangeSum(int[] nums, int lower, int upper)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                long sum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sum += nums[j];
+                    if (sum >= lower && sum <= upper)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
